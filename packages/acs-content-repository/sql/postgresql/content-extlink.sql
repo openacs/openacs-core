a9c937cefb4b8856d157ef3ab6099ca046cae7db
Dan Wickstrom <dcwickstrom@earthlink.net>
2001-03-25 21:18:51 +0000

    initial port to postgresql

---
+++ packages/acs-content-repository/sql/postgresql/content-extlink.sql
@@ -0,0 +1,113 @@
+-- Data model to support content repository of the ArsDigita
+-- Community System
+
+-- Copyright (C) 1999-2000 ArsDigita Corporation
+-- Author: Karl Goldstein (karlg@arsdigita.com)
+
+-- $Id$
+
+-- This is free software distributed under the terms of the GNU Public
+-- License.  Full text of the license is available from the GNU Project:
+-- http://www.fsf.org/copyleft/gpl.html
+
+-- create or replace package body content_extlink
+-- function new
+create function content_extlink__new (varchar,varchar,varchar,varchar,integer,integer,timestamp,integer,varchar)
+returns integer as '
+declare
+  new__name                   alias for $1;  
+  new__url                    alias for $2;  
+  new__label                  alias for $3;  
+  new__description            alias for $4;  
+  new__parent_id              alias for $5;  
+  new__extlink_id             alias for $6;  
+  new__creation_date          alias for $7;  
+  new__creation_user          alias for $8;  
+  new__creation_ip            alias for $9;  
+  v_extlink_id                cr_extlinks.extlink_id%TYPE;
+  v_label                     cr_extlinks.label%TYPE;
+  v_name                      cr_items.name%TYPE;
+begin
+
+  if new__label is null then
+    v_label := new__url;
+  else
+    v_label := new__label;
+  end if;
+
+  if new__name is null then
+    select acs_object_id_seq.nextval into v_extlink_id from dual;
+    v_name := ''link'' || v_extlink_id;
+  else
+    v_name := new__name;
+  end if;
+
+  v_extlink_id := content_item__new(
+      name          => v_name, 
+      parent_id     => content_extlink.new__parent_id,
+      item_id       => content_extlink.new__extlink_id,
+      null,
+      creation_date => content_extlink.new__creation_date, 
+      creation_user => content_extlink.new__creation_user, 
+      null,
+      creation_ip   => content_extlink.new__creation_ip, 
+      ''content_item'',
+      content_type  => ''content_extlink'', 
+      null,
+      null,
+      ''text/plain'',
+      null,
+      null,
+      null
+  );
+
+  insert into cr_extlinks
+    (extlink_id, url, label, description)
+  values
+    (v_extlink_id, new__url, v_label, new__description);
+
+  return v_extlink_id;
+
+end;' language 'plpgsql';
+
+
+-- procedure delete
+create function content_extlink__delete (integer)
+returns integer as '
+declare
+  delete__extlink_id             alias for $1;  
+begin
+
+  delete from cr_extlinks
+    where extlink_id = delete__extlink_id;
+
+  PERFORM content_item__delete(delete__extlink_id);
+
+return 0; 
+end;' language 'plpgsql';
+
+
+-- function is_extlink
+create function content_extlink__is_extlink (integer)
+returns boolean as '
+declare
+  is_extlink__item_id                alias for $1;  
+  v_extlink_p                        boolean;
+begin
+
+  select 
+    count(1) = 1 into v_extlink_p
+  from 
+    cr_extlinks
+  where 
+    extlink_id = is_extlink__item_id;
+  
+  return v_extlink_p;
+ 
+end;' language 'plpgsql';
+
+
+
+-- show errors
+
+
