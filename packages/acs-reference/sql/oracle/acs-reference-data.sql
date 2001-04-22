31842e2013a924e137e7b65f27e6517df2a64aeb
Jon Griffin <jon@jongriffin.com>
2001-04-22 00:53:12 +0000

    initial openacs import

---
+++ packages/acs-reference/sql/oracle/acs-reference-data.sql
@@ -0,0 +1,16 @@
+-- packages/acs-reference/sql/acs-reference-data.sql
+--
+-- Load all of the basic reference data.
+--
+-- @author  ron@arsdigita.com
+-- @created 2000-12-08
+-- @cvs-id  $Id$
+
+@@language
+@@country
+@@currency
+@@s-and-p-data
+@@timezones
+@@us-states
+@@us-counties
+@@us-zipcodes
---
+++ packages/acs-reference/sql/oracle/acs-reference-drop.sql
@@ -0,0 +1,36 @@
+-- Drop the ACS Reference packages
+--
+-- @author ron@arsdigita.com
+-- @cvs-id $Id$
+
+set serveroutput on
+
+-- drop all associated tables and packages
+
+declare
+    cursor refsrc_cur is
+	 select   table_name,
+                  package_name
+	 from     acs_reference_repositories
+         order by creation_date desc;
+begin
+    for rec in refsrc_cur loop
+	 dbms_output.put_line('Dropping ' || rec.table_name);
+	 execute immediate 'drop table ' || rec.table_name;
+	 if rec.package_name is not null then
+	     execute immediate 'drop package ' || rec.package_name;
+         end if;
+    end loop;
+end;
+/
+show errors
+
+begin
+    acs_object_type.drop_type('acs_reference_repository','t');
+end;
+/
+show errors
+  
+drop package acs_reference_repository;
+drop table   acs_reference_repositories;
+
