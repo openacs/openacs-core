d7cb0bd2bf212dfcdd46d15aeca6a56ec4bc5ea3
Luke Pond <dlpond@pobox.com>
2001-07-05 21:11:37 +0000

    add tree_sortkey to acs_messages to enable hierarchical queries on the reply_to field

--- packages/acs-messaging/sql/postgresql/acs-messaging-views.sql
+++ packages/acs-messaging/sql/postgresql/acs-messaging-views.sql
@@ -11,13 +11,13 @@
 
 create view acs_messages_all as
     select m.message_id, m.reply_to, m.sent_date, m.sender, m.rfc822_id,
-           r.revision_id, r.title, r.mime_type, r.content
+           m.tree_sortkey, r.revision_id, r.title, r.mime_type, r.content
         from cr_items i, cr_revisions r, acs_messages m
         where i.item_id = m.message_id and r.revision_id = i.live_revision;
 
 create view acs_messages_latest as
     select m.message_id, m.reply_to, m.sent_date, m.sender, m.rfc822_id,
-           r.revision_id, r.title, r.mime_type, r.content
+           m.tree_sortkey, r.revision_id, r.title, r.mime_type, r.content
         from cr_items i, cr_revisions r, acs_messages m
         where i.item_id = m.message_id
             and r.revision_id = content_item__get_latest_revision(i.item_id);
