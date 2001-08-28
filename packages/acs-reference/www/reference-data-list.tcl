559a6a49ae7fb29c46c237b0feefc4314f48abe9
Jon Griffin <jon@jongriffin.com>
2001-08-28 04:00:52 +0000

    Initial import

---
+++ packages/acs-reference/www/reference-data-list.tcl
@@ -0,0 +1,25 @@
+ad_page_contract {
+    This page lists all the formats
+    
+    @author Jon Griffin (jon@jongriffin.com)
+    @creation-date 2000-10-04
+    @cvs-id $Id$
+} {
+} -properties {
+    context_bar:onevalue
+    package_id:onevalue
+    user_id:onevalue
+    data:multirow
+}
+
+
+set package_id [ad_conn package_id]
+set title "List Reference Data"
+set context_bar [list "$title"]
+ 
+set user_id [ad_verify_and_get_user_id]
+
+db_multirow data data_select { 
+}
+
+ad_return_template
