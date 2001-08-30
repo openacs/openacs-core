6933dff364dfcce51b010fe7349fdb354c363b2b
Don Baccus <dhogaza@pacifier.com>
2001-08-30 21:40:14 +0000

    Another ACS vs. OpenACS patch from Jonathan.

--- packages/acs-bootstrap-installer/installer/index.tcl
+++ packages/acs-bootstrap-installer/installer/index.tcl
@@ -19,27 +19,27 @@ ad_page_contract {
 if { [ns_queryexists done_p] } {
     # When installation is complete, the user is redirected to /index?done_p=1
     # (well, actually, /?done_p=1). This is so the user can just hit his/her
-    # browser's Reload button to get the main ACS login page once (s)he's
+    # browser's Reload button to get the main OpenACS login page once (s)he's
     # restarted the OpenNSD.
     
   if { [ad_verify_install] } {
-    install_return 200 "Complete" "
+    install_return 200 "OpenACS Installation Complete" "
 
-You have successfully installed the ArsDigita Community System!
+You have successfully installed the OpenACS Community System!
 
 <p> Your server process has been terminated.  Unless you have configured your
-server to restart automatically, as outlined in the 
-<a href=\"http://acs40.arsdigita.com/doc/install-guide/acs.html#startup\">
-ACS Installation Guide</a>, you will need to start your server again.
-When the server restarts, the ACS will be fully functional and you can reload 
-this page to access the running server.
+web server to restart automatically, as outlined in the 
+<a href=\"http://openacs.org/4/\">ACS 4.x Installation Guide</a>, 
+you will need to start your web server again.
+When the web server restarts, OpenACS will be fully functional and you can reload 
+this page to access the running web server.
 "
     exit
   } else {
     install_return 200 "Error" "
-The installation program has encounted an error.  Please drop your ACS tablespace
-and the ACS username, recreate them, and try again.  You can log this as a bug
-using the <a href=\"http://www.arsdigita.com/sdm/\">SDM</a>. 
+The installation program has encounted an error.  Please drop your OpenACS tablespace
+and the OpenACS username, recreate them, and try again.  You can log this as a bug
+using the <a href=\"http://openacs.org/sdm/\">SDM</a>. 
 "
     return
   }
@@ -49,11 +49,11 @@ using the <a href=\"http://www.arsdigita.com/sdm/\">SDM</a>.
 
 set body "
 
-Thank you for installing the ArsDigita Community System,
+Thank you for installing the OpenACS Community System,
 a suite of fully-integrated enterprise-class solutions
 for collaborative commerce.
-This is the ACS Installer which performs all the steps necessary
-to get the ArsDigita Community System running on your server.<p>
+This is the OpenACS Installer which performs all the steps necessary
+to get the OpenACS Community System running on your server.<p>
 Please read the <a href=\"/doc/release-notes/\">Release Notes</a> 
 before proceeding to better understand what is contained in this release.
 
@@ -69,7 +69,7 @@ if { [nsv_exists acs_properties database_problem] } {
     append body "<p>
 [nsv_get acs_properties database_problem]
 
-<p><b>The first step involved in setting up your ACS
+<p><b>The first step involved in setting up your OpenACS
 installation is to configure your RDBMS, correctly install a database driver,
 and configure OpenNSD to use it.  You can download 
 and install the latest version of the ArsDigita Oracle driver for
@@ -93,7 +93,7 @@ if { !$error_p } {
 # OpenNSD must support ns_sha1
 if { [catch { ns_sha1 quixotusishardcore }] } {
     append errors "<li><p><b>The ns_sha1 function is missing. This function is
-    required in ACS 4.0 so that passwords can be securely stored in
+    required in OpenACS 4.x so that passwords can be securely stored in
     the database. This function is available in the nssha1 module that is part of the <a
     href=\"http://www.arsdigita.com/aol3/\">ArsDigita server
     distribution</a>.</b></p>"
@@ -104,7 +104,7 @@ if { [catch { ns_sha1 quixotusishardcore }] } {
 # OpenNSD must support Tcl 8.x
 if { [string range [info tclversion] 0 0] < 8 } {
     append errors " <li><p><strong> You are using a version of Tcl less than 8.0.  You must use Tcl version 8.0
-    for the ACS to work.  Probably your <code>nsd</code> executable is linked to <code>nsd76</code>.  Please
+    for OpenACS to work.  Probably your <code>nsd</code> executable is linked to <code>nsd76</code>.  Please
     link it to <code>nsd8x</code> to fix this problem.  Please refer to the 
     <a href=\"/doc/install-guide/\">Installation Guide</a>.
     <blockquote><pre>
@@ -117,7 +117,7 @@ if { [string range [info tclversion] 0 0] < 8 } {
 # OpenNSD must support ns_cache.
 if {[llength [info commands ns_cache]] < 1} {
     append errors "<li><p><strong>The <code>ns_cache</code> module is not installed.  This
-is required to support the ACS Security system.  Please make sure that <code>ns_cache</code>
+is required to support the OpenACS Security system.  Please make sure that <code>ns_cache</code>
 is included in your module list.  An example module list is shown below:
 file (usually in <code>/home/aol30/yourservername.ini</code>) or see the 
 <a href=\"/doc/install-guide/\">Installation Guide</a> for more information.<p>
@@ -128,7 +128,7 @@ nslog=nslog.so
 nssha1=nssha1.so
 nscache=nscache.so
 </blockquote></pre>
-After adding <code>ns_cache</code>, please restart your server.
+After adding <code>ns_cache</code>, please restart your web server.
 </strong></p>"
     set error_p 1
 } 
@@ -143,7 +143,7 @@ if {![xml_support_ok xml_status_msg]} {
 set adp_support [ns_config "ns/server/[ns_info server]/adp" DefaultParser]
 if { [string compare $adp_support "fancy"] } {
     append errors "<li><p><strong>The fancy ADP parser is not enabled.  This is required to support 
-the ACS Templating System.  Without this templating system, none of the ACS pages installed by default
+the OpenACS Templating System.  Without this templating system, none of the OpenACS pages installed by default
 will display.  Please add the following to your OpenNSD configuration file (usually in 
 <code>/home/aol30/yourservname.ini</code>) or see the <a href=\"/doc/install-guide/\">Installation Guide</a> for 
 more information.<p>
@@ -152,7 +152,7 @@ more information.<p>
 Map=/*.adp 
 DefaultParser=fancy
 </blockquote></pre>
-After adding support for the fancy ADP parser, please restart your server.
+After adding support for the fancy ADP parser, please restart your web server.
 </strong></p>"
     set error_p 1
 }   
@@ -217,8 +217,8 @@ db_helper_checks errors error_p
 # correctly, let's check out the actual db.
 if {$error_p} {
     append body "<p>
-<strong>A number of misconfigurations were discovered that need to be corrected.
-Please fix all of them, restart the server, and try running the installer again.
+<strong>At least one misconfiguration was discovered that must be corrected.
+Please fix all of them, restart the web server, and try running the OpenACS installer again.
 You can proceed without resolving these errors, but the system may not function
 correctly.
 </strong>
@@ -234,7 +234,7 @@ $errors
 # thing to be installed is the apm_packages table - does that exist?
 if { ![db_table_exists apm_packages] } {
     # Nope. Need to install the data model.
-    append body "<p>The next step is to install the ACS kernel data model. Click the <i>Next</i>
+    append body "<p>The next step is to install the OpenACS kernel data model. Click the <i>Next</i>
     button to proceed.
     
     [install_next_button "install-data-model"]
@@ -242,10 +242,10 @@ if { ![db_table_exists apm_packages] } {
 } else {
     # OK, apm_packages is installed - let's check out some other stuff too:
     if { ![install_good_data_model_p] } {
-	append body "<p>It appears that the ACS data model is only partially installed.
+	append body "<p>It appears that the OpenACS data model is only partially installed.
 	Please drop your tablespace and start from scratch."
     } else {
-	append body "<p>The ACS data model is already installed. Click <i>Next</i> 
+	append body "<p>The OpenACS data model is already installed. Click <i>Next</i> 
 	to scan the available packages.
 	
 	[install_next_button "packages-install"]
