8b452f9a14613a4d0fb47dc3a655ba5ecfe85bf4
Dan Wickstrom <dcwickstrom@earthlink.net>
2001-09-26 12:09:27 +0000

    modified cr_fs_path to support alternate file storage areas

--- packages/acs-content-repository/tcl/acs-content-repository-init.tcl
+++ packages/acs-content-repository/tcl/acs-content-repository-init.tcl
@@ -12,3 +12,10 @@ ad_proc -public acs_cr_scheduled_release_exec {} {
 }
 
 ad_schedule_proc [expr 15 * 60] acs_cr_scheduled_release_exec
+
+nsv_set CR_LOCATIONS . ""
+if ![nsv_exists CR_LOCATIONS CR_FILES] {
+
+    nsv_set CR_LOCATIONS CR_FILES "[file dirname [string trimright [ns_info tcllib] "/"]]/content-repository-content-files"
+
+}
