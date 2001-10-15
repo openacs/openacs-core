2bba5455e8089d50478bd064a807035f71a987f8
Don Baccus <dhogaza@pacifier.com>
2001-10-15 19:33:38 +0000

    Made ref-timezones work with PostgreSQL, made names of reference packages be consistent.

--- packages/ref-timezones/sql/postgresql/ref-timezones-drop.sql
+++ packages/ref-timezones/sql/postgresql/ref-timezones-drop.sql
@@ -6,20 +6,38 @@
 -- @created 2000-12-04
 -- @cvs-id  $Id$
 
+create function inline_0() returns integer as '
 declare
-    v_repository_id integer;
+    rec        acs_reference_repositories%ROWTYPE;
 begin
-    select repository_id into v_repository_id
-    from   acs_reference_repositories
-    where  lower(table_name) = 'timezones';
+    for rec in select * from acs_reference_repositories where upper(table_name) = ''TIMEZONES'' loop
+	 execute ''drop table '' || rec.table_name;
+         perform acs_reference__delete(rec.repository_id);
+    end loop;
+    return 0;
+end;' language 'plpgsql';
 
-    acs_reference.delete(v_repository_id);
-end;
-/
+select inline_0();
+drop function inline_0();
 
 drop sequence timezone_seq;
 drop table    timezone_rules;
-drop table    timezones;
-drop package  timezone;
 
-
+drop function rdbms_date(varchar);
+drop function timezone__new (varchar, varchar);
+drop function timezone__delete (integer);
+drop function timezone__get_id (varchar);
+drop function timezone__add_rule (varchar, varchar, integer, varchar, varchar, varchar, varchar, varchar);
+drop function timezone__convert_to_utc (integer, varchar);
+drop function timezone__convert_to_utc (varchar, varchar);
+drop function timezone__get_offset (integer, timestamp);
+drop function timezone__get_offset (varchar, timestamp);
+drop function timezone__get_rawoffset (integer, timestamp);
+drop function timezone__get_rawoffset (varchar, timestamp);
+drop function timezone__get_abbrev (integer, timestamp);
+drop function timezone__get_abbrev (varchar, timestamp);
+drop function timezone__get_date (integer, timestamp, varchar);
+drop function timezone__get_date (varchar, timestamp, varchar);
+drop function timezone__isdst_p (integer, timestamp);
+drop function timezone__isdst_p (varchar, timestamp);
+drop function timezone__get_zone_offset (varchar, varchar, timestamp);
