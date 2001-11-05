53cf5e3b2cf69238d37dc766636b96dbc8cea4af
Don Baccus <dhogaza@pacifier.com>
2001-11-05 03:55:27 +0000

    Copying a version of a package didn't copy the database-supported field for its files.

--- packages/acs-kernel/sql/oracle/apm-create.sql
+++ packages/acs-kernel/sql/oracle/apm-create.sql
@@ -2114,8 +2114,8 @@ as
 	    from apm_package_dependencies
 	    where version_id = copy.version_id;
     
-	insert into apm_package_files(file_id, version_id, path, file_type)
-	    select acs_object_id_seq.nextval, v_version_id, path, file_type
+	insert into apm_package_files(file_id, version_id, path, file_type, db_type)
+	    select acs_object_id_seq.nextval, v_version_id, path, file_type, db_type
 	    from apm_package_files
 	    where version_id = copy.version_id;
     
