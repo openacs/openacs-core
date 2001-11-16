072f7d2e755e186111a6e65d305eeab5b26778d6
Don Baccus <dhogaza@pacifier.com>
2001-11-16 03:11:08 +0000

    1. The PG port of apm__register_parameter had a tiny error which caused it    to not set the new parameter to its default value in any of the effected    package instances.
    2. Glossary - added admin as a parent of glossary_admin so admins can admin.
    
    3. edit-this-page PG: removed code that was written because of #1 above
    
    4. edit-this-page Oracle: finished the datamodel port, added drop script

--- packages/acs-content-repository/sql/oracle/content-create.sql
+++ packages/acs-content-repository/sql/oracle/content-create.sql
@@ -447,9 +447,8 @@ create table cr_folders (
                      default 'f'
                      constraint cr_folder_symlink_chk
                      check (has_child_symlinks in ('t', 'f')),
-  package_id integer 
-  constraint cr_fldr_pkg_id_fk
-  references apm_packages
+  package_id        integer 
+                    constraint cr_fldr_pkg_id_fk references apm_packages
 );  
 
 comment on table cr_folders is '
