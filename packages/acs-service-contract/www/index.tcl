f7b948a222561c2722d09bb566fea7c13ca1eed7
Don Baccus <dhogaza@pacifier.com>
2002-03-08 20:23:12 +0000

    Fixed trailing semicolons in queries that Oracle doesn't like.

--- packages/acs-service-contract/www/index.tcl
+++ packages/acs-service-contract/www/index.tcl
@@ -1,31 +1,11 @@
 
 set context_bar [ad_context_bar]
 
-db_multirow valid_installed_binding valid_installed_binding {
-    select 
-        contract_id,
-        impl_id,
-        acs_sc_contract__get_name(contract_id) as contract_name,
-        acs_sc_impl__get_name(impl_id) as impl_name
-    from
-        acs_sc_bindings
-}
+db_multirow valid_installed_binding valid_installed_binding ""
 
+db_multirow valid_uninstalled_binding valid_uninstalled_binding ""
 
-db_multirow valid_uninstalled_binding valid_uninstalled_binding {
-    select contract_id, contract_name, impl_name,impl_id 
-    from valid_uninstalled_bindings;
-}
+db_multirow invalid_uninstalled_binding invalid_uninstalled_binding ""
 
-
-db_multirow invalid_uninstalled_binding invalid_uninstalled_binding {
-    select contract_id, contract_name, impl_name,impl_id 
-    from invalid_uninstalled_bindings;
-}
-
-
-db_multirow orphan_implementation orphan_implementation {
-    select impl_id, impl_name, impl_contract_name  
-    from orphan_implementations
-}
+db_multirow orphan_implementation orphan_implementation ""
 
