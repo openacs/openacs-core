d4a87aff7f5c6359d50460e3f400072f3f0169bb
Don Baccus <dhogaza@pacifier.com>
2002-03-13 01:58:06 +0000

    Applied Lars Pind's patch #178, removing an extraneous "]" from the context bar.

--- packages/acs-subsite/www/admin/groups/one.tcl
+++ packages/acs-subsite/www/admin/groups/one.tcl
@@ -51,7 +51,7 @@ db_1row group_info {
        and g.group_id = :group_id
 }
 
-set context_bar [list [list "[ad_conn package_url]admin/groups/]" "Groups"] "One Group"]
+set context_bar [list [list "[ad_conn package_url]admin/groups/" "Groups"] "One Group"]
 
 attribute::multirow \
 	-start_with group \
