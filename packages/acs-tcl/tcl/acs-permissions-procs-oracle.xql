7d67820216958fec6f8626def6b3da71505d7ec8
Yonatan Feldman <yon@milliped.com>
2002-03-18 21:59:14 +0000

    cleaned up permissions api and added a few useful functions

--- packages/acs-tcl/tcl/acs-permissions-procs-oracle.xql
+++ packages/acs-tcl/tcl/acs-permissions-procs-oracle.xql
@@ -3,41 +3,48 @@
 <queryset>
    <rdbms><type>oracle</type><version>8.1.6</version></rdbms>
 
-<fullquery name="ad_permission_grant.grant_permission">
-<querytext>
-	declare begin
-	acs_permission.grant_permission(object_id => :object_id,
-					grantee_id => :user_id,
-					privilege => :privilege);
-	end;
-</querytext>
-</fullquery>
+    <fullquery name="permission::grant.grant_permission">
+        <querytext>
+            declare
+            begin
+                acs_permission.grant_permission(
+                    object_id => :object_id,
+                    grantee_id => :party_id,
+                    privilege => :privilege
+                );
+            end;
+        </querytext>
+    </fullquery>
 
-<fullquery name="ad_permission_revoke.revoke_permission">
-<querytext>
-	declare begin
-	acs_permission.revoke_permission(object_id => :object_id,
-					grantee_id => :user_id,
-					privilege => :privilege);
-	end;
-</querytext>
-</fullquery>
+    <fullquery name="permission::revoke.revoke_permission">
+        <querytext>
+            declare
+            begin
+                acs_permission.revoke_permission(
+                    object_id => :object_id,
+                    grantee_id => :party_id,
+                    privilege => :privilege
+                );
+            end;
+        </querytext>
+    </fullquery>
 
-<fullquery name="ad_permission_p.result">      
-      <querytext>
-      
-    select count(*) 
-      from dual
-     where acs_permission.permission_p(:object_id, :user_id, :privilege) = 't'
-  
-      </querytext>
-</fullquery>
- 
-<fullquery name="ad_require_permission.name">      
-      <querytext>
-      select acs_object.name(:object_id) from dual
-      </querytext>
-</fullquery>
+    <fullquery name="permission::permission_p.select_permission_p">
+        <querytext>
+            select count(*)
+            from dual
+            where exists (select 1
+                          from dual
+                          where 't' = acs_permission.permission_p(:object_id, :party_id, :privilege))
+        </querytext>
+    </fullquery>
+
+    <fullquery name="permission::require_permission.name">      
+        <querytext>
+            select acs_object.name(:object_id)
+            from dual
+        </querytext>
+    </fullquery>
 
     <fullquery name="permission::toggle_inherit.toggle_inherit">
         <querytext>
--- packages/acs-tcl/tcl/acs-permissions-procs.tcl
+++ packages/acs-tcl/tcl/acs-permissions-procs.tcl
@@ -1,9 +1,9 @@
 ad_library {
-  Tcl procs for the acs permissioning system.
+    Tcl procs for the acs permissioning system.
 
-  @author rhs@mit.edu
-  @creation-date 2000-08-17
-  @cvs-id $Id$
+    @author rhs@mit.edu
+    @creation-date 2000-08-17
+    @cvs-id $Id$
 }
 
 ad_proc -public ad_permission_grant {
@@ -15,7 +15,7 @@ ad_proc -public ad_permission_grant {
 
     @author ben@openforce
 } {
-    db_exec_plsql grant_permission {}
+    permission::grant -party_id $user_id -object_id $object_id -privilege $privilege
 }
 
 ad_proc -public ad_permission_revoke {
@@ -27,71 +27,98 @@ ad_proc -public ad_permission_revoke {
 
     @author ben@openforce
 } {
-    db_exec_plsql revoke_permission {}
+    permission::revoke -party_id $user_id -object_id $object_id -privilege $privilege
 }
 
 ad_proc -public ad_permission_p {
-  {-user_id ""}
-  object_id
-  privilege
+    {-user_id ""}
+    object_id
+    privilege
 } {
-  if {[empty_string_p $user_id]} {
-    set user_id [ad_verify_and_get_user_id]
-  }
-
-  if { [db_string result {
-    select count(*) 
-      from dual
-     where acs_permission.permission_p(:object_id, :user_id, :privilege) = 't'
-  }] } {
-      return 1
-  }
-
-  # This user doesn't have permission. If we're not in performance mode, 
-  # Let's check the name of the privilege and throw an error if no
-  # such privilege exists.
-  if { ![rp_performance_mode] && ![db_string n_privs {
-      select count(*)
-        from acs_privileges
-       where privilege = :privilege
-  }] } {
-    error "$privilege isn't a valid privilege"
-  }
-
-  return 0
+    return [permission::permission_p -party_id $user_id -object_id $object_id -privilege $privilege]
 }
 
 ad_proc -public ad_require_permission {
   object_id
   privilege
 } {
-  set user_id [ad_verify_and_get_user_id]
-  if {![ad_permission_p $object_id $privilege]} {
-    if {$user_id == 0} {
-      ad_maybe_redirect_for_registration
-    } else {
-      ns_log Notice "$user_id doesn't have $privilege on object $object_id"
-      ad_return_forbidden "Security Violation" "<blockquote>
-      You don't have permission to $privilege [db_string name {select acs_object.name(:object_id) from dual}].
-      <p>
-      This incident has been logged.
-      </blockquote>"
-    }
-    ad_script_abort
-  }
+    permission::require_permission -object_id $object_id -privilege $privilege
 }
 
 ad_proc -private ad_admin_filter {} {
-  ad_require_permission [ad_conn object_id] admin
-  return filter_ok
+    permission::require_permission -object_id [ad_conn object_id] -privilege "admin"
+    return filter_ok
 }
 
 ad_proc -private ad_user_filter {} {
-  ad_require_permission [ad_conn object_id] read
-  return filter_ok
+    permission::require_permission -object_id [ad_conn object_id] -privilege "read"
+    return filter_ok
 }
 
 namespace eval permission {
+
+    ad_proc -public grant {
+        {-party_id:required}
+        {-object_id:required}
+        {-privilege:required}
+    } {
+        grant privilege Y to party X on object Z
+    } {
+        db_exec_plsql grant_permission {}
+    }
+
+    ad_proc -public revoke {
+        {-party_id:required}
+        {-object_id:required}
+        {-privilege:required}
+    } {
+        revoke privilege Y from party X on object Z
+    } {
+        db_exec_plsql revoke_permission {}
+    }
+
+    ad_proc -public permission_p {
+        {-party_id ""}
+        {-object_id:required}
+        {-privilege:required}
+    } {
+        does party X have privilege Y on object Z
+    } {
+        if {[empty_string_p $party_id]} {
+            set party_id [ad_conn user_id]
+        }
+
+        return [db_string select_permission_p {}]
+    }
+
+    ad_proc -public require_permission {
+        {-party_id ""}
+        {-object_id:required}
+        {-privilege:required}
+    } {
+        require that party X have privilege Y on object Z
+    } {
+        if {[empty_string_p $party_id]} {
+            set party_id [ad_conn user_id]
+        }
+
+        if {![permission_p -party_id $party_id -object_id $object_id -privilege $privilege]} {
+            if {!${party_id}} {
+                ad_maybe_redirect_for_registration
+            } else {
+                ns_log notice "$party_id doesn't have $privilege on object $object_id"
+                ad_return_forbidden \
+                    "Security Violation" \
+                    "<blockquote>
+  You don't have permission to $privilege [db_string name {}].
+  <br>
+  This incident has been logged.
+</blockquote>"
+            }
+
+            ad_script_abort
+        }
+    }
 
     ad_proc -public toggle_inherit {
         {-object_id:required}
