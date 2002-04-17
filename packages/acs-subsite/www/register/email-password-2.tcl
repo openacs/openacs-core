5d725c5bfe064574ee747ed0acf485941a7a825d
Yonatan Feldman <yon@milliped.com>
2002-04-17 20:43:40 +0000

    make variable always available

--- packages/acs-subsite/www/register/email-password-2.tcl
+++ packages/acs-subsite/www/register/email-password-2.tcl
@@ -32,8 +32,8 @@ if {![db_0or1row select_email {}]} {
     return
 }
 
+set ask_question_p 0
 if {!$validated_p} {
-    set ask_question_p 0
     if {[exists_and_not_null answer]} {
         if {[db_string select_answer_matches_p {}} {
             set validated_p 1
