448e5e64a9875be22d8930988caa658365cd3577
Yonatan Feldman <yon@milliped.com>
2002-05-04 18:27:52 +0000

    first version of this simple mail queue

---
+++ packages/acs-mail-lite/sql/postgresql/acs-mail-lite-drop.sql
@@ -0,0 +1,9 @@
+--
+-- A simple mail queue
+--
+-- @author <a href="mailto:eric@openforce.net">eric@openforce.net</a>
+-- @version $Id$
+--
+
+drop table acs_mail_lite_queue;
+drop sequence acs_mail_lite_id_seq;
