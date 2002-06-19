17d938597519ec1d2e31be54ba7a3f8307f24b21
Yonatan Feldman <yon@milliped.com>
2002-06-19 19:42:20 +0000

    adding variation on function

--- packages/acs-kernel/sql/postgresql/acs-objects-create.sql
+++ packages/acs-kernel/sql/postgresql/acs-objects-create.sql
@@ -1333,6 +1333,14 @@ begin
   
 end;' language 'plpgsql';
 
+create function acs_object__update_last_modified (integer)
+returns integer as '
+declare
+    acs_object__update_last_modified__object_id     alias for $1;
+begin
+    return acs_object__update_last_modified(acs_object__update_last_modified__object_id, now());
+end;' language 'plpgsql';
+
 create function acs_object__update_last_modified (integer, timestamp)
 returns integer as '
 declare
