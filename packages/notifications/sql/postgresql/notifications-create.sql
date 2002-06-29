8dc281ee7497674d2b522a33f5fe9ec18ca6100b
Ben Adida <ben@openforce.net>
2002-06-29 01:27:50 +0000

    reporting to PG

--- packages/notifications/sql/postgresql/notifications-create.sql
+++ packages/notifications/sql/postgresql/notifications-create.sql
@@ -10,8 +10,18 @@
 \i notifications-core-create.sql
 \i notifications-package-create.sql
 
+-- replies
+\i notifications-replies-create.sql
+\i notifications-replies-package-create.sql
+
 -- the service contracts will eventually be created
 -- @ notifications-interval-sc-create.sql
 -- @ notifications-delivery-sc-create.sql
 
+\i notification-type-sc-create.sql
+
+-- WORK HERE!! (ben)
+
+\i delivery-method-sc-create.sql
 \i notifications-init.sql
+\i email-sc-impl-create.sql
