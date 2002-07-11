e169bfc4265e0f1f48385330e73db0078161f3c6
Yonatan Feldman <yon@milliped.com>
2002-07-11 04:30:28 +0000

    moved site_node::new_with_package to site_node_apm_integration::new_site_node_and_package

--- packages/acs-tcl/tcl/site-node-apm-integration-procs.tcl
+++ packages/acs-tcl/tcl/site-node-apm-integration-procs.tcl
@@ -11,6 +11,29 @@ ad_library {
 
 namespace eval site_node_apm_integration {
 
+    ad_proc -public new_site_node_and_package {
+        {-name:required}
+        {-parent_id:required}
+        {-package_key:required}
+        {-instance_name:required}
+        {-context_id:required}
+    } {
+        create site node, instantiate package, mount package at new site node
+    } {
+        set node_id [site_node::new -name $name -parent_id $parent_id]
+
+        set package_id [apm_package_create_instance $instance_name $context_id $package_key]
+
+        site_node::mount -node_id $node_id -object_id $package_id
+
+        site_node::update_cache -node_id $node_id
+
+        # call post instantiation proc for the package
+        apm_package_call_post_instantiation_proc $package_id $package_key
+
+        return $package_id
+    }
+
     ad_proc -public get_child_package_id {
         {-package_id ""}
         {-package_key:required}
@@ -32,7 +55,7 @@ namespace eval site_node_apm_integration {
         Returns 1 if there exists a child package with the given package_key, 
         or 0 if not.
     } {
-        set child_package_id [site_node_apm_integration::get_child_package_id \
+        set child_package_id [get_child_package_id \
             -package_id $package_id \
             -package_key $package_key
         ]
@@ -43,4 +66,5 @@ namespace eval site_node_apm_integration {
             return 1 
         }
     }
+
 }
