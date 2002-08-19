ee07d6e33dd55e5d203f193596200b1d53db688a
Arjun Sanyal <arjun@openforce.net>
2002-08-19 20:38:49 +0000

    fixing

--- packages/notifications/sql/postgresql/upgrade/upgrade-0.1d-0.2d.sql
+++ packages/notifications/sql/postgresql/upgrade/upgrade-0.1d-0.2d.sql
@@ -1,1 +1,13 @@
-\i ../notifications-package-create.sql
+drop function notification__delete;
+
+create function notification__delete(integer)
+returns integer as '
+declare
+    p_notification_id               alias for $1;
+begin
+    delete from notifications where notification_id = p_notification_id;
+    perform acs_object__delete(p_notification_id);
+    return 0;
+end;
+' language 'plpgsql';
+
