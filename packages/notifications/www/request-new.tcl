df5d5bb60dd6d77cf9b97e3a0ced5795297a7a26
Lars Pind <lars@pinds.com>
2002-09-16 12:36:31 +0000

    Maybe redirect for registration.

--- packages/notifications/www/request-new.tcl
+++ packages/notifications/www/request-new.tcl
@@ -12,7 +12,7 @@ ad_page_contract {
     return_url
 }
 
-set user_id [ad_conn user_id]
+set user_id [ad_maybe_redirect_for_registration]
 
 # Check that the object can be subcribed to
 notification::security::require_notify_object -object_id $object_id
