9906521e0a2d59f679ac9d7cda569f83b9b55999
Dan Wickstrom <dcwickstrom@earthlink.net>
2002-10-10 16:52:13 +0000

    changes to make search work with openfts 0.3

--- packages/search/sql/postgresql/search-packages-create.sql
+++ packages/search/sql/postgresql/search-packages-create.sql
@@ -24,15 +24,15 @@ end;' language 'plpgsql';
 create function search_observer__dequeue(integer,timestamp,varchar)
 returns integer as '
 declare
-    p_object_id			alias for $1;
-    p_event_date		alias for $2;
-    p_event			alias for $3;
+    p_object_id                 alias for $1;
+    p_event_date                alias for $2;
+    p_event                     alias for $3;
 begin
 
     delete from search_observer_queue 
     where object_id = p_object_id 
     and event = p_event
-    and event_date = p_event_date;
+    and to_char(event_date,''yyyy-mm-dd hh24:mi:ss.us-tz'') = to_char(p_event_date,''yyyy-mm-dd hh24:mi:ss.us-tz'');
 
     return 0;
 
