829e1f3e209af69679448fe4a6f8bc8c2ecd4d50
Eric Lorenzo <eric@openforce.net>
2002-10-24 21:34:08 +0000

    Fixed bad primary key on host_node_map table.  Note: Fixes to Oracle data model have not been tested.

--- packages/acs-subsite/sql/postgresql/host-node-map-create.sql
+++ packages/acs-subsite/sql/postgresql/host-node-map-create.sql
@@ -1,8 +1,10 @@
 -- @author Mark Dettinger (mdettinger@arsdigita.com)
+-- @author Eric Lorenzo (eric@openforce.net)
 
 create table host_node_map (
-   host                 varchar(200),
-   node_id              integer 
-			constraint host_node_map_pk primary key
-			constraint host_node_map_fk references acs_objects (object_id)
+   host         varchar(200)
+                constraint host_node_map_pk primary key,
+   node_id      integer not null
+                constraint host_node_map_fk
+                           references acs_objects (object_id)
 );
