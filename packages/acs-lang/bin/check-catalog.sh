9b04aa5465ae4c8c2570ac2e8d546eb99a31ed76
Peter Marklund <peter_marklund@fastmail.fm>
2002-10-25 14:40:39 +0000

    the catalog check script wasn't checking adp lookups properly, typo, improved indentation

--- packages/acs-lang/bin/check-catalog.sh
+++ packages/acs-lang/bin/check-catalog.sh
@@ -19,10 +19,31 @@ package_path="${script_path}/../../${package_key}"
 cd $package_path
 
 # Check that all keys in the catalog file are either in tcl or adp or info files
-for catalog_key in $(${script_path}/mygrep '<msg key="([^"]+)"' catalog/${package_key}.en_US.ISO-8859-1.xml); do find -iname '*.tcl'|xargs ${script_path}/mygrep "(?ms)\[_\s+(?:\[ad_conn locale\]\s+)?\"?${package_key}\.$catalog_key\"?" || find -regex '.*\.\(info\|adp\)'|xargs ${script_path}/mygrep "#${package_key}\.[a-zA-Z0-9_\.]+#" || echo "Warning key $catalog_key in catalog file not found in any adp or tcl file"; done
+for catalog_key in $(${script_path}/mygrep '<msg key="([^"]+)"' catalog/${package_key}.en_US.ISO-8859-1.xml) 
+do 
+  find -iname '*.tcl' | xargs ${script_path}/mygrep \
+    "(?ms)\[_\s+(?:\[ad_conn locale\]\s+)?\"?${package_key}\.$catalog_key\"?" \
+    || \
+  find -regex '.*\.\(info\|adp\)' | xargs ${script_path}/mygrep \
+    "#${package_key}\.$catalog_key#" \
+    || \
+  echo "Warning key $catalog_key in catalog file not found in any adp or tcl file"
+done
 
 # Check that all message lookups in tcl files have entries in the message catalog
-for tcl_message_key in $(find -iname '*.tcl'|xargs ${script_path}/mygrep '(?ms)\[_\s+(?:\[ad_conn locale\]\s+)?"?${package_key}\.([a-zA-Z0-9_\.]+)"?'); do grep -L $tcl_message_key catalog/${package_key}.en_US.ISO-8859-1.xml || echo "Warning: key $tcl_message_key not in catalog file"; done
+for tcl_message_key in $(find -iname '*.tcl'|xargs ${script_path}/mygrep \
+                         '(?ms)\[_\s+(?:\[ad_conn locale\]\s+)?"?${package_key}\.([a-zA-Z0-9_\.]+)"?')
+do 
+    grep -L $tcl_message_key catalog/${package_key}.en_US.ISO-8859-1.xml \
+      || \
+    echo "Warning: key $tcl_message_key not in catalog file" 
+done
 
 # Check that all message lookups in adp and info files are in the catalog file
-for adp_message_key in $(find -regex '.*\.\(info\|adp\)'|xargs ${script_path}/mygrep '#${package_key}\.([a-zA-Z0-9_\.]+)#'); do grep -L $adp_message_key catalog/${package_key}.en_US.ISO-8859-1.xml || echo "Warning: key $adp_message_key not in catalog file"; done
+for adp_message_key in $(find -regex '.*\.\(info\|adp\)'|xargs ${script_path}/mygrep \
+                        '#${package_key}\.([a-zA-Z0-9_\.]+)#')
+do 
+    grep -L $adp_message_key catalog/${package_key}.en_US.ISO-8859-1.xml \
+      || \
+    echo "Warning: key $adp_message_key not in catalog file"
+done
