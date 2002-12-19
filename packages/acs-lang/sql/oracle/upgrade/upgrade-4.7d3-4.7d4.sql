fef7e84cd3f0d68503f70ed76120b9e20b8fd1a3
Peter Marklund <peter_marklund@fastmail.fm>
2002-12-19 15:30:01 +0000

    message catalog upgrade support - adding upgrade_status columns to tables lang_messages and lang_message_keys

---
+++ packages/acs-lang/sql/oracle/upgrade/upgrade-4.7d3-4.7d4.sql
@@ -0,0 +1,9 @@
+alter table lang_message_keys add 
+          upgrade_status varchar2(30)
+                         constraint lang_message_keys_us_ck
+                         check (upgrade_status in ('no_upgrade', 'added','deleted'));
+
+alter table lang_messages add
+    upgrade_status     varchar2(30)
+                       constraint lang_messages_us_ck
+                       check (upgrade_status in ('no_upgrade', 'added', 'deleted', 'updated'));
