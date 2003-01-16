b5b3786aeb4cae1a13fa71878ed677533ab98d8b
Jeff Davis <davis@xarg.net>
2003-01-16 12:17:34 +0000

    merge changes for context id and default of instance name to pretty name from package info

--- packages/acs-subsite/www/admin/site-map/package-new.tcl
+++ packages/acs-subsite/www/admin/site-map/package-new.tcl
@@ -9,7 +9,7 @@ ad_page_contract {
 } {
     new_package_id:integer,notnull
     node_id:integer,notnull
-    instance_name:notnull
+    {instance_name ""}
     package_key:notnull
     {expand:integer,multiple ""}
     root_id:integer,optional
@@ -20,7 +20,21 @@ if { [string equal $package_key "/new"] } {
     ad_script_abort
 }
 
-set context_id [ad_conn package_id]
+set context_id [db_string context_id {
+    select parent.object_id as context_id
+    from   site_nodes parent, site_nodes child
+    where  child.node_id = :node_id
+    and    parent.node_id = child.parent_id
+}]
+
+# If the parent node didn't have anything mounted, use the current package_id as context_id
+if { [empty_string_p $context_id] } {
+    set context_id [ad_conn package_id]
+}
+
+if { [empty_string_p $instance_name] } {
+        set instance_name [db_string instance_default_name "select pretty_name from apm_package_types where package_key = :package_key"]
+}
 
 db_transaction {
     set package_id [site_node_create_package_instance -package_id $new_package_id $node_id $instance_name $context_id $package_key]
