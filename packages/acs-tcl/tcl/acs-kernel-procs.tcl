e2f5f104c97ad41eb036bb5d0166787594531d25
Peter Marklund <peter_marklund@fastmail.fm>
2003-01-30 16:12:19 +0000

    As part of moving mounting of core packages from installer to bootstrapper I forgot to commit these changes to ad_verify_install. The proc should no longer check that acs-admin is mounted.

--- packages/acs-tcl/tcl/acs-kernel-procs.tcl
+++ packages/acs-tcl/tcl/acs-kernel-procs.tcl
@@ -49,18 +49,13 @@ ad_proc -public ad_verify_install {} {
 	return 0
     }
     set kernel_install_p [apm_package_installed_p acs-kernel] 
-    set admin_node_p [ad_acs_admin_node] 
-    set admin_exists_p [ad_acs_administrator_exists_p]
 
-    ns_log Debug "Verifying Installation: Kernel Installed? $kernel_install_p \
-	    ACS Administration Link Id: $admin_node_p  An Administrator? $admin_exists_p"
+    ns_log Debug "Verifying Installation: Kernel Installed? $kernel_install_p"
 
-    if { $kernel_install_p && $admin_node_p && $admin_exists_p} {
+    if { $kernel_install_p } {
 	return 1 
     } else {
 	ad_proc util_memoize {script {max_age ""}} {no cache} {eval $script}
 	return 0
     }
 }
-
-
