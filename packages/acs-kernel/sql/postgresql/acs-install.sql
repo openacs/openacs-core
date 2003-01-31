c44e8251b203f590994d04ce0f94ae88017fceda
Peter Marklund <peter_marklund@fastmail.fm>
2003-01-31 16:18:27 +0000

    acs-install.sql doesn't need to instantiate acs-kernel as this is done during install of that package (since it's a singleton)

--- packages/acs-kernel/sql/postgresql/acs-install.sql
+++ packages/acs-kernel/sql/postgresql/acs-install.sql
@@ -1,9 +1,9 @@
 --
 -- /packages/acs-kernel/sql/acs-install.sql
 --
--- Complete the install of the system by setting up some default URL mappings.
+-- Mount the main site.
 --
--- @author Bryan Quinn (bquinn@arsdigita.com
+-- @author Peter Marklund
 -- @creation-date 2000/10/01
 -- @cvs-id acs-install.sql,v 1.9.2.1 2001/01/12 18:32:21 dennis Exp
 --
@@ -11,31 +11,9 @@
 create function inline_0 ()
 returns integer as '
 declare
-    kernel_id           apm_packages.package_id%TYPE;
     node_id             site_nodes.node_id%TYPE;
     main_site_id        site_nodes.node_id%TYPE;
-    admin_id            apm_packages.package_id%TYPE;
-    lang_id             apm_packages.package_id%TYPE;
-    docs_id             apm_packages.package_id%TYPE;
-    api_doc_id          apm_packages.package_id%TYPE;
-    acs_sc_id		apm_packages.package_id%TYPE;
-    cr_id		apm_packages.package_id%TYPE;
-    schema_user         varchar(100);
-    jobnum              integer;
 begin   
-
-  kernel_id := apm_service__new (
-                    null,
-                    ''ACS Kernel'',
-                    ''acs-kernel'',
-                    ''apm_service'',
-                    now(),
-                    null,
-                    null,
-                    acs__magic_object_id(''default_context'')
-                    );
-
-  PERFORM apm_package__enable (kernel_id);
 
   main_site_id := apm_service__new(
                     null,
