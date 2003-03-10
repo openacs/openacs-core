fa4fab5573c425e600ade5c523d182b27dfa86e5
Peter Marklund <peter_marklund@fastmail.fm>
2003-03-10 12:39:23 +0000

    changing the reload page to reflect that files are no longer stored in the db

--- packages/acs-admin/www/apm/version-reload.tcl
+++ packages/acs-admin/www/apm/version-reload.tcl
@@ -15,24 +15,21 @@ doc_body_append "[apm_header "Reload a Package"]
 # files in $files.
 apm_mark_version_for_reload $version_id files
 
-set file_id_list [list]
+set files_to_watch [list]
 
 if { [llength $files] == 0 } {
     doc_body_append "There are no changed files to reload in this package.<p>"
 } else {
     doc_body_append "Marked the following file[ad_decode [llength $files] 1 "" "s"] for reloading:<ul>\n"
-    foreach info $files {
-	set file_id [lindex $info 0]
-	set file [lindex $info 2]
-	set path [lindex $info 1]
-	doc_body_append "<li>$path"
+    foreach file $files {
+	doc_body_append "<li>$file"
 	if { [nsv_exists apm_reload_watch $file] } {
 	    doc_body_append " (currently being watched)"
 	} else {
 	    # This file isn't being watched right now - provide a link setting a watch on it.
 	    set files_to_watch_p 1
-	    doc_body_append " (<a href=\"file-watch?[export_vars { file_id }]\">watch this file</a>)"
-            lappend file_id_list $file_id
+	    doc_body_append " (<a href=\"file-watch?[export_vars { version_id { paths $file } }]\">watch this file</a>)"
+            lappend files_to_watch $file
 	}
 	doc_body_append "\n"
     }
@@ -43,7 +40,7 @@ if { [info exists files_to_watch_p] } {
     doc_body_append "If you know you're going to be modifying one of the above files frequently,
     select the \"watch this file\" link next to a filename to cause the interpreters to
     reload the file immediately whenever it is changed.<p>
-    (<a href=\"file-watch?[export_vars { { file_id:multiple $file_id_list } }]\">watch all above files</a>)
+    (<a href=\"file-watch?[export_vars { version_id { paths:multiple $files_to_watch } }]\">watch all above files</a>)
     <p>
 "
 }
@@ -52,4 +49,3 @@ doc_body_append "
 <a href=\"index\">Return to the Package Manager</a>
 [ad_footer]
 "
-
