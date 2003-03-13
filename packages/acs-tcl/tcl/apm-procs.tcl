78d02918a3e12739d09ce27e0180acb5b1f6e4a2
Peter Marklund <peter_marklund@fastmail.fm>
2003-03-13 18:19:30 +0000

    rephrasing log statement to be able to close bug 222

--- packages/acs-tcl/tcl/apm-procs.tcl
+++ packages/acs-tcl/tcl/apm-procs.tcl
@@ -453,7 +453,7 @@ ad_proc -private apm_load_queries {
             } 
         }
     }
-    ns_log Notice "APM/QD = DONE looping through files to load queries from"
+    ns_log Notice "APM/QD = DONE looping through files from which to load queries"
 }
 
 ad_proc -private apm_subdirs { path } {
