3228524e5cafc3ea01fd04b5f632b85ebc19e97f
Tilmann Singer <tils@tils.net>
2003-03-31 14:45:26 +0000

    Cosmetic improvement: only display the warning about parameters set in config files when there are such parameters set.

--- packages/acs-subsite/www/admin/site-map/parameter-set.tcl
+++ packages/acs-subsite/www/admin/site-map/parameter-set.tcl
@@ -39,10 +39,14 @@ if {[exists_and_not_null dimensional_list] } {
 # DRB: This should be rewritten to optionally allow for the definition of possible parameter values,
 # with a drop-down select widget used rather than a text input widget. 
 
+# TIL: only show the from-file-parameter-warning when there are
+# actually parameters from a file in this listing.
+set display_warning_p 0
+
 lappend table_def [list attr_value "Value" no_sort \
 	{<td>
 	   <input name=params.$parameter_id value=\"$attr_value\" size=50>
-           <font color=red><strong>[ad_parameter_from_file $parameter_name [uplevel set package_key]]</strong></font>
+	    <font color=red><strong>[if { ![empty_string_p [ad_parameter_from_file $parameter_name [uplevel set package_key]]] } { uplevel set display_warning_p 1 } ; ad_parameter_from_file $parameter_name [uplevel set package_key]]</strong></font>
 	    </td>}]
 
 append additional_sql [ad_order_by_from_sort_spec $orderby $table_def]
@@ -50,12 +54,6 @@ append additional_sql [ad_order_by_from_sort_spec $orderby $table_def]
 set body "[ad_header "Parameters for $instance_name"]
 <h2>Parameters for $instance_name</h2>
 [ad_context_bar [list "index" "Site Map"] "$instance_name Parameters"]
-<hr>
-Note text in red below the parameter entry fields indicates the value of this
-parameter is being overridden by an entry in the OpenACS parameter file.  The
-use of the parameter file is discouraged but some sites need it to provide
-instance-specific values for parameters independent of the apm_parameter
-tables.
 <hr>
 "
 
@@ -74,6 +72,19 @@ set table [ad_table -Torderby $orderby \
 <td></td><td></td>
 <td><input type=submit value=\"Set Parameters\">
 </td></tr>" parameter_table $table_sql $table_def]
+
+
+if { $display_warning_p } {
+    append body "
+Note text in red below the parameter entry fields indicates the value of this
+parameter is being overridden by an entry in the OpenACS parameter file.  The
+use of the parameter file is discouraged but some sites need it to provide
+instance-specific values for parameters independent of the apm_parameter
+tables.
+<hr>
+"
+}
+
 
 ns_return 200 text/html "$body
 <blockquote>
