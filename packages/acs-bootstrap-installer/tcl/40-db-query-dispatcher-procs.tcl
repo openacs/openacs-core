b76d3abfff632c27a4a59166e0fd6cc466b21da1
Peter Marklund <peter_marklund@fastmail.fm>
2003-03-31 17:21:45 +0000

    making the query *NOT* compatible error message more informative by logging the db version of the query as well as the system db version

--- packages/acs-bootstrap-installer/tcl/40-db-query-dispatcher-procs.tcl
+++ packages/acs-bootstrap-installer/tcl/40-db-query-dispatcher-procs.tcl
@@ -567,7 +567,7 @@ ad_proc db_qd_internal_store_cache {fullquery} {
 
     # Check if it's compatible at all!
     if {![db_rdbms_compatible_p [db_fullquery_get_rdbms $fullquery] [db_current_rdbms]]} {
-	ns_log Error "Query [db_fullquery_get_name $fullquery] is *NOT* compatible"
+	ns_log Error "Query [db_fullquery_get_name $fullquery] has an rdbms of [db_fullquery_get_rdbms $fullquery] which is *NOT* compatible with system rdbms [db_current_rdbms]"
 	return
     }
 
