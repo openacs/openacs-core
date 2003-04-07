625599396a548369d8f1241bc4f8fd89eba835cb
Tilmann Singer <tils@tils.net>
2003-04-07 06:23:07 +0000

    Make instantiate_and_mount work with package names that start with a dash.

--- packages/acs-tcl/tcl/site-nodes-procs.tcl
+++ packages/acs-tcl/tcl/site-nodes-procs.tcl
@@ -108,7 +108,7 @@ namespace eval site_node {
         }
 
         # Instantiate the package
-        set package_id [apm_package_instance_new $package_name $context_id $package_key]
+        set package_id [apm_package_instance_new -- $package_name $context_id $package_key]
 
         # Mount the package
         site_node::mount -node_id $node_id -object_id $package_id
