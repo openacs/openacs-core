1fb9346a5f094856366452ed73a7ad993592b322
Peter Marklund <peter_marklund@fastmail.fm>
2003-06-24 12:31:48 +0000

    adding new locales from the translation server

--- packages/acs-lang/sql/oracle/ad-locales.sql
+++ packages/acs-lang/sql/oracle/ad-locales.sql
@@ -173,6 +173,46 @@ insert into ad_locales
         nls_charset, mime_charset, default_p)
  values ('TH_TH', 'Thai (TH)', 'th', 'TH', 'THAI', 'THAILAND', 'TH8TISASCII', 'TIS-620', 't');
 
+insert into ad_locales 
+       (locale, label, language, country, nls_language, nls_territory, 
+        nls_charset, mime_charset, default_p)
+ values ('gl_ES', 'Galizan-Portuguese (gl_ES)', 'gl ', 'ES', 'SPANISH', 'SPAIN', 'WE8ISO8859P1', 'ISO-8859-1', 't');
+
+insert into ad_locales 
+       (locale, label, language, country, nls_language, nls_territory, 
+        nls_charset, mime_charset, default_p)
+ values ('ast_ES', 'Asturian (ast_ES)', 'as ', 'ES', 'SPANISH', 'SPAIN', 'WE8ISO8859P1', 'ISO-8859-1', 't');
+
+insert into ad_locales 
+       (locale, label, language, country, nls_language, nls_territory, 
+        nls_charset, mime_charset, default_p)
+ values ('AR_EG', 'Arabic (AR_EG)', 'AR ', 'EG', 'ARABIC', 'EGYPT', 'AR8ISO8859P6', 'ISO8859-6', 't');
+
+insert into ad_locales 
+       (locale, label, language, country, nls_language, nls_territory, 
+        nls_charset, mime_charset, default_p)
+ values ('tr_TR', 'Turkish (TR)', 'tr ', 'TR', 'TURKISH', 'TURKEY', 'WE8ISO8859P9', 'ISO-8859-9', 't');
+
+insert into ad_locales 
+       (locale, label, language, country, nls_language, nls_territory, 
+        nls_charset, mime_charset, default_p)
+ values ('ms_my', 'Malaysia (MY)', 'ms ', 'MY', 'MALAY', 'MALAYSIA', 'US7ASCII', 'US-ASCII', 't');
+
+insert into ad_locales 
+       (locale, label, language, country, nls_language, nls_territory, 
+        nls_charset, mime_charset, default_p)
+ values ('hi_IN', 'Hindi (IN)', 'hi ', 'IN', 'HINDI', 'INDIA', 'UTF8', 'UTF-8', 't');
+
+insert into ad_locales 
+       (locale, label, language, country, nls_language, nls_territory, 
+        nls_charset, mime_charset, default_p)
+ values ('ko_KR', 'Korean(KOR)', 'ko ', 'KR', 'KOREAN', 'KOREA', 'KO16KSC5601', 'EUC-KR', 't');
+
+insert into ad_locales 
+       (locale, label, language, country, nls_language, nls_territory, 
+        nls_charset, mime_charset, default_p)
+ values ('zh_TW', 'Chinese (TW)', 'zh ', 'TW', 'TRADITIONAL CHINESE', 'TAIWAN', 'ZHT16BIG5', 'Big5', 't');
+
 create table ad_locale_user_prefs (
   user_id               integer
                         constraint ad_locale_user_prefs_users_fk
