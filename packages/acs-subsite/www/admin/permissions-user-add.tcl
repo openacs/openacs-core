5835f1ea3437af0b0686fadb1e1d7d6ce13b56a1
Lars Pind <lars@pinds.com>
2003-07-04 12:56:13 +0000

    Added permissions page to easily set permissions for the subsite

---
+++ packages/acs-subsite/www/admin/permissions-user-add.tcl
@@ -0,0 +1,12 @@
+ad_page_contract {
+    Redirect page for adding users to the permissions list.
+    
+    @author Lars Pind (lars@collaboraid.biz)
+    @creation-date 2003-06-13
+    @cvs-id $Id$
+}
+
+set page_title "Add User"
+
+set context [list [list "permissions" "Permissions"] $page_title]
+
