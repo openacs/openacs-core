fe72afcfd11cc7f4515736d1f9ded896c5797338
Lars Pind <lars@pinds.com>
2003-07-04 12:53:50 +0000

    Added parameters page and configure page

---
+++ packages/acs-subsite/www/admin/configure.tcl
@@ -0,0 +1,12 @@
+ad_page_contract {
+    Configuration home page.
+    
+    @author Lars Pind (lars@collaboraid.biz)
+    @creation-date 2003-06-13
+    @cvs-id $Id$
+}
+
+set page_title "[ad_conn instance_name] Configuration"
+
+set context [list "Configuration"]
+
