527c0b7385f8f2e6085207d2fbe39bd0d797f99f
Simon Carstensen <simon@bcuni.net>
2003-08-05 13:07:06 +0000

    changed string range from 100 to 99

--- packages/notifications/tcl/notification-reply-procs.tcl
+++ packages/notifications/tcl/notification-reply-procs.tcl
@@ -27,7 +27,7 @@ namespace eval notification::reply {
         set extra_vars [ns_set create]
 
         # Truncate subject to 100 chars, which is the limit in the data model (for some obscure reason)
-        set subject [string range $subject 0 100]
+        set subject [string range $subject 0 99]
         
         oacs_util::vars_to_ns_set -ns_set $extra_vars -var_list {reply_id object_id type_id from_user subject content reply_date}
         
