108a6d67826fe971921bf7cdcebd97b99844d318
Dave Bauer <dave@thedesignexperience.org>
2003-08-16 23:22:14 +0000

    Fix paths to ETP templates which are now outside the pageroot.

--- tcl/openacsorg-init.tcl
+++ tcl/openacsorg-init.tcl
@@ -10,40 +10,40 @@ ad_library {
 
 
 etp::modify_application default {
-    index_template        www/templates/default-index
-    content_template      www/templates/default-content
+    index_template        templates/default-index
+    content_template      templates/default-content
 }    
 
 etp::define_application toplevel {
-    index_template        www/templates/homepage-index
+    index_template        templates/homepage-index
 }
 
 etp::define_application site-node {
-    index_template        www/templates/site-node
+    index_template        templates/site-node
 }
 
 # new design (olah)
 etp::define_application homepage-new {
-    index_template        www/templates/homepage-new-index
+    index_template        templates/homepage-new-index
 }
 
 etp::define_application community {
-    index_template        www/templates/community-index
+    index_template        templates/community-index
     content_content_attr_name "Special Events HTML"
 }
 
 # new design (olah)
 etp::define_application community-new {
-    index_template        www/templates/community-new-index
+    index_template        templates/community-new-index
     content_content_attr_name "Spec Events HTML"
 }
 
 etp::modify_application news {
-    index_template        www/templates/news-index
-    content_template      www/templates/news-content
+    index_template        templates/news-index
+    content_template      templates/news-content
 }    
 
 etp::modify_application faq {
-    index_template        www/templates/faq-index
-    content_template      www/templates/faq-content
+    index_template        templates/faq-index
+    content_template      templates/faq-content
 }    
