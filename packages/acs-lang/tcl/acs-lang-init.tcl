7f2f68dcb29bf631f6363d1530e03413c9cec19e
Lars Pind <lars@pinds.com>
2003-08-18 08:10:21 +0000

    Added queries to deal with descriptions

--- packages/acs-lang/tcl/acs-lang-init.tcl
+++ packages/acs-lang/tcl/acs-lang-init.tcl
@@ -6,12 +6,6 @@ ad_library {
     @cvs-id $Id$
 }
 
-# Load message catalog from the database to memory
-
-lang::message::cache
-
-
-
 # Load message catalog files from packages that don't have messages in the database already
 # This is done in a scheduled proc so that it won't take up time at server startup.
 # Instead, it can be done by a thread after the server has started multithreading.
@@ -20,3 +14,7 @@ lang::message::cache
 # PETER: Do we want this at all?
 
 ad_schedule_proc -once t 5 lang::catalog::import_from_all_files_and_cache
+
+# Load message catalog from the database to memory
+
+lang::message::cache
--- packages/acs-lang/tcl/lang-message-procs-postgresql.xql
+++ packages/acs-lang/tcl/lang-message-procs-postgresql.xql
@@ -1,3 +1,4 @@
+
 <?xml version="1.0"?>
 <queryset>
   <rdbms><type>postgresql</type><version>7.2</version></rdbms>
@@ -19,5 +20,14 @@
        and    package_key = :package_key
      </querytext>
  </fullquery>
+
+  <fullquery name="lang::message::update_description.update_description">
+    <querytext>
+      update lang_message_keys
+      set    description = :description
+      where  message_key = :message_key
+      and    package_key = :package_key
+    </querytext>
+  </fullquery>
 
 </queryset>
