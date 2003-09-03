b9f2cfc80eb9843aab6c3c1f057796ec15724ce7
Lars Pind <lars@pinds.com>
2003-09-03 19:52:38 +0000

    Just an indentation fix

--- packages/acs-kernel/sql/postgresql/authentication-create.sql
+++ packages/acs-kernel/sql/postgresql/authentication-create.sql
@@ -75,21 +75,23 @@ select acs_object_type__create_type (
 );
 
 create table auth_driver_params(
-      authority_id    integer
-                      constraint auth_driver_params_aid_fk 
-                      references auth_authorities(authority_id)
-                      constraint auth_driver_params_aid_nn
-                      not null,
-      impl_id         integer
-                      constraint auth_driver_params_iid_fk
-                      -- Cannot reference acs_sc_impls table as it doesn't exist yet
-                      references acs_objects(object_id)
-                      constraint auth_driver_params_iid_nn
-                      not null,
-      key             varchar(200),
-      value           text,
-      unique (authority_id, impl_id)
+    authority_id             integer
+                             constraint auth_driver_params_aid_fk 
+                             references auth_authorities(authority_id)
+                             constraint auth_driver_params_aid_nn
+                             not null,
+    impl_id                  integer
+                             constraint auth_driver_params_iid_fk
+                             -- Cannot reference acs_sc_impls table as it doesn't exist yet
+                             references acs_objects(object_id)
+                             constraint auth_driver_params_iid_nn
+                             not null,
+    key                      varchar(200),
+    value                    text,
+    constraint auth_driver_params_pk
+    primary key (authority_id, impl_id, key)
 );
+
 
 -- Create PLSQL package
 \i authentication-package-create.sql
