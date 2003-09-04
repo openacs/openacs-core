23a69bfaaeb7e820d4be7b53e39e86f49c04ceba
Lars Pind <lars@pinds.com>
2003-09-04 15:35:48 +0000

    Added context bar, cleaned up page

--- packages/acs-admin/www/apm/version-i18n-export.tcl
+++ packages/acs-admin/www/apm/version-i18n-export.tcl
@@ -6,16 +6,14 @@ ad_page_contract {
     @cvs-id $Id$  
 } {
     version_id:integer
-    {return_url ""}
+    {return_url {[export_vars -base "version-i18n-index" { version_id }]}}
 }
 
-set page_title "Message Export Results"
+set package_key [apm_package_key_from_version_id $version_id]
 
-# Default return_url
-if { [empty_string_p $return_url] } {
-    set return_url "version-i18n-index?[export_vars { version_id }]"
-}
+set page_title "$package_key Messagess Exported"
+set context [list $page_title]
 
-set catalog_dir [lang::catalog::package_catalog_dir [apm_package_key_from_version_id $version_id]]
+set catalog_dir [lang::catalog::package_catalog_dir $package_key]
 
 lang::catalog::export_package_to_files [apm_package_key_from_version_id $version_id]
