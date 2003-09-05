5faba846091013522c6d5a262173bcb679828a79
Lars Pind <lars@pinds.com>
2003-09-05 12:48:44 +0000

    Removed this file, it was empty

--- packages/acs-lang/tcl/test/acs-lang-test-init.tcl
+++
@@ -1,7 +0,0 @@
-ad_library {
-    acs-automated-testing test definitions to be sourced on server startup.
-
-    @author Peter Marklund (peter@collaboraid.biz)
-    @creation-date 18 October 2002
-}
-
