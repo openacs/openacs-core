b6d443d0c53ea310702b4efce7f52e241877854d
Lars Pind <lars@pinds.com>
2003-09-09 09:32:06 +0000

    - Added link to acs-service-contract - Made all the links conditional on the package being mounted - Removed link to acs-admin/test

--- packages/acs-admin/lib/site-wide-services.tcl
+++ packages/acs-admin/lib/site-wide-services.tcl
@@ -2,5 +2,7 @@ set acs_admin_url [apm_package_url_from_key "acs-admin"]
 
 set acs_automated_testing_url [apm_package_url_from_key "acs-automated-testing"]
 
+set acs_service_contract_url [apm_package_url_from_key "acs-service-contract"]
+
 set acs_lang_admin_url "[apm_package_url_from_key "acs-lang"]admin/"
 
