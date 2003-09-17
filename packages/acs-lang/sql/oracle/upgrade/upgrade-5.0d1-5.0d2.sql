add9a1ceeafe817de709461c4747b65fc793e835
Peter Marklund <peter_marklund@fastmail.fm>
2003-09-17 14:14:44 +0000

    adding Arabic Lebanese locale from translation server

---
+++ packages/acs-lang/sql/oracle/upgrade/upgrade-5.0d1-5.0d2.sql
@@ -0,0 +1,4 @@
+insert into ad_locales 
+       (locale, label, language, country, nls_language, nls_territory, 
+        nls_charset, mime_charset, default_p, enabled_p)
+ values ('AR_LB', 'Arabic (AR_LB)', 'AR ', 'LB', 'ARABIC', 'LEBANON', 'AR8ISO8859P6', 'ISO8859-6', 't', 'f');
