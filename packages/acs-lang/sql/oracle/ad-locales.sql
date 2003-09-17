e671fd48455aeacd5f6d574984173d10936e02c3
Peter Marklund <peter_marklund@fastmail.fm>
2003-09-17 14:14:44 +0000

    adding Arabic Lebanese locale from translation server

--- packages/acs-lang/sql/oracle/ad-locales.sql
+++ packages/acs-lang/sql/oracle/ad-locales.sql
@@ -192,6 +192,11 @@ insert into ad_locales
 insert into ad_locales 
        (locale, label, language, country, nls_language, nls_territory, 
         nls_charset, mime_charset, default_p, enabled_p)
+ values ('AR_LB', 'Arabic (AR_LB)', 'AR ', 'LB', 'ARABIC', 'LEBANON', 'AR8ISO8859P6', 'ISO8859-6', 't', 'f');
+
+insert into ad_locales 
+       (locale, label, language, country, nls_language, nls_territory, 
+        nls_charset, mime_charset, default_p, enabled_p)
  values ('tr_TR', 'Turkish (TR)', 'tr ', 'TR', 'TURKISH', 'TURKEY', 'WE8ISO8859P9', 'ISO-8859-9', 't', 'f');
 
 insert into ad_locales 
