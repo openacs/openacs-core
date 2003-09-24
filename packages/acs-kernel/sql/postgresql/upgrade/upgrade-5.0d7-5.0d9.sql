debaa8d3d117c8f0bd7f48a0b580eece7e3888fa
Peter Marklund <peter_marklund@fastmail.fm>
2003-09-24 12:20:28 +0000

    Upgrading kernel to 5.0d9. Fixing bug 555 - Removing apm_packages.enabled_p column and taking the opportunity to remove obsolete file related datamodel

---
+++ packages/acs-kernel/sql/postgresql/upgrade/upgrade-5.0d7-5.0d9.sql
@@ -0,0 +1,15 @@
+-- Remove obsolete parts of the APM datamodel not used
+--
+-- @author Peter Marklund
+
+-- *** Remove a column not needed
+-- See http://openacs.org/bugtracker/openacs/bug?bug%5fnumber=555
+alter table apm_packages drop enabled_p;
+
+-- *** Get rid of file-related data no longer used
+drop table apm_package_file_types cascade;
+drop table apm_package_files cascade;
+-- View was dropped by previous drop
+--drop view apm_file_info;
+drop function apm_package_version__add_file (integer,integer,varchar,varchar, varchar);
+drop function apm_package_version__remove_file (integer,varchar);
