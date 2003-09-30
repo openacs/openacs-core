56f6d676a913285951fe48b9dbda4c7f5103889d
Lars Pind <lars@pinds.com>
2003-09-30 10:08:00 +0000

    Added page for testing translator mode

---
+++ packages/acs-lang/www/admin/test/translator-mode.adp
@@ -0,0 +1,26 @@
+<master>
+
+<p>
+  Tcl message: @tcl_message@
+</p>
+
+<p>
+  ADP Message: #acs-lang.Spanish#
+</p>
+
+<p>
+  Form:
+  <formtemplate id="test"></formtemplate>
+</p>
+
+<p>
+  In an HTML attribute: <a href="#" title="#acs-lang.English#">#acs-lang.Spanish#</a>
+</p>
+
+<p>
+  Two HTML attributes: <a href="#" title="#acs-lang.English#" alt="#acs-lang.German#">#acs-lang.Spanish#</a>
+</p>
+
+<p>
+  Message inside a message: @complete_message@
+</p>
---
+++ packages/acs-lang/www/admin/test/translator-mode.tcl
@@ -0,0 +1,37 @@
+ad_page_contract {
+    Testing translator mode.
+}
+
+set tcl_message [_ acs-lang.French]
+
+set options [list]
+
+foreach elm { English French German Spanish } {
+    lappend options [list [_ acs-lang.$elm] $elm]
+}
+
+ad_form -name test -form {
+    {lang:text(select)
+        {label Language}
+        {options $options}
+    }
+    {lang2:text(select)
+        {label Language}
+        {options $options}
+    }
+}
+
+lang::message::register \
+    en_US \
+    acs-lang \
+    Test_Contained_Message \
+    "Contains message %contained_message% inside of it."
+
+lang::message::register \
+    [ad_conn locale] \
+    acs-lang \
+    Test_Contained_Message \
+    "Contains message %contained_message% inside of it."
+
+set contained_message [_ acs-lang.German]
+set complete_message [_ acs-lang.Test_Contained_Message]
