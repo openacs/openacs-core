b8c857880d0b4fc569388c82482e07fc1dcb4236
Lars Pind <lars@pinds.com>
2003-10-02 21:18:11 +0000

    Show # members of subsite

--- packages/acs-subsite/lib/subsites.tcl
+++ packages/acs-subsite/lib/subsites.tcl
@@ -14,27 +14,6 @@ if { $admin_p } {
 }
 
 
-# Get the subsite node ID
-set subsite_url [site_node_closest_ancestor_package_url]
-array set subsite_sitenode [site_node::get -url $subsite_url]
-set subsite_node_id $subsite_sitenode(node_id)
-
-multirow create subsites node_id name package_id instance_name read_p
-
-foreach url [site_node::get_children -package_type apm_service -node_id $subsite_node_id] {
-    array unset node 
-    array set node [site_node::get_from_url -url $url -exact]
-
-    if { [string equal $node(package_key) "acs-subsite"] } { 
-        multirow append subsites \
-            $node(node_id) \
-            $node(name) \
-            $node(object_id) \
-            $node(instance_name) \
-            [permission::permission_p -object_id $node(object_id) -privilege read]
-    }
-}
-
 list::create \
     -name subsites \
     -multirow subsites \
@@ -44,7 +23,69 @@ list::create \
             label "Name"
             link_url_eval {$name/}
         }
+        num_members {
+            label "\# Members"
+            html { align right }
+        }
     }
 
 
+
+# Get the subsite node ID
+set subsite_url [site_node_closest_ancestor_package_url]
+array set subsite_sitenode [site_node::get -url $subsite_url]
+set subsite_node_id $subsite_sitenode(node_id)
+
+set subsites [list]
+set package_ids [list]
+
+foreach url [site_node::get_children -package_type apm_service -node_id $subsite_node_id] {
+    array unset node 
+    array set node [site_node::get_from_url -url $url -exact]
+
+    if { [string equal $node(package_key) "acs-subsite"] } { 
+        lappend subsites [list \
+                              $node(instance_name) \
+                              $node(node_id) \
+                              $node(name) \
+                              $node(object_id) \
+                              [permission::permission_p -object_id $node(object_id) -privilege read]]
+        lappend package_ids $node(object_id)
+    }
+}
+
+array set num_members [list]
+if { [llength $package_ids] > 0 } { 
+    db_foreach num_members "
+        select ag.package_id,
+               count(member_id) as n_members
+        from   application_groups ag,
+               group_approved_member_map m
+        where  ag.package_id in ('[join $package_ids "','"]')
+        and    m.group_id = ag.group_id
+        group  by ag.package_id
+    " {
+        set num_members($package_id) [lc_numeric $n_members]
+    }
+}
+
+# Sort them by instance_name
+set subsites [lsort -index 0 $subsites]
+
+multirow create subsites instance_name node_id name package_id read_p num_members
+
+foreach elm $subsites {
+    set package_id [lindex $elm 3]
+    if { ![info exists num_members($package_id)] } {
+        set num_members($package_id) {}
+    }
+
+    multirow append subsites \
+        [lindex $elm 0] \
+        [lindex $elm 1] \
+        [lindex $elm 2] \
+        [lindex $elm 3] \
+        [lindex $elm 4] \
+        $num_members($package_id)
+}
 
