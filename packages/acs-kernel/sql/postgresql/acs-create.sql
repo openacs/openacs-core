4f0371af2b349dfca9fd4b8fac3d629d5e14d0c3
Peter Marklund <peter_marklund@fastmail.fm>
2003-10-06 13:16:11 +0000

    making acs__remove_user do what the Oracle version already does, i.e. remove records referencing the user record before attempting to delete it. Version number bumped to 5.0d12

--- packages/acs-kernel/sql/postgresql/acs-create.sql
+++ packages/acs-kernel/sql/postgresql/acs-create.sql
@@ -107,9 +107,20 @@ create function acs__remove_user (integer)
 returns integer as '
 declare
   remove_user__user_id                alias for $1;  
+  v_rec           record;
 begin
-    delete from users
-    where user_id = remove_user__user_id;
+    delete
+    from acs_permissions
+    where grantee_id = remove_user__user_id;
+
+    for v_rec in select rel_id
+                 from acs_rels
+                 where object_id_two = remove_user__user_id
+    loop
+        perform acs_rel__delete(v_rec.rel_id);
+    end loop;
+
+    perform acs_user__delete(remove_user__user_id);
 
     return 0; 
 end;' language 'plpgsql';
