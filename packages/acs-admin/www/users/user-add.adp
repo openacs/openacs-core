9fbdd2172734eeff58cff7c2fa17a146f8572c1c
Lars Pind <lars@pinds.com>
2003-10-08 09:57:11 +0000

    Fixed focus, removed old vars

--- packages/acs-admin/www/users/user-add.adp
+++ packages/acs-admin/www/users/user-add.adp
@@ -1,6 +1,6 @@
 <master>
 <property name=title>Add a user</property>
-<property name="focus">user.email</property>
+<property name="focus">register.email</property>
 <property name="context">@context;noquote@</property>
 
 <include src="/packages/acs-subsite/lib/user-new" next_url="@next_url;noquote@" self_register_p="0"/>
--- packages/acs-admin/www/users/user-add.tcl
+++ packages/acs-admin/www/users/user-add.tcl
@@ -9,12 +9,6 @@ ad_page_contract {
     export_vars:onevalue
 }
 
-set context [list [list "./" "Users"] "Add user"]
-
-# generate unique key here so we can handle the "user hit s" case
-set user_id [db_nextval acs_object_id_seq]
-set export_vars [export_form_vars user_id]
+set context [list [list "." "Users"] "Add user"]
 
 set next_url user-add-2
-
-ad_return_template
