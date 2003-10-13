38d15b48bb05d4bba1d00329014c7292fe23a672
Lars Pind <lars@pinds.com>
2003-10-13 17:19:25 +0000

    Templated page

--- packages/acs-admin/www/apm/version-reload.tcl
+++ packages/acs-admin/www/apm/version-reload.tcl
@@ -7,10 +7,11 @@ ad_page_contract {
     @cvs-id $Id$
 } {
     {version_id:integer}
+    {return_url "index"}
 }
 
-doc_body_append "[apm_header "Reload a Package"]
-"
+set page_title "Reload a Package"
+set context [list [list "/acs-admin/apm/" "Package Manager"] $page_title]
 
 # files in $files.
 apm_mark_version_for_reload $version_id files
@@ -18,13 +19,13 @@ apm_mark_version_for_reload $version_id files
 set files_to_watch [list]
 
 if { [llength $files] == 0 } {
-    doc_body_append "There are no changed files to reload in this package.<p>"
+    append body "There are no changed files to reload in this package.<p>"
 } else {
-    doc_body_append "Marked the following file[ad_decode [llength $files] 1 "" "s"] for reloading:<ul>\n"
+    append body "Marked the following file[ad_decode [llength $files] 1 "" "s"] for reloading:<ul>\n"
     foreach file $files {
-	doc_body_append "<li>$file"
+	append body "<li>$file"
 	if { [nsv_exists apm_reload_watch $file] } {
-	    doc_body_append " (currently being watched)"
+	    append body " (currently being watched)"
 	} else {
 	    # This file isn't being watched right now - provide a link setting a watch on it.
 	    set files_to_watch_p 1
@@ -32,16 +33,16 @@ if { [llength $files] == 0 } {
             # Remove the two first elements of the path, namely packages/package-key/
             set local_path [eval [concat file join [lrange [file split $file] 2 end]]]
 
-	    doc_body_append " (<a href=\"file-watch?[export_vars { version_id { paths $local_path } }]\">watch this file</a>)"
+	    append body " (<a href=\"file-watch?[export_vars { version_id { paths $local_path } }]\">watch this file</a>)"
             lappend files_to_watch $local_path
 	}
-	doc_body_append "\n"
+	append body "\n"
     }
-    doc_body_append "</ul>\n"
+    append body "</ul>\n"
 }
 
 if { [info exists files_to_watch_p] } {
-    doc_body_append "If you know you're going to be modifying one of the above files frequently,
+    append body "If you know you're going to be modifying one of the above files frequently,
     select the \"watch this file\" link next to a filename to cause the interpreters to
     reload the file immediately whenever it is changed.<p>
     (<a href=\"file-watch?[export_vars { version_id { paths:multiple $files_to_watch } }]\">watch all above files</a>)
@@ -49,7 +50,6 @@ if { [info exists files_to_watch_p] } {
 "
 }
 
-doc_body_append "
-<a href=\"index\">Return to the Package Manager</a>
-[ad_footer]
-"
+append body "
+<p><b>&raquo;</b> <a href=\"$return_url\">Return</a></p>"
+
