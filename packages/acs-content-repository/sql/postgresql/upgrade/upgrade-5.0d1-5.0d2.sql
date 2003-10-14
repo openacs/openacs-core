ca705a0b75cb5b3106761d0b9fe79e954e558cfb
Lars Pind <lars@pinds.com>
2003-10-14 07:30:43 +0000

    Added PG define_function_args to a single PL/SQL proc, so we can use package_instantiate_object - need to add it to the rest

---
+++ packages/acs-content-repository/sql/postgresql/upgrade/upgrade-5.0d1-5.0d2.sql
@@ -0,0 +1,7 @@
+-- @author Lars Pind (lars@collaboraid.biz)
+-- @creation-date 2003-10-14
+-- @cvs-id $Id$
+
+select define_function_args('content_folder__new','name,label,description,parent_id,context_id,folder_id,creation_date,creation_user,creation_ip');
+
+
