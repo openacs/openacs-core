f99c01816f0b9ccd556965393437c082421519d7
Lars Pind <lars@pinds.com>
2003-10-22 13:30:31 +0000

    Renamed page

--- packages/acs-lang/www/admin/package-list.tcl
+++ packages/acs-lang/www/admin/package-list.tcl
@@ -115,5 +115,5 @@ ad_form -extend -name search -form {
 
 
 set import_all_url [export_vars -base import-locale-from-files { { locale $current_locale } }]
-set export_all_url [export_vars -base export-locale-from-files { { locale $current_locale } }]
+set export_all_url [export_vars -base export-locale-to-files { { locale $current_locale } }]
 
