e57a10f0e36a83bc68561551bb67536a3ebf8934
Lars Pind <lars@pinds.com>
2003-10-28 08:54:44 +0000

    - Changed title to 'restarting', not 'restart', to imply that it's already happening. - Changing delay to two seconds, to increase the chance that the page is served before the server shuts down

--- packages/acs-admin/www/server-restart.tcl
+++ packages/acs-admin/www/server-restart.tcl
@@ -7,11 +7,11 @@ ad_page_contract {
     @cvs-id $Id$
 }
 
-set page_title "Restart Server"
+set page_title "Restarting Server"
 
 set context [list $page_title]
 
 
 # We do this as a schedule proc, so the server will have time to serve the page
 
-ad_schedule_proc -thread t -once t 1 ns_shutdown
+ad_schedule_proc -thread t -once t 2 ns_shutdown
