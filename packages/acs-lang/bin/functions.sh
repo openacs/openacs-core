b747833c6666ed6d438fc97bfa9f4d2b2f5f56f9
Peter Marklund <peter_marklund@fastmail.fm>
2003-10-30 13:10:53 +0000

    making check-catalog.sh output warning keyword on every line relevant to warnings for easy greping. Adding the list-keys script that will list all keys in catalog files on the server

---
+++ packages/acs-lang/bin/functions.sh
@@ -0,0 +1,9 @@
+# Functions re-used by scripts in acs-lang/bin
+#
+# @author Peter Marklund
+
+# Assumes script_path to be set
+get_catalog_keys() {
+    file_name=$1
+    echo $(${script_path}/mygrep '<msg key="([^"]+)"' $file_name)
+}
