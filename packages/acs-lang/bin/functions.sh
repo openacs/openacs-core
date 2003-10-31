8b3bf46985c8e24ae0f6b6ed915f7f132ab624a5
Peter Marklund <peter_marklund@fastmail.fm>
2003-10-31 12:58:29 +0000

    extracting command to find all en_US catalog files into a shell function

--- packages/acs-lang/bin/functions.sh
+++ packages/acs-lang/bin/functions.sh
@@ -7,3 +7,7 @@ get_catalog_keys() {
     file_name=$1
     echo $(${script_path}/mygrep '<msg key="([^"]+)"' $file_name)
 }
+
+find_en_us_files() {
+    echo $(find ${script_path}/../../ -regex '.*/catalog/.*en_US.*\.xml' -maxdepth 3)
+}
