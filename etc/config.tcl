8968de46efbb8cdf665a7b66f1083350ad3a90bf
Joel Aufrecht <joel@aufrecht.org>
2003-11-04 10:35:32 +0000

    added cookie logging; rollout support; more formatting

--- etc/config.tcl
+++ etc/config.tcl
@@ -8,6 +8,7 @@ ns_log notice "nsd.tcl: starting to read config file..."
 #
 ###################################################################### 
 
+#---------------------------------------------------------------------
 # which database do you want? postgres or oracle
 set database              postgres 
 
@@ -15,57 +16,87 @@ if {$database == "oracle"} {
     set db_password        "mysitepassword"
 }
 
+#---------------------------------------------------------------------
 # change to 80 and 443 for production use
-set httpport              8000
-set httpsport             8443 
+set httpport                  8000
+set httpsport                 8443 
 
 # The hostname and address should be set to actual values.
-set hostname               [ns_info hostname]
-set address                127.0.0.1 
+set hostname                  [ns_info hostname]
+set address                   [ns_info address]
 
-set server              "service0" 
-set db_name             $server
-set servername          "New OpenACS Installation - Development"
+set server                    "service0" 
+set db_name                   $server
+set servername                "New OpenACS Installation - Development"
 
-set serverroot          "/var/lib/aolserver/${server}"
+set serverroot                "/var/lib/aolserver/${server}"
 
+#---------------------------------------------------------------------
 # if debug is false, all debugging will be turned off
 set debug false
 
-set homedir                 /usr/local/aolserver
-set bindir                  [file dirname [ns_info nsd]] 
+set homedir                   /usr/local/aolserver
+set bindir                    [file dirname [ns_info nsd]] 
 
+#---------------------------------------------------------------------
 # which modules should be loaded?  Missing modules break the server, so
 # don't uncomment modules unless they have been installed.
 
 ns_section ns/server/${server}/modules 
-ns_param   nssock          ${bindir}/nssock.so 
-ns_param   nslog           ${bindir}/nslog.so 
-ns_param   nssha1          ${bindir}/nssha1.so 
-ns_param   nscache         ${bindir}/nscache.so 
-ns_param   nsrewrite       ${bindir}/nsrewrite.so 
-ns_param   libtdom         ${bindir}/libtdom.so
+ns_param   nssock             ${bindir}/nssock.so 
+ns_param   nslog              ${bindir}/nslog.so 
+ns_param   nssha1             ${bindir}/nssha1.so 
+ns_param   nscache            ${bindir}/nscache.so 
+ns_param   nsrewrite          ${bindir}/nsrewrite.so 
+ns_param   libtdom            ${bindir}/libtdom.so
 
+#---------------------------------------------------------------------
 # nsopenssl will fail unless the cert files are present as specified
 # later in this file, so it's disabled by default
-#ns_param   nsopenssl       ${bindir}/nsopenssl.so
+#ns_param   nsopenssl          ${bindir}/nsopenssl.so
 
 # Full Text Search
-#ns_param   nsfts           ${bindir}/nsfts.so
+#ns_param   nsfts              ${bindir}/nsfts.so
 
 # PAM authentication
-#ns_param   nspam           ${bindir}/nspam.so
+#ns_param   nspam              ${bindir}/nspam.so
 
 # LDAP authentication
-#ns_param   nsldap          ${bindir}/nsldap.so
+#ns_param   nsldap             ${bindir}/nsldap.so
 
 # These modules aren't used in standard OpenACS installs
-#ns_param   nsperm          ${bindir}/nsperm.so 
-#ns_param   nscgi           ${bindir}/nscgi.so 
-#ns_param   nsjava          ${bindir}/libnsjava.so
+#ns_param   nsperm             ${bindir}/nsperm.so 
+#ns_param   nscgi              ${bindir}/nscgi.so 
+#ns_param   nsjava             ${bindir}/libnsjava.so
 
 # Required for AOLserver 4
-#ns_param   nsdb            ${bindir}/nsdb.so
+#ns_param   nsdb               ${bindir}/nsdb.so
+
+
+#---------------------------------------------------------------------
+#
+# Rollout email support
+#
+# These procs help manage differing email behavior on 
+# dev/staging/production.
+#
+#---------------------------------------------------------------------
+
+ns_section ns/server/${server}/acs/acs-rollout-support
+
+# EmailDeliveryMode can be:
+#   default:  Email messages are sent in the usual manner.
+#   log:      Email messages are written to the server's error log.
+#   redirect: Email messages are redirected to the addresses specified 
+#             by the EmailRedirectTo parameter.  If this list is absent 
+#             or empty, email messages are written to the server's error log.
+#   filter:   Email messages are sent to in the usual manner if the 
+#             recipient appears in the EmailAllow parameter, otherwise they 
+#             are logged.
+
+#ns_param   EmailDeliveryMode redirect
+#ns_param   EmailRedirectTo    somenerd@yourdomain.test, othernerd@yourdomain.test
+#ns_param   EmailAllow         somenerd@yourdomain.test,othernerd@yourdomain.test
 
 ###################################################################### 
 #
@@ -76,22 +107,22 @@ ns_param   libtdom         ${bindir}/libtdom.so
 ###################################################################### 
 
 
-###################################################################### 
+#---------------------------------------------------------------------
 #
 # AOLserver's directories. Autoconfigurable. 
 #
-###################################################################### 
+#---------------------------------------------------------------------
 
-#
+#---------------------------------------------------------------------
 # Where are your pages going to live ?
 #
-set pageroot                ${serverroot}/www 
-set directoryfile           index.tcl,index.adp,index.html,index.htm
+set pageroot                  ${serverroot}/www 
+set directoryfile             index.tcl,index.adp,index.html,index.htm
 
 
-# 
+#---------------------------------------------------------------------
 # Global server parameters 
-#
+#---------------------------------------------------------------------
 
 ns_section ns/parameters 
 ns_param   serverlog          ${serverroot}/log/error.log 
@@ -101,9 +132,10 @@ ns_param   logroll            on
 ns_param   maxbackup          5
 ns_param   debug              $debug
 
-# 
+#---------------------------------------------------------------------
 # Thread library (nsthread) parameters 
-# 
+#---------------------------------------------------------------------
+
 ns_section ns/threads 
 ns_param   mutexmeter         true      ;# measure lock contention 
 ns_param   stacksize          500000
@@ -127,12 +159,12 @@ ns_param   .doc               application/vnd.ms-word
 # Tcl Configuration 
 # 
 ns_section ns/server/${server}/tcl
-ns_param   library        ${serverroot}/tcl
-ns_param   autoclose      on 
-ns_param   debug          $debug
+ns_param   library            ${serverroot}/tcl
+ns_param   autoclose          on 
+ns_param   debug              $debug
  
 
-###################################################################### 
+#---------------------------------------------------------------------
 # 
 # Server-level configuration 
 # 
@@ -141,10 +173,10 @@ ns_param   debug          $debug
 #  one server is in use so it is set at the top in the "server" Tcl variable
 #  Other host-specific values are set up above as Tcl variables, too.
 # 
-###################################################################### 
+#---------------------------------------------------------------------
 
 ns_section ns/servers 
-ns_param   $server     $servername 
+ns_param   $server            $servername 
 
 # 
 # Server parameters 
@@ -160,39 +192,39 @@ ns_param   threadtimeout      120
 ns_param   globalstats        false    ;# Enable built-in statistics 
 ns_param   urlstats           false    ;# Enable URL statistics 
 ns_param   maxurlstats        1000     ;# Max number of URL's to do stats on
-#ns_param   directoryadp    $pageroot/dirlist.adp ;# Choose one or the other
-#ns_param   directoryproc    _ns_dirlist          ;#  ...but not both!
-#ns_param   directorylisting  fancy               ;# Can be simple or fancy
+#ns_param   directoryadp       $pageroot/dirlist.adp ;# Choose one or the other
+#ns_param   directoryproc      _ns_dirlist          ;#  ...but not both!
+#ns_param   directorylisting   fancy               ;# Can be simple or fancy
 
 #
 # Special HTTP pages
 #
 
-ns_param   NotFoundResponse     "/global/file-not-found.html"
-ns_param   ServerBusyResponse   "/global/busy.html"
+ns_param   NotFoundResponse   "/global/file-not-found.html"
+ns_param   ServerBusyResponse "/global/busy.html"
 ns_param   ServerInternalErrorResponse "/global/error.html"
 
-###################################################################### 
+#---------------------------------------------------------------------
 # 
 # ADP (AOLserver Dynamic Page) configuration 
 # 
-###################################################################### 
+#---------------------------------------------------------------------
 
 ns_section ns/server/${server}/adp 
-ns_param   map           /*.adp    ;# Extensions to parse as ADP's 
-#ns_param   map          "/*.html" ;# Any extension can be mapped 
-ns_param   enableexpire  false     ;# Set "Expires: now" on all ADP's 
-ns_param   enabledebug   $debug    ;# Allow Tclpro debugging with "?debug"
-ns_param   defaultparser fancy
+ns_param   map                /*.adp    ;# Extensions to parse as ADP's 
+#ns_param   map                "/*.html" ;# Any extension can be mapped 
+ns_param   enableexpire       false     ;# Set "Expires: now" on all ADP's 
+ns_param   enabledebug        $debug    ;# Allow Tclpro debugging with "?debug"
+ns_param   defaultparser      fancy
 
 ns_section ns/server/${server}/adp/parsers
 ns_param   fancy    ".adp"
  
-###################################################################### 
+#---------------------------------------------------------------------
 # 
 # Socket driver module (HTTP)  -- nssock 
 # 
-###################################################################### 
+#---------------------------------------------------------------------
 
 ns_section ns/server/${server}/module/nssock
 ns_param   timeout            120
@@ -200,33 +232,33 @@ ns_param   address            $address
 ns_param   hostname           $hostname
 ns_param   port               $httpport
 
-###################################################################### 
+#---------------------------------------------------------------------
 #
 # OpenSSL
 #
-###################################################################### 
+#---------------------------------------------------------------------
 
 ns_section "ns/server/${server}/module/nsopenssl"
 
-ns_param ModuleDir                       ${serverroot}/etc/certs
+ns_param ModuleDir            ${serverroot}/etc/certs
 
 # NSD-driven connections:
-ns_param ServerPort                      $httpsport
-ns_param ServerHostname                  $hostname
-ns_param ServerAddress                   $address
-ns_param ServerCertFile                  certfile.pem
-ns_param ServerKeyFile                   keyfile.pem
-ns_param ServerProtocols                 "SSLv2, SSLv3, TLSv1"
-ns_param ServerCipherSuite               "ALL:!ADH:RC4+RSA:+HIGH:+MEDIUM:+LOW:+SSLv2:+EXP"
-ns_param ServerSessionCache              false
-ns_param ServerSessionCacheID            1
-ns_param ServerSessionCacheSize          512
-ns_param ServerSessionCacheTimeout       300
-ns_param ServerPeerVerify                true
-ns_param ServerPeerVerifyDepth           3
-ns_param ServerCADir                     ca
-ns_param ServerCAFile                    ca.pem
-ns_param ServerTrace                     false
+ns_param ServerPort                $httpsport
+ns_param ServerHostname            $hostname
+ns_param ServerAddress             $address
+ns_param ServerCertFile            certfile.pem
+ns_param ServerKeyFile             keyfile.pem
+ns_param ServerProtocols           "SSLv2, SSLv3, TLSv1"
+ns_param ServerCipherSuite         "ALL:!ADH:RC4+RSA:+HIGH:+MEDIUM:+LOW:+SSLv2:+EXP"
+ns_param ServerSessionCache        false
+ns_param ServerSessionCacheID      1
+ns_param ServerSessionCacheSize    512
+ns_param ServerSessionCacheTimeout 300
+ns_param ServerPeerVerify          true
+ns_param ServerPeerVerifyDepth     3
+ns_param ServerCADir               ca
+ns_param ServerCAFile              ca.pem
+ns_param ServerTrace               false
 
 # For listening and accepting SSL connections via Tcl/C API:
 ns_param SockServerCertFile              certfile.pem
@@ -259,23 +291,23 @@ ns_param SockClientCAFile                ca.pem
 ns_param SockClientTrace                 false
 
 # OpenSSL library support:
-#ns_param RandomFile                      /some/file
-ns_param SeedBytes                       1024
+#ns_param RandomFile          /some/file
+ns_param SeedBytes            1024
 
 
-###################################################################### 
+#---------------------------------------------------------------------
 # 
 # Database drivers 
 # The database driver is specified here.
 # Make sure you have the driver compiled and put it in {aolserverdir}/bin
 #
-###################################################################### 
+#---------------------------------------------------------------------
 
 ns_section "ns/db/drivers" 
 if { $database == "oracle" } {
-    ns_param   ora8            ${bindir}/ora8.so
+    ns_param   ora8           ${bindir}/ora8.so
 } else {
-    ns_param   postgres        ${bindir}/nspostgres.so  ;# Load PostgreSQL driver
+    ns_param   postgres       ${bindir}/nspostgres.so  ;# Load PostgreSQL driver
 }
 
 # 
@@ -288,9 +320,9 @@ if { $database == "oracle" } {
 # and even different different database servers.
 # 
 ns_section ns/db/pools 
-ns_param   pool1       "Pool 1"
-ns_param   pool2       "Pool 2"
-ns_param   pool3       "Pool 3"
+ns_param   pool1              "Pool 1"
+ns_param   pool2              "Pool 2"
+ns_param   pool3              "Pool 3"
 
 ns_section ns/db/pool/pool1
 ns_param   maxidle            1000000000
@@ -358,32 +390,35 @@ ns_param   404                "global/file-not-found.html"
 ns_param   403                "global/forbidden.html"
 
 
-###################################################################### 
+#---------------------------------------------------------------------
 # 
 # Access log -- nslog 
 # 
-###################################################################### 
+#---------------------------------------------------------------------
 
 ns_section ns/server/${server}/module/nslog 
-ns_param   file                 ${serverroot}/log/${server}.log
+ns_param   debug              false
+ns_param   dev                false
 ns_param   enablehostnamelookup false
-ns_param   logcombined          true
-#ns_param   logrefer             false
-#ns_param   loguseragent         false
-ns_param   maxbackup            1000
-ns_param   rollday              *
-ns_param   rollfmt              %Y-%m-%d-%H:%M
-ns_param   rollhour             0
-ns_param   rollonsignal         true
-ns_param   rolllog              true
+ns_param   file               ${serverroot}/log/${server}.log
+ns_param   logcombined        true
+ns_param   extendedheaders    COOKIE
+#ns_param   logrefer           false
+#ns_param   loguseragent       false
+ns_param   maxbackup          1000
+ns_param   rollday            *
+ns_param   rollfmt            %Y-%m-%d-%H:%M
+ns_param   rollhour           0
+ns_param   rollonsignal       true
+ns_param   rolllog            true
 
-###################################################################### 
+#---------------------------------------------------------------------
 #
 # nsjava - aolserver module that embeds a java virtual machine.  Needed to 
 #          support webmail.  See http://nsjava.sourceforge.net for further 
 #          details. This may need to be updated for OpenACS4 webmail
 #
-###################################################################### 
+#---------------------------------------------------------------------
 
 ns_section ns/server/${server}/module/nsjava
 ns_param   enablejava         off  ;# Set to on to enable nsjava.
@@ -393,13 +428,13 @@ ns_param   destroyjvm         off  ;# Destroy jvm on shutdown.
 ns_param   disablejitcompiler off  
 ns_param   classpath          /usr/local/jdk/jdk118_v1/lib/classes.zip:${bindir}/nsjava.jar:${pageroot}/webmail/java/activation.jar:${pageroot}/webmail/java/mail.jar:${pageroot}/webmail/java 
 
-###################################################################### 
+#---------------------------------------------------------------------
 # 
 # CGI interface -- nscgi, if you have legacy stuff. Tcl or ADP files inside 
 # AOLserver are vastly superior to CGIs. I haven't tested these params but they
 # should be right.
 # 
-###################################################################### 
+#---------------------------------------------------------------------
 
 #ns_section "ns/server/${server}/module/nscgi" 
 #       ns_param   map "GET  /cgi-bin/ /web/$server/cgi-bin"
@@ -410,14 +445,14 @@ ns_param   classpath          /usr/local/jdk/jdk118_v1/lib/classes.zip:${bindir}
 #       ns_param .pl "/usr/bin/perl"
 
 
-###################################################################### 
+#---------------------------------------------------------------------
 #
 # PAM authentication
 #
-###################################################################### 
+#---------------------------------------------------------------------
 
 ns_section ns/server/${server}/module/nspam
-ns_param   PamDomain               "pam_domain"
+ns_param   PamDomain          "pam_domain"
 
 ns_log notice "nsd.tcl: finished reading config file."
 
