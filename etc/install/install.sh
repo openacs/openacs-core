8a192cd3f26e657f66d77ec2d9fe565413c55df9
Joel Aufrecht <joel@aufrecht.org>
2003-11-05 11:31:34 +0000

    assorted db-related bug fixes in install scripts

--- etc/install/install.sh
+++ etc/install/install.sh
@@ -223,30 +223,25 @@ fi
 # Recreate the database user and database
 echo "$0: Recreating database user and database at $(date)"
 if [ $database == "postgres" ]; then
+
     # Postgres
     pg_bindir=`get_config_param pg_bindir`
     pg_port=`get_config_param pg_port`
     db_name=`get_config_param db_name`
-    su  `get_config_param pg_db_admin` -c "export LD_LIBRARY_PATH=${pg_bindir}/../lib; ${pg_bindir}/dropuser -\
-p $pg_port $db_name"
-    su  `get_config_param pg_db_admin` -c "export LD_LIBRARY_PATH=${pg_bindir}/../lib; ${pg_bindir}/createuser\
- -d -a -p $pg_port $db_name"
-    su  `get_config_param pg_db_admin` -c "export LD_LIBRARY_PATH=${pg_bindir}/../lib; ${pg_bindir}/dropdb -p \
-$pg_port $db_name"
-    su  `get_config_param pg_db_admin` -c "export LD_LIBRARY_PATH=${pg_bindir}/../lib; ${pg_bindir}/createdb -\
-p $pg_port $db_name"
+    su  `get_config_param pg_db_admin` -c "export LD_LIBRARY_PATH=${pg_bindir}/../lib; ${pg_bindir}/dropuser -p $pg_port $db_name"
+    su  `get_config_param pg_db_admin` -c "export LD_LIBRARY_PATH=${pg_bindir}/../lib; ${pg_bindir}/createuser -d -a -p $pg_port $db_name"
 
-    su  `get_config_param pg_db_admin` -c "export LD_LIBRARY_PATH=${pg_bindir}/../lib; ${pg_bindir}/createuser \
--d -a -p $pg_port $db_name"
-    su  `get_config_param pg_db_admin` -c "export LD_LIBRARY_PATH=${pg_bindir}/../lib; ${pg_bindir}/dropdb -p $\
-pg_port $db_name"
-    su  `get_config_param pg_db_admin` -c "export LD_LIBRARY_PATH=${pg_bindir}/../lib; ${pg_bindir}/createdb -p\
- $pg_port $db_name"
+    # dropdb may be redundant becasue dropping the user should drop the db, 
+    # but only if our assumption that db_user owns db_name is true
+    su  `get_config_param pg_db_admin` -c "export LD_LIBRARY_PATH=${pg_bindir}/../lib; ${pg_bindir}/dropdb -p $pg_port $db_name"
+    su  `get_config_param pg_db_admin` -c "export LD_LIBRARY_PATH=${pg_bindir}/../lib; ${pg_bindir}/createdb -p $pg_port $db_name"
 
-    # createlang was part of this command but is not necessary (and causes an error) for 
-    # newer installs
+    # createlang was part of this command but is not necessary 
+    # (and causes an error) for newer installs
     # ${pg_bindir}/createlang -p $pg_port plpgsql $db_name";
+
 else
+
     #Oracle
     # Need to su to login shell for sqlplus to be in path. Should maybe make ORA_HOME
     # a config param instead.
@@ -407,8 +402,9 @@ if parameter_true $do_install; then
 
   if [ $database == "postgres" ]; then
       # Run vacuum analyze
+      db_name=`get_config_param db_name`
       echo "$0: Beginning 'vacuum analyze' at $(date)"
-      su  `get_config_param pg_db_user` -c "export LD_LIBRARY_PATH=${pg_bindir}/../lib; ${pg_bindir}/vacuumdb -p $pg_port -z `get_config_param db_name`"
+      su  `get_config_param pg_db_user` -c "export LD_LIBRARY_PATH=${pg_bindir}/../lib; ${pg_bindir}/vacuumdb -p $pg_port -z $db_name"
   fi
   
   # Report the time at which we were done
