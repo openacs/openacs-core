5c718eb9eaca78f66468f382184dffddf8737ed9
Jeff Davis <davis@xarg.net>
2003-11-06 21:08:20 +0000

    use site-nodes cache instead of going to the db

--- packages/acs-subsite/www/admin/host-node-map/index.tcl
+++ packages/acs-subsite/www/admin/host-node-map/index.tcl
@@ -27,13 +27,12 @@ db_foreach host_node_pair {
 set nodes [list]
 set root_id [ad_conn node_id]
 set node_list [list]
-db_foreach node_list {} { 
+foreach node_id [site_node::get_children -all -element node_id -node_id [site_node::get_node_id -url "/"]] { 
     lappend node_list [list [site_node::get_element -node_id $node_id -element url] $node_id]
 }
 foreach node [lsort $node_list] {
     append nodes "<input type=radio name=root value=\"[lindex $node 1]\">[lindex $node 0]<br>\n"
 }
-
 
 doc_body_append "
 [ad_header "Host-Node Map"]
