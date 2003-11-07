437e61fb5c0f0bcc2bf29655ab97d9617d958d65
Peter Marklund <peter_marklund@fastmail.fm>
2003-11-07 11:37:47 +0000

    adding edit history page

---
+++ packages/acs-lang/www/admin/edit-history.adp
@@ -0,0 +1,32 @@
+<master>
+
+<b>Displaying:</b>
+<p>
+  <ul>
+    <li>Locale: @locale@</li>
+    <li>Number of edits: @number_of_edits@</li
+    <li>Excluding emails like: @email_exclude@</li>
+  </ul>
+</p>
+
+<p>
+  <formtemplate id="locale"></formtemplate>
+</p>
+
+<table>
+  <tr>
+    <th>Key</th>
+    <th>Overwrite date</th>
+    <th>Old message</th>
+    <th>User</th>
+  </tr>
+
+  <multiple name="history">
+    <tr>
+      <td><a href="@history.key_url@">@history.package_key@.@history.message_key@</a></td>  
+      <td>@history.overwrite_date@</td>  
+      <td>@history.old_message@</td>  
+      <td>@history.user_name@
+    </tr>
+  </multiple>
+</table>
---
+++ packages/acs-lang/www/admin/edit-history.tcl
@@ -0,0 +1,51 @@
+ad_page_contract {
+    A page displaying recent editing of translations.
+    
+    @author Peter Marklund
+} {
+    {locale "de_DE"}
+    {number_of_edits 400}
+    {email_exclude ""}
+}
+
+set list_of_locales [db_list_of_lists locale_loop { select label, locale from enabled_locales order by label }]
+
+set admin_email peter@collaboraid.biz
+
+ad_form \
+    -name locale \
+    -method GET \
+    -form {
+        {locale:text(select)
+            {label "Locale"}
+            {options $list_of_locales}
+        }
+        {number_of_edits:text,optional
+            {label "Number of edits"}
+            {value $number_of_edits}
+        }
+        {email_exclude:text,optional
+            {label "Email pattern to exclude"}
+            {value $email_exclude}
+        }
+    }    
+
+set email_clause [ad_decode $email_exclude "" "" "and cu.email not like '%$email_exclude%'"]
+
+db_multirow -extend { key_url } history german_edit_history "
+    select q.* from (select lma.overwrite_date, 
+                            lma.old_message,
+                            lma.message_key,
+                            lma.package_key,
+                            lma.locale,
+                            cu.first_names || cu.last_name as user_name
+                     from lang_messages_audit lma,
+                          cc_users cu
+                     where cu.user_id = lma.overwrite_user
+                       and lma.locale = :locale
+                       $email_clause
+                     order by lma.overwrite_date desc) q 
+    where rownum < :number_of_edits
+" {
+    set key_url [export_vars -base /acs-lang/admin/edit-localized-message {package_key message_key locale}]
+}
