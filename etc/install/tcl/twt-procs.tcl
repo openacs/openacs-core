bbfd6cef1767287995a71b4fe52321818ba0ca76
Peter Marklund <peter_marklund@fastmail.fm>
2003-11-19 10:05:56 +0000

    debugging the crawler and adding ability to exclude links

--- etc/install/tcl/twt-procs.tcl
+++ etc/install/tcl/twt-procs.tcl
@@ -175,6 +175,57 @@ ad_proc ::twt::write_response_to_file { filename } {
     puts $file_id "[response body]"
 }
 
+ad_proc ::twt::set_crawler_exclude_links { url_list } {
+    A list of URLs that the crawler should not visit (for example
+    URLs that would delete data that shouldn't be deleted).
+
+    @author Peter Marklund
+} {
+    global __crawler_exclude_links
+    
+    set __crawler_exclude_links $url_list
+}
+
+ad_proc ::twt::get_crawler_exclude_links {} {
+    Get the list of URLs that the crawler should exclude.
+
+    @see ::twt::set_crawler_exclude_links
+
+    @author Peter Marklund
+} {
+    global __crawler_exclude_links
+    
+    return $__crawler_exclude_links
+}
+
+ad_proc ::twt::exclude_crawl_link_p { url } {
+    Return 1 if the given URL matches the patterns in the
+    URL exclude list and 0 otherwise.
+} {
+    foreach exclude_pattern [get_crawler_exclude_links] {
+        if { [regexp [string trim $exclude_pattern] $url] } {
+            return 1
+        }
+    }
+
+    return 0
+}
+
+ad_proc ::twt::record_excluded_url { url } {
+    Record that a URL was not crawled because it matched
+    a pattern in the exclude list.
+} {
+    global __crawler_excluded_links
+    
+    lappend __crawler_excluded_links $url
+}
+
+ad_proc ::twt::get_excluded_urls {} {
+    global __crawler_excluded_links
+    
+    return $__crawler_excluded_links
+}
+
 ad_proc ::twt::crawl_links { 
     {-max_requests 2000}
     {-previous_url ""}
@@ -184,10 +235,12 @@ ad_proc ::twt::crawl_links {
     url. For example, if start_url is "/simulation" then a link
     with the URL "/simulation/object-display?object_id=125" would be visited
     whereas a link with a URL not under "/simulation", such as "/", would not.
-    Never visit links with external URLs (outside the server).
+    Never visit links with external URLs (outside the server). 
 
     @param max_requests The maximum number of links that the proc will crawl
-    @param start_url    The url to start crawling from
+    @param start_url    The url to start crawling from. The first
+                        time the proc is invoked the start_url needs to have
+                        a trailing slash if it is a directory.
 
     @author Peter Marklund
 } {
@@ -196,6 +249,7 @@ ad_proc ::twt::crawl_links {
         # Tclwebtest understand that we are now relative to a different URL than the one last requested, namely
         # relative to the URL of the page the link is on.
         #::twt::log "pm debug setting previous_url $previous_url"
+        #::twt::log "pm debug set ::tclwebtest::url $previous_url"
         set ::tclwebtest::url $previous_url
     }
 
@@ -223,8 +277,14 @@ ad_proc ::twt::crawl_links {
         return
     }
 
-    lappend __url_history $start_url_absolute
+    # Before requesting, check if the URL matches the exclude list
+    if { [::twt::exclude_crawl_link_p $start_url_absolute] } {
+        # Keep a record of URLs not visited because the matched the exclude link
+        ::twt::record_excluded_url $start_url_absolute
+        return
+    }
 
+    lappend __url_history $start_url_absolute
     # Request the page
     #::twt::log "pm debug about to invoke \"do_request $start_url_absolute\" start_url=$start_url previous_url=$previous_url"
     if { [catch {::twt::do_request $start_url_absolute} errmsg] } {
@@ -259,11 +319,9 @@ ad_proc ::twt::crawl_links {
             set anchor_link_p 0
         }
         set under_start_url_p [expr [string first $start_url_absolute $absolute_url] != -1]
+
         if { $new_url_p && !$anchor_link_p && $under_start_url_p } {
-            #::twt::log "pm debug crawl_links -previous_url $start_url_absolute $url"
             crawl_links -previous_url $start_url_absolute $url
-        } else {
-            #::twt::log "pm debug skipping url $url $new_url_p $anchor_link_p $under_start_url_p"
         }
     }
 }
