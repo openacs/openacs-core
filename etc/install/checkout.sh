aa1bc8ec432c9996860d987795cffb5eab682c45
Peter Marklund <peter_marklund@fastmail.fm>
2003-11-20 13:58:58 +0000

    moving up the get_config_param command for oacs_branch in checkout.sh to be above the cd command as the config_file path was relative to current dir (.)

--- etc/install/checkout.sh
+++ etc/install/checkout.sh
@@ -17,20 +17,24 @@ set -e
 #set -x
 
 export CVS_RSH=ssh
-script_path=$(dirname $(which $0))
+export script_path=$(dirname $(which $0))
 
 cd $script_path
 
 # Sometimes script path will be the dot so I need this workaround
-script_path=$PWD
+export script_path=$(pwd)
 
 source ./functions.sh
 
 # Fetch config parameters
+# NOTE: fetching parameters further down in the script will not work
+# if the config_file=./install.tcl since we are cd:ing to a different dir
+# below
 serverroot=`get_config_param serverroot`
 aolserver_user=`get_config_param aolserver_user`
 aolserver_group=`get_config_param aolserver_group`
 packages_list=`get_config_param packages_list`
+oacs_branch=`get_config_param oacs_branch`
 
 echo "$0: Starting checkout for server path $serverroot with config_file $config_file and dotlrn=$dotlrn"
 
@@ -52,13 +56,13 @@ fi
 # Checkout OpenACS core
 mkdir -p ${serverroot}-tmp
 cd ${serverroot}-tmp
-oacs_branch=`get_config_param oacs_branch`
 if [ "$oacs_branch" == "HEAD" ]; then
     oacs_branch_switch=""
 else
     oacs_branch_switch="-r $oacs_branch"
 fi
-echo "$0: Checking out acs-core from branch $oacs_branch"
+
+echo "$0: Checking out acs-core from branch \"$oacs_branch\""
 cvs -q -d :pserver:anonymous:@openacs.org:/cvsroot login
 cvs -q -z3 -d :pserver:anonymous@openacs.org:/cvsroot checkout $oacs_branch_switch acs-core
 mv ${serverroot}-tmp/openacs-4 ${serverroot}
