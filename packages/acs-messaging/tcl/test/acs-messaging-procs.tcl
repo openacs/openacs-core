addb51bb67bc4f9da4873598072f1f00ce870206
Simon Carstensen <simon@bcuni.net>
2003-12-10 16:05:29 +0000

    Added automated test

--- packages/acs-messaging/tcl/test/acs-messaging-procs.tcl
+++ packages/acs-messaging/tcl/test/acs-messaging-procs.tcl
@@ -6,8 +6,8 @@ ad_library {
     @cvs-id $Id$
 }
 
-aa_register_case acs_messaging_trivial_smoke_test {
-    Minimal smoke test.
+aa_register_case acs_messaging_format_as_html {
+    Test acs_messaging_format_as_html proc.
 } {    
 
     aa_run_with_teardown \
@@ -17,8 +17,20 @@ aa_register_case acs_messaging_trivial_smoke_test {
             set name [ad_generate_random_string]
 
             set formatted_name [acs_messaging_format_as_html text/html $name]
-
             aa_true "Name is formatted" ![string match "<pre>$name<pre>" $formatted_name]
+        }
+}
+
+aa_register_case acs_messaging_message_p {
+    Test message_p proc.
+} {    
+
+    aa_run_with_teardown \
+        -rollback \
+        -test_code {
+
+            set message_p [acs_message_p "0"]
+            aa_true "Integer is not a message_id" !$message_p
 
         }
 }
