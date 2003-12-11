382463eca2edd7bba3471375ec44f9308426aee6
Peter Marklund <peter_marklund@fastmail.fm>
2003-12-11 12:40:17 +0000

    adding primary key to the lang_messages_audit table. Fixed upgrade script to version 5.0.0b5 so that sync_time is set for the first revision of each I18N message

--- packages/acs-lang/sql/oracle/upgrade/upgrade-5.0.0b4-5.0.0b5.sql
+++ packages/acs-lang/sql/oracle/upgrade/upgrade-5.0.0b4-5.0.0b5.sql
@@ -8,22 +8,22 @@
 alter table lang_message_keys drop column upgrade_status;
 
 -- Add new columns to the lang_messages table 
-alter table lang_messages add deleted_p boolean;
-alter table lang_messages alter column deleted_p set default 'f';
-alter table lang_messages add sync_time timestamptz;
-alter table lang_messages add conflict_p boolean;
-alter table lang_messages alter column conflict_p set default 'f';
+alter table lang_messages add deleted_p char(1) default 'f'
+                              constraint lang_messages_dp_ck check (deleted_p in ('t','f'));
+alter table lang_messages add sync_time date;
+alter table lang_messages add conflict_p  char(1) default 'f'
+                              constraint lang_messages_cp_ck check (conflict_p in ('t','f'));
 update lang_messages set deleted_p = 'f', conflict_p = 'f';
 
 -- Add new columns to the lang_messages_audit tables
-alter table lang_messages_audit add deleted_p boolean;
-alter table lang_messages_audit alter column deleted_p set default 'f';
-alter table lang_messages_audit add sync_time timestamptz;
-alter table lang_messages_audit add conflict_p boolean;
-alter table lang_messages_audit alter column conflict_p set default 'f';
-alter table lang_messages_audit add    upgrade_status     varchar(30)
-                                       constraint lang_messages_us_ck
-                                       check (upgrade_status in ('no_upgrade', 'added', 'deleted', 'updated'));
+alter table lang_messages_audit add deleted_p char(1) default 'f'
+                              constraint lang_messages_audit_dp_ck check (deleted_p in ('t','f'));
+alter table lang_messages_audit add sync_time date;
+alter table lang_messages_audit add conflict_p  char(1) default 'f'
+                              constraint lang_messages_audit_cp_ck check (conflict_p in ('t','f'));
+alter table lang_messages_audit add upgrade_status     varchar2(30)
+                              constraint lang_messages_audit_us_ck
+                              check (upgrade_status in ('no_upgrade', 'added', 'deleted', 'updated'));
 update lang_messages_audit set deleted_p = 'f', conflict_p = 'f', upgrade_status = 'no_upgrade';
 
 -- Missing this primary key made some queries below very slow
@@ -37,7 +37,7 @@ alter table lang_messages_audit add constraint lang_messages_audit_pk primary ke
 -- Messages with only one revision
 -- This query is slow. Not sure how to speed it up.
 update lang_messages lm
-       set lm.sync_time = current_timestamp
+       set lm.sync_time = sysdate
        where not exists (select 1
                                from lang_messages_audit lma
                                where lma.package_key = lm.package_key
@@ -48,7 +48,7 @@ update lang_messages lm
 -- Messages with multiple revisions
 -- This query is slow as well.
  update lang_messages_audit lma1
-        set lma1.sync_time = current_timestamp
+        set lma1.sync_time = sysdate
         where lma1.overwrite_date = (select min(lma2.overwrite_date)
                                     from lang_messages_audit lma2
                                     where lma2.package_key = lma1.package_key
