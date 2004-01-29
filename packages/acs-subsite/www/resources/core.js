4f586847feea6d178025662aa8302e0df751aca2
Lars Pind <lars@pinds.com>
2004-01-29 11:04:06 +0000

    Added richtext widget javascript

--- packages/acs-subsite/www/resources/core.js
+++ packages/acs-subsite/www/resources/core.js
@@ -49,3 +49,52 @@ function acs_CopyText(text) {
         clip.setData(trans, null, clipid. kGlobalClipboard);
     }
 }
+
+function acs_RichText_FormatStr (v) {
+    if (!document.selection) return;
+    var str = document.selection.createRange().text;
+    if (!str) return;
+    document.selection.createRange().text = '<' + v + '>' + str + '</' + v + '>';
+}
+
+function acs_RichText_InsertLink () {
+    if (!document.selection) return;
+    var str = document.selection.createRange().text;
+    if (!str) return;
+    var my_link = prompt('Enter URL:', 'http://');
+    if (my_link != null)
+        document.selection.createRange().text = '<a href="' + my_link + '">' + str + '</a>';
+}
+
+function acs_RichText_WriteButtons () {
+    if (document.selection) {
+        document.write('<table border="0" cellspacing="0" cellpadding="0" width="80">');
+        document.write('<tr>');
+        document.write('<td width="24"><a href="javascript:acs_RichText_FormatStr(\'b\')" tabIndex="-1"><img src="/resources/acs-subsite/bold-button.gif" alt="bold" width="24" height="18" border="0"></a></td>');
+        document.write('<td width="24"><a href="javascript:acs_RichText_FormatStr(\'i\')" tabIndex="-1"><img src="/resources/acs-subsite/italic-button.gif" alt="italic" width="24" height="18" border="0"></a></td>');
+        document.write('<td width="26"><a href="javascript:acs_RichText_InsertLink()" tabIndex="-1"><img src="/resources/acs-subsite/url-button.gif" alt="link" width="26" height="18" border="0"></a></td>');
+        document.write('</tr>');
+        document.write('</table>');
+    }
+}
+
+function acs_RichText_InputMethodChanged(form_name, richtext_name, radio_elm) {
+    if (radio_elm == null) return;
+    if (document.forms == null) return;
+    if (document.forms[form_name] == null) return;
+
+    if ( radio_elm.value == 'upload' ) {
+        document.forms[form_name].elements[richtext_name].disabled = true;
+        document.forms[form_name].elements[richtext_name+".format"].disabled = true;
+        document.forms[form_name].elements[richtext_name+".spellcheck"].disabled = true;
+
+        document.forms[form_name].elements[richtext_name+".file"].disabled = false;
+    } else {
+        document.forms[form_name].elements[richtext_name].disabled = false;
+        document.forms[form_name].elements[richtext_name+".format"].disabled = false;
+        document.forms[form_name].elements[richtext_name+".spellcheck"].disabled = false;
+
+        document.forms[form_name].elements[richtext_name+".file"].disabled = true;
+    }
+}
+
