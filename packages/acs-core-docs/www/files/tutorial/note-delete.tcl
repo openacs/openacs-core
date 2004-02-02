934d588fad37d124ea971c77e137d417a76ff4c7
Joel Aufrecht <joel@aufrecht.org>
2004-02-02 16:17:28 +0000

    tutorial files

---
+++ packages/acs-core-docs/www/files/tutorial/note-delete.tcl
@@ -0,0 +1,14 @@
+ad_page_contract {
+    This deletes a note
+
+    @author Your Name (you@example.com)
+    @cvs-id $Id$
+ 
+    @param item_id The item_id of the note to delete
+} {
+    item_id:integer
+}
+
+permission::require_write_permission -object_id $item_id
+set title [item::get_title $item_id]
+mfp::note::delete -item_id $item_id
