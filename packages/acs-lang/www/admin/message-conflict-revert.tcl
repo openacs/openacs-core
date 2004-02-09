801bfa3009076a396ebfa734de0fc5145492633b
Peter Marklund <peter_marklund@fastmail.fm>
2004-02-09 16:37:49 +0000

    making import page report errors. Making conflict page show both new and old message and offer the option of accepting new or reverting to old message

---
+++ packages/acs-lang/www/admin/message-conflict-revert.tcl
@@ -0,0 +1,23 @@
+ad_page_contract {
+    Revert the a message to the last overwritten version. 
+    Mark conflict of an I18N message as resolved, i.e.
+    set the conflict_p flag to false.
+
+    @author Peter Marklund
+} {
+    package_key
+    message_key
+    locale
+    {return_url {[export_vars -base "message-conflicts" { package_key locale }]}}
+}
+
+db_transaction {
+    lang::message::revert \
+        -package_key $package_key \
+        -message_key $message_key \
+        -locale $locale
+
+    lang::message::edit $package_key $message_key $locale [list conflict_p f]
+}
+
+ad_returnredirect $return_url
