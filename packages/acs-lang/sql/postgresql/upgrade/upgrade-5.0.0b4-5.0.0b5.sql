6b8824af3cb4587ec62cd14def5cbca2b3f310f2
Peter Marklund <peter_marklund@fastmail.fm>
2004-02-09 16:34:59 +0000

    leaving sync_time null and not assuming we were ever in sync. This has the downside that we have no base when bootstrapping the new upgrade feature causing a lot of update conflicts. However, it avoids delete conflicts.

--- packages/acs-lang/sql/postgresql/upgrade/upgrade-5.0.0b4-5.0.0b5.sql
+++ packages/acs-lang/sql/postgresql/upgrade/upgrade-5.0.0b4-5.0.0b5.sql
@@ -29,29 +29,5 @@ update lang_messages_audit set deleted_p = 'f', conflict_p = 'f', upgrade_status
 -- Missing this primary key made some queries below very slow
 alter table lang_messages_audit add constraint lang_messages_audit_pk primary key (package_key, message_key, locale, overwrite_date);
 
--- Make sure sync_time is not null for the first message imported. If we don't
--- do this we are missing a set of base messages to merge against on the next 
--- catalog import.
--- Messages with only one revision
-
--- Messages with only one revision
--- This query is slow. Not sure how to speed it up.
-update lang_messages 
-       set sync_time = current_timestamp
-       where not exists (select 1
-                               from lang_messages_audit lma
-                               where lma.package_key = lang_messages.package_key
-                                 and lma.message_key = lang_messages.message_key
-                                 and lma.locale = lang_messages.locale
-                              );
-
--- Messages with multiple revisions
--- This query is slow as well.
- update lang_messages_audit 
-        set sync_time = current_timestamp
-        where overwrite_date = (select max(lma2.overwrite_date)
-                                    from lang_messages_audit lma2
-                                    where lma2.package_key = lang_messages_audit.package_key
-                                      and lma2.message_key = lang_messages_audit.message_key
-                                      and lma2.locale      = lang_messages_audit.locale
-                                    );
+-- We have to leave sync_time null since we don't know when the messages in the db were last in sync
+-- with the catalog files
