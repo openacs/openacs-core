7647bc15ee8aaa7261ece5cf9e3ecee3fa05d298
cvs2svn <oct@openacs.org>
2004-02-25 10:48:49 +0000

    This commit was manufactured by cvs2svn to create branch 'oacs-5-0'.
    Cherrypick from master 2004-02-25 10:48:48 UTC Peter Marklund  <peter_marklund@fastmail.fm> 'adding auto-login page':
        packages/acs-subsite/www/register/auto-login.tcl

---
+++ packages/acs-subsite/www/register/auto-login.tcl
@@ -0,0 +1,23 @@
+ad_page_contract {
+    Log in a user without the timestamp protection of the /register/index page.
+    Useful when you need to log in a user from a different server.
+
+    @author Peter Marklund
+} {
+    email
+    password
+    {return_url "/"}
+}
+
+array set auth_info [auth::authenticate \
+                         -return_url $return_url \
+                         -email [string trim $email] \
+                         -password $password]
+
+if { [string equal $auth_info(auth_status) "ok"] } {
+    ad_returnredirect $return_url
+    ad_script_abort
+} else {
+    # Login problem - redirect to login form
+    ad_returnredirect [export_vars -base /register { email return_url }]    
+}
