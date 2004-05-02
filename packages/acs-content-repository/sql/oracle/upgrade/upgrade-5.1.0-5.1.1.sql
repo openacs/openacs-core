2574d916836bb605d94ec2aed02bb3dfcd2cb6ec
Dave Bauer <dave@thedesignexperience.org>
2004-05-02 19:13:06 +0000

    Fix content_folder.rename.

---
+++ packages/acs-content-repository/sql/oracle/upgrade/upgrade-5.1.0-5.1.1.sql
@@ -0,0 +1,10 @@
+-- 
+
+-- @author Dave Bauer (dave@thedesignexperience.org)
+-- @creation-date 2004-05-02
+-- @cvs-id $Id$
+--
+
+-- reload packages to get fixed version of content_folder.rename
+
+@@ ../packages-create.sql
