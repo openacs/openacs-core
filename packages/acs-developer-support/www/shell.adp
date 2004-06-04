bffd2c47b68037f67438aa7f975fe606ce59bb1c
Jeff Davis <davis@xarg.net>
2004-06-04 08:04:51 +0000

    add spaces around shell output in the pre so long lines still display, check that the script came from a POST before executing it.

--- packages/acs-developer-support/www/shell.adp
+++ packages/acs-developer-support/www/shell.adp
@@ -4,4 +4,10 @@
 
 <formtemplate id="shell"></formtemplate>
 <h3>Result</h3>
-<pre style="border: 1px solid black; padding: 4px">@result@</pre>
+<comment>The spaces below are intentional since in mozilla and some others extremely long strings in pre
+tags will not display properly</comment>
+<pre style="border: 1px solid black; padding: 4px">
+
+@out@
+
+</pre>
--- packages/acs-developer-support/www/shell.tcl
+++ packages/acs-developer-support/www/shell.tcl
@@ -1,32 +1,36 @@
 ad_page_contract {
-  @Author Nis Jorgensen
+    @Author Nis Jorgensen
 } {
-  {script:optional,allhtml {}}
-  } -properties {
-  result
+    {script:optional,allhtml {}}
+} -properties {
+    out
 }
 
 ds_require_permission [ad_conn package_id] "admin"
-
-set page_title "OpenACS Shell"
-set context $page_title
 
 if { ![acs_user::site_wide_admin_p] } {
     ad_return_warning "Error" "Sorry, only site-wide admins may use this."
     ad_script_abort
 }
 
-set result ""
+set page_title "OpenACS Shell"
+set context [list $page_title]
+
+set out {}
 
 ad_form -name shell -form {
     {
-      script:text(textarea),nospell
-      {label {Input tcl_script}}
-      {html {cols 80 rows 10}}
+        script:text(textarea),nospell
+        {label {Input tcl_script}}
+        {html {cols 80 rows 10}}
     }
-  } -on_submit {
-    if {[catch {set result [uplevel 1 $script]}]} {
-      global errorInfo
-      set result "ERROR:\n$errorInfo"
+} -on_submit {
+    if { ![string equal POST [ns_conn method]] } {
+        set out "You cannot use GET to invoke a script on this page.\nClick OK to resubmit the form as a POST."
+    } else {
+        if {[catch {set out [uplevel 1 $script]}]} {
+            global errorInfo
+            set out "ERROR:\n$errorInfo"
+        }
     }
-  }
+}
