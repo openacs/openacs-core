2ccf331632ca827246cc3238e6e1e8676c42858b
Jade Rubick <jade@rubick.com>
2004-08-04 16:13:00 +0000

    Resolves bug 2045: tcl/prase-procs.tcl spelling fix, courtesy of Frank Myhr.

--- packages/acs-templating/tcl/parse-procs.tcl
+++ packages/acs-templating/tcl/parse-procs.tcl
@@ -419,7 +419,7 @@ ad_proc -public template::adp_compile { source_type source } {
   regsub -all {([^0-9])%>} $chunk {\1</tcl>} chunk
   # warn about the first ambiguity in the source
   if [regexp {[0-9]+%>} $chunk match] {
-    ns_log warning "ambiguous '$match'; write tcl ecapes with a space like\
+    ns_log warning "ambiguous '$match'; write tcl escapes with a space like\
       <% set x 50 %> and HTML tags with proper quoting, like <hr width=\"50%\">\
       when compiling ADP source: template::adp_compile $source_type {$source}"
   }
