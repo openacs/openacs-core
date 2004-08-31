050ba4ced42d427912cedfa31f02cca808e0edfe
Don Baccus <dhogaza@pacifier.com>
2004-08-31 22:36:00 +0000

    Upgrade to track my earlier bug fix

--- packages/acs-content-repository/sql/postgresql/content-item.sql
+++ packages/acs-content-repository/sql/postgresql/content-item.sql
@@ -44,7 +44,7 @@ end;' language 'plpgsql' stable;
 
 -- new 19 param version of content_item__new
 
-select define_function_args('content_item__new','name,parent_id,item_id,locale,creation_date;now,creation_user,context_id,creation_ip,item_subtype;content_item,content_type;content_revision,title,description,mime_type;text/plain,nls_language,text,data,relation_tag,is_live;f,storage_type;lob');
+select define_function_args('content_item__new','name,parent_id,item_id,locale,creation_date;now,creation_user,context_id,creation_ip,item_subtype;content_item,content_type;content_revision,title,description,mime_type;text/plain,nls_language,text,data,relation_tag,is_live;f,storage_type;lob,package_id');
 
 create or replace function content_item__new (
   cr_items.name%TYPE,
