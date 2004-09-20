68888d4525196afc4ffb1479e15ef4f319d8bea2
Andrew Grumet <aegrumet@alum.mit.edu>
2004-09-20 20:21:20 +0000

    Modified cc_users moving the magic object lookup to its own query.  The view was really slow when joined against the acs_magic_objects table.

--- packages/acs-kernel/sql/oracle/acs-create.sql
+++ packages/acs-kernel/sql/oracle/acs-create.sql
@@ -193,13 +193,12 @@ pa.party_id, pa.email, pa.url,
 pe.person_id, pe.first_names, pe.last_name, 
 u.user_id,u.authority_id,username,u.password,u.salt,u.screen_name,u.priv_name,u.priv_email,u.email_verified_p,u.email_bouncing_p,u.no_alerts_until,u.last_visit,u.second_to_last_visit,u.n_sessions,u.password_question,u.password_answer,password_changed_date,
 mr.member_state, mr.rel_id
-from acs_objects o, parties pa, persons pe, users u, group_member_map m, membership_rels mr, acs_magic_objects mo
+from acs_objects o, parties pa, persons pe, users u, group_member_map m, membership_rels mr
 where o.object_id = pa.party_id
 and pa.party_id = pe.person_id
 and pe.person_id = u.user_id
 and u.user_id = m.member_id
-and m.group_id = mo.object_id
-and mo.name = 'registered_users'
+and m.group_id = (select acs.magic_object_id('registered_users') from dual)
 and m.rel_id = mr.rel_id
 and m.container_id = m.group_id
 and m.rel_type = 'membership_rel';
