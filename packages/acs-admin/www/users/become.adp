4611ce990c48d50db02259fe65be431de03c1e4d
Jose Pablo Escobedo Del Cid <jose.escobedo@int-edu.eu>
2004-10-13 18:34:00 +0000

    using the file in acs-admin/lib/become.*, so the user can become another user with the role of dotlrn-admin

---
+++ packages/acs-admin/www/users/become.adp
@@ -0,0 +1,2 @@
+
+<include src="/packages/acs-admin/lib/become" user_id=@user_id@>
--- packages/acs-admin/www/users/become.tcl
+++ packages/acs-admin/www/users/become.tcl
@@ -8,24 +8,3 @@ ad_page_contract {
     user_id
 }
 
-set return_url [ad_pvt_home]
-
-# Get the password and user ID
-
-if ![db_0or1row password "select password from users where user_id=$user_id"] {
-    ad_return_error "Couldn't find user $user_id" "Couldn't find user $user_id."
-    return
-}
-
-# just set a session cookie
-set expire_state "s"
-
-# note here that we stuff the cookie with the password from Oracle,
-# NOT what the user just typed (this is because we want log in to be
-# case-sensitive but subsequent comparisons are made on ns_crypt'ed 
-# values, where string toupper doesn't make sense)
-
-ad_user_login $user_id
-ad_returnredirect $return_url
-#ad_returnredirect "/cookie-chain.tcl?cookie_name=[ns_urlencode ad_auth]&cookie_value=[ad_encode_id $user_id $password]&expire_state=$expire_state&final_page=[ns_urlencode $return_url]"
-
