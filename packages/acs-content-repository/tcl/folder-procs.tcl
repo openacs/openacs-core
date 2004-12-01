7ba95c510741fa1caad6aef04508fa5d044c955d
Alfred Werner <alfred@thunderstick.com>
2004-12-01 05:11:01 +0000

    Deprecated in favor of new CR TCL API.

--- packages/acs-content-repository/tcl/folder-procs.tcl
+++ packages/acs-content-repository/tcl/folder-procs.tcl
@@ -7,14 +7,16 @@ ad_library {
 
 namespace eval folder {}
 
-ad_proc -public folder::delete {
+ad_proc -public -deprecated folder::delete {
     {-folder_id:required}
 } {
+    Deprecated. See content::folder::delete instead.
     Delete a content folder. If the folder
     to delete has children content items referencing it
     via acs_objects.context_id then this proc will fail.
 
     @author Peter Marklund
+    @see content::folder::delete
 } {
     db_exec_plsql delete_folder {}
 }
