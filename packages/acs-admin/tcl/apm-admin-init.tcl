15455799672c2835b7b4b8ba3f14271901778a69
Joel Aufrecht <joel@aufrecht.org>
2004-12-18 00:42:27 +0000

    converted build-repository.tcl to a proc and init

---
+++ packages/acs-admin/tcl/apm-admin-init.tcl
@@ -0,0 +1,15 @@
+ad_library {
+
+    Automated functions for acs-admin
+
+    @cvs-id $Id$
+
+}
+
+# Only run this if this is openacs.org
+#ad_schedule_proc -thread t -schedule_proc ns_schedule_daily [list 0 0] apm_build_repository
+
+
+
+
+
