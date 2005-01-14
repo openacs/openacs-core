5289441f76866af9cc71ee195733cf99ec7b36d2
Miguel Marin <miguelmmarin@viaro.net>
2005-01-14 14:36:05 +0000

    Addign apm callbacks for upgrade and for install to support email-image

---
+++ packages/acs-subsite/tcl/apm-callback-procs.tcl
@@ -0,0 +1,41 @@
+ad_library {
+    Installation procs for email-image management
+
+    @author Miguel Marin (miguelmarin@viaro.net) Viaro Networks (www.viaro.net)
+}
+
+namespace eval subsite {}
+
+ad_proc -private subsite::package_install {} {} {
+    set type_id [content::type::new -content_type "email_image" -pretty_name "Email_Image" \
+		 -pretty_plural "Email_Images" -table_name "users_email_image" -id_column "email_image_id"]
+
+    set folder_id [content::folder::new -name "Email_Images" -label "Email_Images"]
+
+    content::folder::register_content_type -folder_id $folder_id -content_type "email_image" 
+
+    rel_types::new email_image_rel "Email Image" "Email Images" user 0 1 content_item 0 1
+
+}
+
+ad_proc -public asubsite::after_upgrade {
+    {-from_version_name:required}
+    {-to_version_name:required}
+} {
+    apm_upgrade_logic \
+        -from_version_name $from_version_name \
+        -to_version_name $to_version_name \
+        -spec {
+            5.2.0d2 5.2.0d3 {
+		set type_id [content::type::new -content_type "email_image" -pretty_name "Email_Image" \
+		     -pretty_plural "Email_Images" -table_name "users_email_image" -id_column "email_image_id"]
+
+		set folder_id [content::folder::new -name "Email_Images" -label "Email_Images"]
+
+		content::folder::register_content_type -folder_id $folder_id -content_type "email_image" 
+		
+		rel_types::new email_image_rel "Email Image" "Email Images" user 0 1 content_item 0 1
+	    }
+	}
+}
+
