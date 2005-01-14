cfb10e89e597bb18dff1cc5da7cfbf0a867c00ad
Miguel Marin <miguelmmarin@viaro.net>
2005-01-14 14:46:40 +0000

    Shows user email-image

---
+++ packages/acs-subsite/www/shared/email-image-bits.tcl
@@ -0,0 +1,10 @@
+ad_page_contract {
+    spits out correctly MIME-typed bits for a user's email-image
+
+    @author Miguel Marin (miguelmarin@viaro.net) Viaro Networks (www.viaro.net)
+} {
+    revision_id:integer
+}
+
+cr_write_content -revision_id $revision_id
+
