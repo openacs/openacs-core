5e0b9ec4f4daf7b67887f9b7e21b7195d58afbf1
Dave Bauer <dave@thedesignexperience.org>
2005-01-21 20:41:56 +0000

    Remove pl/sql function from where clause.

--- packages/acs-content-repository/tcl/filter-procs.tcl
+++ packages/acs-content-repository/tcl/filter-procs.tcl
@@ -182,18 +182,24 @@ ad_proc -public content::init {
         return 0
     }
 
-    # cache this query persistently for 1 hour
-    db_0or1row get_item_info "" -column_array item_info
-
-    # No item found, so do not handle this request
-    if { ![info exists item_info] } {
-        db_0or1row get_template_info "" -column_array item_info
-
-        if { ![info exists item_info] } {
-            ns_log notice "content::init: no content found for url $url"
-            return 0 
-        }
-    }
+  set item_info(item_id) [::content::item::get_id -item_path $url \
+                   -root_folder_id $content_root \
+                   -resolve_index "f"]
+  set item_info(content_type) [::content::item::get_content_type \
+                        -item_id $item_id]
+  
+  # No item found, so do not handle this request
+  if { [string equal "" $item_info(item_id)] } { 
+      set item_info(item_id) [::content::item::get_id -item_path $url \
+                   -root_folder_id $content_root \
+                   -resolve_index "f"]
+      set item_info(content_type) [::content::item::get_content_type \
+                        -item_id $item_id]
+      if { [string equal "" $item_info(item_id)] } { 
+          ns_log notice "content::init: no content found for url $url"
+          return 0 
+      }
+  }
 
     variable item_url
     set item_url $url
