d6caef4fb67bc602e6ad6bfc5329b33e560b1e1d
Jeff Davis <davis@xarg.net>
2005-02-18 18:36:11 +0000

    fix the build_query function to be more in line with the lexer, sort by rank high to low, index title, keywords as weight A,B respectively

--- packages/tsearch2-driver/tcl/tsearch2-driver-procs.tcl
+++ packages/tsearch2-driver/tcl/tsearch2-driver-procs.tcl
@@ -1,9 +1,6 @@
-# 
-
 ad_library {
-    
     Procedures for tsearch full text enginge driver
-    
+
     @author Dave Bauer (dave@thedesignexperience.org)
     @creation-date 2004-06-05
     @arch-tag: 49a5102d-7c06-4245-8b8d-15a3b12a8cc5
@@ -18,44 +15,42 @@ ad_proc -public tsearch2::index {
     title
     keywords
 } {
-    
     add object to full text index
-    
+
     @author Dave Bauer (dave@thedesignexperience.org)
     @creation-date 2004-06-05
-    
+
     @param object_id
     @param txt
     @param title
     @param keywords
-    
-    @return 
-    
-    @error 
+
+    @return nothing
 } {
     set index_exists_p [db_0or1row object_exists "select 1 from txt where object_id=:object_id"]
     if {!$index_exists_p} {
-	db_dml index "insert into txt (object_id,fti) values ( :object_id, to_tsvector('default',:txt))"
-
+	db_dml index "
+            insert into txt (object_id,fti)
+            values ( :object_id,
+                     setweight(to_tsvector('default',coalesce(:title,'')),'A')
+                   ||setweight(to_tsvector('default',coalesce(:keywords,'')),'B')
+                   ||to_tsvector('default',coalesce(:txt,'')))"
     } else {
 	tsearch2::update_index $object_id $txt $title $keywords
     }
-	     
 }
 
 ad_proc -public tsearch2::unindex {
     object_id
 } {
     Remove item from FTS index
-    
+
     @author Dave Bauer (dave@thedesignexperience.org)
     @creation-date 2004-06-05
-    
+
     @param object_id
 
-    @return 
-    
-    @error 
+    @return nothing
 } {
     db_dml unindex "delete from txt where object_id=:object_id"
 }
@@ -66,28 +61,30 @@ ad_proc -public tsearch2::update_index {
     title
     keywords
 } {
-    
     update full text index
-    
+
     @author Dave Bauer (dave@thedesignexperience.org)
     @creation-date 2004-06-05
-    
+
     @param object_id
     @param txt
     @param title
     @param keywords
-    
-    @return 
-    
-    @error 
+
+    @return nothing
 } {
     set index_exists_p [db_0or1row object_exists "select 1 from txt where object_id=:object_id"]
     if {!$index_exists_p} {
-	db_dml index "insert into txt (object_id,fti) values ( :object_id, to_tsvector('default',:txt))"
+	tsearch2::index $object_id $txt $title $keywords
     } else {
-	db_dml update_index "update txt set fti = to_tsvector('default',:txt) where object_id=:object_id"
+	db_dml update_index "
+            update txt set fti =
+                     setweight(to_tsvector('default',coalesce(:title,'')),'A')
+                   ||setweight(to_tsvector('default',coalesce(:keywords,'')),'B')
+                   ||to_tsvector('default',coalesce(:txt,''))
+            where object_id=:object_id
+        "
     }
-	     
 }
 
 ad_proc -public tsearch2::search {
@@ -103,7 +100,7 @@ ad_proc -public tsearch2::search {
     
     @author Dave Bauer (dave@thedesignexperience.org)
     @creation-date 2004-06-05
-    
+
     @param query
 
     @param offset
@@ -116,9 +113,9 @@ ad_proc -public tsearch2::search {
 
     @param dt
 
-    @return 
-    
-    @error 
+    @return
+
+    @error
 } {
     # clean up query
     # turn and into &
@@ -128,6 +125,7 @@ ad_proc -public tsearch2::search {
 
     set limit_clause ""
     set offset_clause ""
+
     if {[string is integer $limit]} {
 	set limit_clause " limit :limit "
     }
@@ -138,7 +136,7 @@ ad_proc -public tsearch2::search {
                    from acs_object_party_privilege_map m
                    where m.object_id = txt.object_id
                      and m.party_id = :user_id
-                     and m.privilege = 'read') order by rank(fti,to_tsquery('default',:query))  ${limit_clause} ${offset_clause}"
+                     and m.privilege = 'read') order by rank(fti,to_tsquery('default',:query)) desc  ${limit_clause} ${offset_clause}"
     set results_ids [db_list search $query_text]
     set count [db_string count "select count(*) from txt where fti @@ to_tsquery('default',:query)  and exists
                   (select 1 from acs_object_party_privilege_map m
@@ -147,7 +145,7 @@ ad_proc -public tsearch2::search {
                      and m.privilege = 'read')"]
     set stop_words [list]
     # lovely the search package requires count to be returned but the
-    # service contract definition doesn't specify it! 
+    # service contract definition doesn't specify it!
     return [list ids $results_ids stopwords $stop_words count $count]
 }
 
@@ -155,18 +153,18 @@ ad_proc -public tsearch2::summary {
     query
     txt
 } {
-    
-    
+    Highlights matching terms.
+
     @author Dave Bauer (dave@thedesignexperience.org)
     @creation-date 2004-06-05
-    
+
     @param query
 
     @param txt
 
     @return summary containing search query terms
-    
-    @error 
+
+    @error
 } {
     set query [tsearch2::build_query -query $query]
    return [db_string summary "select headline('default',:txt,to_tsquery('default',:query))"]
@@ -196,14 +194,14 @@ ad_proc tsearch2::build_query { -query } {
     @return returns formatted query string for tsearch2 tsquery
 } {
     # get rid of everything that isn't a letter or number
-    regsub -all {[^?\d\w\s]} $query {} query
+    regsub -all {[^-/@.\d\w\s]+} $query { } query
 
     # replace boolean words with boolean operators
-    set query [string map {" and " & " or " | " not " !} $query]
+    set query [string map {" and " & " or " | " not " " ! "} " $query "]
     # remove leading and trailing spaces so they aren't turned into |
     set query [string trim $query]
     # remove any spaces between words and operators
-    regsub -all {\s+([!&|])\s+} $query {\1} query
+    regsub -all {\s*([!&|])\s+} $query {\1} query
     # all remaining spaces between words turn into |
     regsub -all {\s+} $query {\&} query
     # if a ! is by itself then prepend &
