6adb8179b6e89cc329c6edf549fdc1c659fc5a3c
Miguel Marin <miguelmmarin@viaro.net>
2005-02-28 18:22:35 +0000

    add page that allows to choose an assessment related to the registration process

--- packages/acs-subsite/www/admin/index.tcl
+++ packages/acs-subsite/www/admin/index.tcl
@@ -23,3 +23,13 @@ set acs_admin_url [apm_package_url_from_key "acs-admin"]
 array set acs_admin_node [site_node::get -url $acs_admin_url]
 set acs_admin_name $acs_admin_node(instance_name)
 set sw_admin_p [permission::permission_p -party_id [ad_conn user_id] -object_id $acs_admin_node(object_id) -privilege admin]
+set asm_p [apm_package_installed_p "assessment"]
+
+
+
+
+
+
+
+
+
