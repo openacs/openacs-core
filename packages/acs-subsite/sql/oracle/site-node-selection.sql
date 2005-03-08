13f2ca2ef9756a692d70db50ad743011afbe02af
Andrew Grumet <aegrumet@alum.mit.edu>
2005-03-08 19:19:46 +0000

    Bringing in a fix from HEAD.

--- packages/acs-subsite/sql/oracle/site-node-selection.sql
+++ packages/acs-subsite/sql/oracle/site-node-selection.sql
@@ -11,8 +11,9 @@ create table site_nodes_selection (
                         references acs_objects (object_id)
                         constraint site_node_sel_id_pk
                         primary key,
-	view_p		boolean
-
+	view_p		char(1)
+                constraint site_nodes_sel_view_p_ck
+                check (view_p in ('t','f'))
 );
 
 --show errors
