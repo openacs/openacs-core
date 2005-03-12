5c73c7cee248c3469080ca0189673d606a19ee4a
Andrew Grumet <aegrumet@alum.mit.edu>
2005-03-12 19:45:05 +0000

    Bugfix.

--- packages/acs-content-repository/sql/oracle/packages-create.sql
+++ packages/acs-content-repository/sql/oracle/packages-create.sql
@@ -1426,7 +1426,7 @@ function new (
   creation_user	in acs_objects.creation_user%TYPE
 			   default null,
   creation_ip	in acs_objects.creation_ip%TYPE default null,
-  package_id    in acs_objects.package_id%TYPE
+  package_id    in acs_objects.package_id%TYPE default null
 ) return cr_templates.template_id%TYPE;
 
 procedure del (
