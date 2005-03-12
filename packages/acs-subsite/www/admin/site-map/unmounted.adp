666ad741ee229de09b0841c037938369b12d11d7
Michael Steigman <michael@steigman.net>
2005-03-12 21:36:54 +0000

    added adp for umounted applications page

---
+++ packages/acs-subsite/www/admin/site-map/unmounted.adp
@@ -0,0 +1,30 @@
+<master>
+  <property name="title">@page_title@</property>
+  <property name="context">@context@</property>
+
+  The following application packages are not mounted anywhere.  You can delete an unmounted package by clicking the "delete" option.
+
+  <ul>
+   <if @packages_normal:rowcount@ ne 0>
+    <multiple name="packages_normal">
+     <li>@packages_normal.name@ [<a href="@packages_normal.instance_delete_url@" onclick="return confirm('Are you sure you want to delete package @packages_normal.name@');">delete</a>]</li>
+    </multiple>
+   </if>
+   <else>
+     <li>There are no unmounted packages</li>
+   </else>
+  </ul>
+
+  The following services are singleton packages and are usually not meant to be mounted anywhere.  Be careful not to delete a service that is in use as this can potentially break the system.
+
+  <ul>
+   <if @packages_singleton:rowcount@ ne 0>
+    <multiple name="packages_singleton">
+     <li>@packages_singleton.name@ [<a href="@packages_singleton.instance_delete_url@" onclick="return confirm('Are you sure you want to delete package @packages_singleton.name@');">delete</a>]</li>
+    </multiple>
+   </if>
+   <else>
+     <li>There are no unmounted singleton packages</li>
+   </else>
+  </ul>
+
--- packages/acs-subsite/www/admin/site-map/unmounted.tcl
+++ packages/acs-subsite/www/admin/site-map/unmounted.tcl
@@ -8,40 +8,14 @@ ad_page_contract {
 
 }
 
-doc_body_append "[ad_header "Unmounted Packages"]
-<h2>Unmounted Packages </h2>
-[ad_context_bar [list "index" "Site Map"] "Unmounted Packages"]
-<hr>
-
-The following application packages are not mounted anywhere.  You can delete an unmounted package 
-by clicking the \"delete\" option.  
-<ul>
-"
-
+set page_title "Unmounted Packages"
+set context [list [list "." "Site Map"] $page_title]
 set user_id [ad_conn user_id]
 
-db_foreach packages_normal_select {} {
-    doc_body_append "<li>$name \[<a href=\"instance-delete?[export_url_vars package_id]\" onclick=\"return confirm('Are you sure you want to delete package $name');\">delete</a>\]"
-} if_no_rows {
-    doc_body_append "<i>There are no unmounted packages</i>"
+db_multirow -extend {instance_delete_url} packages_normal packages_normal_select {} {
+    set instance_delete_url [export_vars -base instance-delete package_id]
 }
 
-doc_body_append "</ul> <p />
-
-The following services are singleton packages and are usually not meant to
-be mounted anywhere.  Be careful not to delete a service that is in use as this
-can potentially break the system.
-
-<ul>"
-
-db_foreach packages_singleton_select {} {
-    doc_body_append "<li>$name \[<a href=\"instance-delete?[export_url_vars package_id]\" onclick=\"return confirm('Are you sure you want to delete package $name');\">delete</a>\]"
-} if_no_rows {
-    doc_body_append "<i>There are no unmounted singleton packages</li>"
+db_multirow -extend {instance_delete_url} packages_singleton packages_singleton_select {} {
+    set instance_delete_url [export_vars -base instance-delete package_id]
 }
-
-doc_body_append "
-</ul>
-
-[ad_footer]
-"
