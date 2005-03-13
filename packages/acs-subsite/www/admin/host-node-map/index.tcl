3dbc72dcbd2e20c2fa77c354d821090f5390f1a5
Michael Steigman <michael@steigman.net>
2005-03-13 00:59:52 +0000

    templated host-node map; now uses listbuilder and ad_form

--- packages/acs-subsite/www/admin/host-node-map/index.tcl
+++ packages/acs-subsite/www/admin/host-node-map/index.tcl
@@ -1,73 +1,73 @@
 ad_page_contract {
     @author Mark Dettinger (mdettinger@arsdigita.com)
+    @author Michael Steigman (michael@steigman.net)
     @creation-date 2000-10-24
     @cvs-id $Id$
 } {
 }
 
-set table ""
-append table "<tr>
-<td>[ns_config ns/server/[ns_info server]/module/nssock Hostname]</td>
-<td>[db_string root_id  "select site_node.node_id('/') from dual"]</td>
-<td>/</td>
-<td>&nbsp;</td>
-</tr>"
-db_foreach host_node_pair {
-    select host, node_id, site_node.url(node_id) as url 
-    from host_node_map
-} {
-    append table "<tr>
-    <td><a href=\"http://$host\">$host</a></td>
-    <td>$node_id</td>
-    <td>$url</td>
-    <td><a href=delete?host=$host&node_id=$node_id>delete</a></td>
-    </tr>"
-} 
+set page_title "Host-Node Map"
+set context [list "Host-Node Map"]
 
-set nodes [list]
-set root_id [ad_conn node_id]
+template::list::create \
+    -name host_node_pairs \
+    -multirow host_node_pairs \
+    -key node_id \
+    -elements {
+	host {
+	    label "Hostname"
+	}
+	node_id {
+	    label "Root Node"
+	} 
+	url {
+	    label "Root URL"
+	}
+	delete_url {
+	    display_template "<if @host_node_pairs.delete_url@ not nil><a href=\"@host_node_pairs.delete_url@\" title=\"Delete this mapping\">delete</a></if>"
+	}
+    }
+	    
+template::multirow create host_node_pairs host node_id url delete_url
+template::multirow append host_node_pairs \
+    [ns_config ns/server/[ns_info server]/module/nssock Hostname] \
+    [db_string root_id  "select site_node.node_id('/') from dual"] \
+    "/" \
+    ""
+
+db_multirow -extend {delete_url} -append host_node_pairs select_host_node_pairs {} {
+    set delete_url [export_vars -base delete {host node_id}]
+}
+
 set node_list [list]
 foreach node_id [site_node::get_children -all -element node_id -node_id [site_node::get_node_id -url "/"]] { 
     lappend node_list [list [site_node::get_element -node_id $node_id -element url] $node_id]
 }
-foreach node [lsort $node_list] {
-    append nodes "<input type=radio name=root value=\"[lindex $node 1]\">[lindex $node 0]<br>\n"
+set sorted_node_list [lsort $node_list]
+
+ad_form -name add_host_node_mapping -form {
+    {host:text(text)
+	{label "Hostname"}
+	{html {size 40}}
+	{value "mydomain.com"}
+	{help_text "Hostname must be unique"}
+    }
+    {root:text(radio)
+	{label "Root Node"}
+	{options $sorted_node_list}
+	{help_text "Site node you would like to map hostname to"}
+    }
+    {submit:text(submit)
+	{label "Add Pair"}
+    }
+} -validate {
+    {host
+	{![db_string check_host "select 1 from host_node_map where host = :host" -default 0]}
+         "Host must be unique"
+    }
+} -on_submit {
+    util_memoize_flush_regexp "rp_lookup_node_from_host"
+    db_dml host_node_insert {}
+} -after_submit {
+    ad_returnredirect index
 }
-
-doc_body_append "
-[ad_header "Host-Node Map"]
-<h2>Host-Node Map</h2>
-[ad_context_bar "Host-Node Map"]
-<hr>
-<h3>Registered hostname/URL pairs</h3>
-<table border>
-<tr>
-  <th>Hostname</th>
-  <th>Root Node</th>
-  <th>Root URL</th>
-  <th>Action</th>
-</tr>
-$table
-</table>
-<h3>Add another hostname/URL pair</h3>
-<form method=get action=add>
-<table border>
-<tr>
-  <th>Hostname</th>
-  <th>Root URL</th>
-</tr>
-<tr>
-  <td valign=top><input type=text name=host value=myname.com></td>
-  <td>$nodes</td>
-</tr>
-</table>
-<input type=submit value=Add>
-</form>
-[ad_footer]
-"
-
-
-
-
-
-
--- packages/acs-subsite/www/admin/host-node-map/index.xql
+++ packages/acs-subsite/www/admin/host-node-map/index.xql
@@ -2,6 +2,17 @@
 
 <queryset>
 
+<fullquery name="host_node_insert">
+    <querytext>
+
+    insert into host_node_map 
+    (host, node_id)
+    values 
+    (:host, :root)
+
+    </querytext>
+</fullquery>
+
 <fullquery name="node_list">      
       <querytext>
           select node_id from site_nodes
