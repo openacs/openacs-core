3f0d346f4893018439b2e120305a743f915c7b7c
Andrew Grumet <aegrumet@alum.mit.edu>
2005-03-18 18:31:52 +0000

    Re-removing stray 'end', introduced by victorg's commit.

--- packages/acs-lang/sql/oracle/ad-locales.sql
+++ packages/acs-lang/sql/oracle/ad-locales.sql
@@ -276,6 +276,3 @@ insert into ad_locales
        (locale, label, language, country, nls_language, nls_territory,
         nls_charset, mime_charset, default_p, enabled_p)
  values ('ca_ES', 'Catalan (ES)', 'ca', 'ES', 'SPANISH',  'SPAIN','WE8DEC', 'ISO-8859-1', 't', 'f');
-
-end;
-
