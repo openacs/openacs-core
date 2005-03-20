05e9d7e6047b28ba7a6b3cfeb1ddad5ab664f473
Jeff Davis <davis@xarg.net>
2005-03-20 00:47:07 +0000

    add a navigation::package_admin callback and an implementation for forums and the lib include to generate html

---
+++ packages/acs-tcl/lib/actions.tcl
@@ -0,0 +1,30 @@
+# Generate an html version of the given package_id's admin actions.
+# expects to be in a conn.
+#
+# @param package_id
+# @param package_key
+# @param return_url
+
+set user_id [ad_conn user_id]
+
+multirow create actions type url_stub text title_text long_text
+
+if {![catch {
+    lindex [callback -catch -impl $package_key navigation::package_admin -user_id $user_id -package_id $package_id -return_url $return_url] 0
+} action_list]} {
+
+    foreach action $action_list {
+
+        if {[string equal [lindex $action 0] "LINK"]} {
+            foreach {type stub text title long} $action {break}
+            multirow append actions $type "$base_url$stub" $text $title $long
+
+        } elseif {[string equal [lindex $action 0] "SECTION"]} {
+            foreach {type title long} $action {break}
+            multirow append actions $type {} {} $title $long
+
+        } else {
+            error "actions.tcl: type [lindex $action 0] unknown"
+        }
+    }
+}
