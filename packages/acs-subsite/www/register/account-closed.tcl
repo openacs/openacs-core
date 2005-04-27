153fa0a56893c0733c70588d4f7d3505ebffdc58
Jeff Davis <davis@xarg.net>
2005-04-27 01:06:00 +0000

    doubled up title/text annoying

--- packages/acs-subsite/www/register/account-closed.tcl
+++ packages/acs-subsite/www/register/account-closed.tcl
@@ -1,11 +1,12 @@
 ad_page_contract {
     Inform the user that his/her account is closed
-    
+
     @cvs-id $Id$
 } {
     {message:html ""}
 }
 
-set page_title [ad_convert_to_text -html_p t -- $message]
+set page_title [_ acs-subsite.Account_closed]
+
 set context [list [_ "acs-kernel.common_Register"]]
 
