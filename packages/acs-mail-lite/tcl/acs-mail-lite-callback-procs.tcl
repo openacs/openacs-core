368f5ef12dcaf90c649c7a718bb7d2ab6fc5c2be
Timo Hentschel <timo@timohentschel.de>
2005-07-26 16:46:01 +0000

    added callback procs

---
+++ packages/acs-mail-lite/tcl/acs-mail-lite-callback-procs.tcl
@@ -0,0 +1,33 @@
+# packages/acs-mail-lite/tcl/acs-mail-lite-callback-procs.tcl
+
+ad_library {
+    
+    Callback procs for acs-mail-lite
+    
+    @author Malte Sussdorff (sussdorff@sussdorff.de)
+    @creation-date 2005-06-15
+    @arch-tag: d9aec4df-102d-4b0d-8d0e-3dc470dbe783
+    @cvs-id $Id$
+}
+
+ad_proc -public -callback acs_mail_lite::complex_send {
+    {-package_id:required}
+    {-from_party_id:required}
+    {-to_party_id:required}
+    {-body}
+    {-message_id:required}
+    {-subject}
+    {-object_id}
+    {-file_ids}
+} {
+}
+
+ad_proc -public -callback acs_mail_lite::send {
+    {-package_id:required}
+    {-from_party_id:required}
+    {-to_party_id:required}
+    {-body}
+    {-message_id:required}
+    {-subject}
+} {
+}
