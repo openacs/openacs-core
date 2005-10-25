3a960c09b03831e8a74be8c5eaffbd71c7fd4caa
Dave Bauer <dave@thedesignexperience.org>
2005-10-25 15:40:19 +0000

    Fix upgrade since we support proper upgrade from beta to release.

---
+++ packages/acs-content-repository/sql/postgresql/upgrade/upgrade-5.2.0b5-5.2.0b6.sql
@@ -0,0 +1,10 @@
+-- 
+-- 
+-- 
+-- @author Dave Bauer (dave@thedesignexperience.org)
+-- @creation-date 2005-10-25
+-- @arch-tag: 14af9d7e-7d34-4db0-aa12-6560f61d2142
+-- @cvs-id $Id$
+--
+
+select define_function_args('content_item__set_live_revision','revision_id,publish_status;ready');
