8a2c822d6b91e0ae716852a86f7637f4626304f0
Timo Hentschel <timo@timohentschel.de>
2005-12-14 22:11:04 +0000

    added new seperate table for application_data_links

--- packages/acs-kernel/sql/oracle/acs-relationships-create.sql
+++ packages/acs-kernel/sql/oracle/acs-relationships-create.sql
@@ -329,6 +329,32 @@ comment on table acs_rels is '
  acs_attributes.
 ';
 
+----------------------------
+-- Application Data Links --
+----------------------------
+
+create sequence acs_data_links_seq start with 1;
+
+create table acs_data_links (
+	rel_id		integer not null
+			constraint acs_data_links_rel_id_fk
+			constraint acs_data_links_pk primary key,
+	object_id_one	integer not null
+			constraint acs_data_links_one_fk
+			references acs_objects (object_id)
+                        on delete cascade,
+	object_id_two	integer not null
+			constraint acs_data_links_two_fk
+			references acs_objects (object_id)
+                        on delete cascade,
+	constraint acs_data_links_un unique
+	(object_id_one, object_id_two)
+);
+
+create index acs_data_links_id_one_idx on acs_data_links (object_id_one);
+create index acs_data_links_id_two_idx on acs_data_links (object_id_two);
+
+
 --------------
 -- TRIGGERS --
 --------------
