23f4d0f48a065c4cc458177fb9b4a381732ac0ca
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2006-06-21 06:34:30 +0000

    fixing bug #2905

--- packages/acs-lang/tcl/localization-procs.tcl
+++ packages/acs-lang/tcl/localization-procs.tcl
@@ -173,7 +173,7 @@ ad_proc -public lc_numeric {
     set grouping [lc_get -locale $locale "grouping"]
 
     # Fall back on en_US if grouping is not on valid format
-    if { ![string equal $locale en_US] && ![regexp {^[0-9 ]+$} $grouping] } {
+    if { $locale ne "en_US" && ![regexp {^[0-9 -]+$} $grouping] } {
         ns_log Warning "lc_numeric: acs-lang.localization-grouping key has invalid value $grouping for locale $locale"
         set sep ,
         set dec .
