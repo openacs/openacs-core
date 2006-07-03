c533e253cccc4795fd6a5ba7c26d3b17a0563720
Malte Sussdorff <openacs@sussdorff.de>
2006-07-03 12:29:50 +0000

    Added fast locale switch taken from categories

--- packages/acs-lang/www/admin/message-list.tcl
+++ packages/acs-lang/www/admin/message-list.tcl
@@ -152,3 +152,12 @@ multirow foreach show_opts {
     }
 }
 
+
+# Locale switch
+set languages [lang::system::get_locale_options]
+
+ad_form -name locale_form -action [ad_conn url] -export { tree_id category_id } -form {
+    {locale:text(select) {label "Language"} {value $locale} {options $languages}}
+}
+
+set form_vars [export_ns_set_vars form {locale form:mode form:id __confirmed_p __refreshing_p formbutton:ok} [ad_conn form]]
