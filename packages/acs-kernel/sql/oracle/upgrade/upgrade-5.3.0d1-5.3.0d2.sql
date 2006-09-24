41387b88eaf11dd48c45a14e8b04895c510aeb6e
Malte Sussdorff <openacs@sussdorff.de>
2006-09-24 08:16:37 +0000

    Bringing acs_object_types in an upgrade in sync with the same table when doing a installation

---
+++ packages/acs-kernel/sql/oracle/upgrade/upgrade-5.3.0d1-5.3.0d2.sql
@@ -0,0 +1,2 @@
+alter table acs_object_types modify table_name null;
+alter table acs_object_types modify id_column null;
---
+++ packages/acs-kernel/sql/postgresql/upgrade/upgrade-5.3.0d1-5.3.0d2.sql
@@ -0,0 +1,2 @@
+alter table acs_object_types alter column table_name drop not null;
+alter table acs_object_types alter column id_column drop not null;
