e1326b3d456594335025d81552fb4620dd8b6c7f
Dave Bauer <dave@thedesignexperience.org>
2006-10-06 14:45:21 +0000

    Adding basic test.

---
+++ packages/acs-tcl/tcl/test/html-email-procs.tcl
@@ -0,0 +1,17 @@
+ad_library {
+    Test html email procs
+}
+
+aa_register_case -cats {api smoke} build_mime_message {
+    Basic test of build mime mesage
+} {
+    aa_false "Build mime message, no error" \
+	[catch {build_mime_message \
+		    "Test Mesage" \
+		    "<p>Test Message</p>"} errmsg]
+
+    aa_false "Package require mime package found" \
+	[catch {package require mime} errmsg]
+
+
+}
