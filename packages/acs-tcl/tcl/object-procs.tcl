242208fcea1677679bbea07b6bcda6b7fc9afbc9
Malte Sussdorff <openacs@sussdorff.de>
2006-11-01 19:25:19 +0000

    Caching

--- packages/acs-tcl/tcl/object-procs.tcl
+++ packages/acs-tcl/tcl/object-procs.tcl
@@ -78,6 +78,36 @@ ad_proc -public acs_object::get {
     db_1row select_object {} -column_array row
 }
 
+ad_proc -public acs_object::package_id {
+    {-object_id:required}
+} {
+    Gets the package_id of the object
+
+    @author Malte Sussdorff (malte.sussdorff@cognovis.de)
+    @creation-date 2006-08-10
+    
+    @param object_id the object to get the package_id for
+    
+    @return package_id of the object. Empty string if the package_id is not stored
+} {
+    return [util_memoize [list acs_object::package_id_not_cached -object_id $object_id]]
+}
+
+ad_proc -public acs_object::package_id_not_cached {
+    {-object_id:required}
+} {
+    Gets the package_id of the object
+
+    @author Malte Sussdorff (malte.sussdorff@cognovis.de)
+    @creation-date 2006-08-10
+    
+    @param object_id the object to get the package_id for
+    
+    @return package_id of the object. Empty string if the package_id is not stored
+} {
+    return [db_string get_package_id {} -default ""]
+}
+
 
 ad_proc -public acs_object::get_element { 
     {-object_id:required}
