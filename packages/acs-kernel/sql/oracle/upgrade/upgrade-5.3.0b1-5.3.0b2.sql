492b2b244db95eff833ed90eaa155c9000cca4ed
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2007-04-16 07:02:12 +0000

    create indices conditionally in upgrade scripts (many thanks to daveb for the snippets for oracle and postres)

--- packages/acs-kernel/sql/oracle/upgrade/upgrade-5.3.0b1-5.3.0b2.sql
+++ packages/acs-kernel/sql/oracle/upgrade/upgrade-5.3.0b1-5.3.0b2.sql
@@ -1,2 +1,12 @@
-create index acs_permissions_object_id_idx on acs_permissions(object_id); 
+declare v_exists integer;
+
+begin
+select count(*) into v_exists from user_indexes where lower(index_name)='acs_permissions_object_id_idx';
+if v_exists = 0 then
+    execute immediate 'create index acs_permissions_object_id_idx on acs_permissions(object_id)';
+end if;
+
+end;
+/
+show errors
 
