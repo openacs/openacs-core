569fe480e36038e65bfdce688858bcdb246a0526
Malte Sussdorff <openacs@sussdorff.de>
2007-04-18 09:13:54 +0000

    Made sure that you are only sending if the to_addr is not empty after working on it

--- packages/acs-mail-lite/tcl/acs-mail-lite-init.tcl
+++ packages/acs-mail-lite/tcl/acs-mail-lite-init.tcl
@@ -15,7 +15,7 @@ ad_schedule_proc -thread t 180 acs_mail_lite::complex_sweeper
 
 set queue_dir [parameter::get_from_package_key -parameter "BounceMailDir" -package_key "acs-mail-lite"]
 
-ad_schedule_proc -thread t 120 acs_mail_lite::load_mails -queue_dir $queue_dir
+# ad_schedule_proc -thread t 120 acs_mail_lite::load_mails -queue_dir $queue_dir
 
 # check every few minutes for bounces
 #ad_schedule_proc -thread t [acs_mail_lite::get_parameter -name BounceScanQueue -default 120] acs_mail_lite::scan_replies
@@ -23,4 +23,4 @@ ad_schedule_proc -thread t 120 acs_mail_lite::load_mails -queue_dir $queue_dir
 nsv_set acs_mail_lite send_mails_p 0
 nsv_set acs_mail_lite check_bounce_p 0
 
-ad_schedule_proc -thread t -schedule_proc ns_schedule_daily [list 0 25] acs_mail_lite::check_bounces
+# ad_schedule_proc -thread t -schedule_proc ns_schedule_daily [list 0 25] acs_mail_lite::check_bounces
