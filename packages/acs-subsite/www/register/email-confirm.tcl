b8426c87081f89f2355c8360fe8eef629d673096
Malte Sussdorff <openacs@sussdorff.de>
2007-05-29 05:40:54 +0000

    Support for EmailConfirm template

--- packages/acs-subsite/www/register/email-confirm.tcl
+++ packages/acs-subsite/www/register/email-confirm.tcl
@@ -1,22 +1,17 @@
 ad_page_contract {
-    @cvs-id $Id$
+    Page for users to register themselves on the site.
 
+    @cvs-id $Id$
 } {
     token:notnull,trim
     user_id:integer
+    
+    {return_url ""}
 }
 
-if {![db_0or1row userp {select 1 from users where user_id = :user_id}]
-    || $token ne [auth::get_user_secret_token -user_id $user_id] } {
-    set title "Bad token"
-    set message "The link given to authenticate your email was invalid."
-    ad_return_template /packages/acs-subsite/lib/message
-} else {
-    auth::set_email_verified -user_id $user_id
+set subsite_id [ad_conn subsite_id]
+set email_confirm_template [parameter::get -parameter "EmailConfirmTemplate" -package_id $subsite_id]
 
-    acs_user::get -user_id $user_id -array user_info
-
-    set export_vars [export_vars -form { { username $user_info(username) } }]
-    set site_link [ad_site_home_link]
-    set system_name [ad_system_name]
+if {$email_confirm_template eq ""} {
+    set email_confirm_template "/packages/acs-subsite/lib/email-confirm"
 }
