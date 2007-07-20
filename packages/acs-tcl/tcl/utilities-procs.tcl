41664e812640a9494d3d775709369d83617f1bd8
Lee Denison <lee@thaum.net>
2007-07-20 12:48:44 +0000

    Fixed file handle leakage in util_http_file_upload when the connection is refused by the remote server.

--- packages/acs-tcl/tcl/utilities-procs.tcl
+++ packages/acs-tcl/tcl/utilities-procs.tcl
@@ -3319,7 +3319,17 @@ ad_proc -public util_http_file_upload { -file -data -binary:boolean -filename
             global errorInfo
             return -code error -errorinfo $errorInfo $errMsg
         }
-    } errmsg] } {return -1}
+    } errmsg] } {
+        if {[lsearch [file channels] $wfd] >= 0} {
+            close $wfd
+        }
+
+        if {[lsearch [file channels] $rfd] >= 0} {
+            close $rfd
+        }
+
+        set page -1
+    }
     
     return $page
 }
