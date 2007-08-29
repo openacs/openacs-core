bbf9dfdb706c7145da6fe33738b79a2d16940a78
Malte Sussdorff <openacs@sussdorff.de>
2007-08-29 12:58:51 +0000

    Fixing Oracle

--- packages/acs-mail-lite/sql/oracle/acs-mail-lite-create.sql
+++ packages/acs-mail-lite/sql/oracle/acs-mail-lite-create.sql
@@ -63,8 +63,8 @@ CREATE TABLE acs_mail_lite_complex_queue (
     id 				integer
                                 constraint acs_mail_lite_complex_queue_pk 
 				PRIMARY KEY,
-    creation_date 		text,
-    locking_server 		text,
+    creation_date 		varchar(4000),
+    locking_server 		varchar(4000),
     to_party_ids 		varchar(4000),
     cc_party_ids 		varchar(4000),
     bcc_party_ids 		varchar(4000),
@@ -85,16 +85,16 @@ CREATE TABLE acs_mail_lite_complex_queue (
     mime_type 			varchar(200),
     object_id 			integer,
     single_email_p 		varchar2(1)
-				constraint acs_mail_lite_co_qu_single_em_p_ck
-				check (valid_email_p in ('t','f')),
+				constraint aml_co_qu_single_em_p_ck
+				check (single_email_p in ('t','f')),
     no_callback_p 		varchar2(1)
-				constraint acs_mail_lite_co_qu_no_callb_p_ck
-				check (valid_email_p in ('t','f')),
+				constraint aml_co_qu_no_callb_p_ck
+				check (no_callback_p in ('t','f')),
     extraheaders 		clob,
     alternative_part_p		varchar2(1)
-				constraint acs_mail_lite_co_qu_alt_part_p_ck
-				check (valid_email_p in ('t','f')),
+				constraint aml_co_qu_alt_part_p_ck
+				check (alternative_part_p in ('t','f')),
     use_sender_p 		varchar2(1)
-				constraint acs_mail_lite_co_qu_use_sender_p_ck
-				check (valid_email_p in ('t','f'))
+				constraint aml_co_qu_use_sender_p_ck
+				check (use_sender_p in ('t','f'))
 );
