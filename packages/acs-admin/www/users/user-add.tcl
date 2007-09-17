48357bdde333e887dd6f56ddce19375a18faccf9
Malte Sussdorff <openacs@sussdorff.de>
2007-09-17 13:26:58 +0000

    Made sure we are using the right user-new template

--- packages/acs-admin/www/users/user-add.tcl
+++ packages/acs-admin/www/users/user-add.tcl
@@ -12,3 +12,11 @@ ad_page_contract {
 set context [list [list "." "Users"] "Add user"]
 
 set next_url user-add-2
+
+
+set subsite_id [ad_conn subsite_id]
+set user_new_template [parameter::get -parameter "UserNewTemplate" -package_id $subsite_id]
+
+if {$user_new_template eq ""} {
+    set user_new_template "/packages/acs-subsite/lib/user-new"
+}
