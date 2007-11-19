c4a1120b94a0348f9fbe1cdbb239373faba711c1
Emmanuelle Raffenne <eraffenne@gmail.com>
2007-11-19 19:20:15 +0000

    Added converter for open docs. Removed path for command passed to exec. Removed unused proc choice_bar

--- packages/search/tcl/search-convert-procs.tcl
+++ packages/search/tcl/search-convert-procs.tcl
@@ -26,36 +26,46 @@ ad_proc -public search::convert::binary_to_text {
     set result ""
 
     switch $mime_type {
-	{application/msword} -
-	{application/vnd.ms-word} {
-	    set convert_command {/usr/local/bin/catdoc $filename >$tmp_filename}
-	}
-	{application/msexcel} -
-	{application/vnd.ms-excel} {
-	    set convert_command {/usr/local/bin/xls2csv $filename >$tmp_filename}
-	}
-	{application/mspowerpoint} -
-	{application/vnd.ms-powerpoint} {
-	    set convert_command {/usr/local/bin/ppthtml $filename >$tmp_filename}
-	}
-	{application/pdf} {
-	    set convert_command {/usr/local/bin/pdftotext $filename $tmp_filename}
-	}
-	{text/html} {
-	    return [catch {[ns_striphtml $filename]} error]
-	}
-	default {
-	    # If there's nothing implemented for a particular mime type
+        {application/msword} -
+        {application/vnd.ms-word} {
+            set convert_command {catdoc $filename >$tmp_filename}
+        }
+        {application/msexcel} -
+        {application/vnd.ms-excel} {
+            set convert_command {xls2csv $filename >$tmp_filename}
+        }
+        {application/mspowerpoint} -
+        {application/vnd.ms-powerpoint} {
+            set convert_command {ppthtml $filename >$tmp_filename}
+        }
+        {application/pdf} {
+            set convert_command {pdftotext $filename $tmp_filename}
+        }
+        {application/vnd.oasis.opendocument.text} -
+        {application/vnd.oasis.opendocument.text-template} -
+        {application/vnd.oasis.opendocument.text-web} -
+        {application/vnd.oasis.opendocument.text-master} -
+        {application/vnd.oasis.opendocument.presentation} -
+        {application/vnd.oasis.opendocument.presentation-template} -
+        {application/vnd.oasis.opendocument.spreadsheet} -
+        {application/vnd.oasis.opendocument.spreadsheet-template} {
+            set convert_command {unzip -p $filename content.xml >$tmp_filename}
+        }
+        {text/html} {
+            return [catch {[ns_striphtml $filename]} error]
+        }
+        default {
+            # If there's nothing implemented for a particular mime type
             # we'll just index filename and pathname
             return ""
-	}
+        }
     }
 
     if {[catch {eval exec $convert_command} err]} {
-	catch {file delete $tmp_filename}
-	puts "DIRK conversion failed - $convert_command: $err"
-	file delete $tmp_filename
-	return
+        catch {file delete $tmp_filename}
+        ns_log Error "SEARCH: conversion failed - $convert_command: $err"
+        file delete $tmp_filename
+        return
     }
 
     set fd [open $tmp_filename "r"]
