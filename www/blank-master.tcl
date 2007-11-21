d134bbda306a03b8c05dca8c2847f87f1d217e4a
Emmanuelle Raffenne <eraffenne@gmail.com>
2007-11-21 14:07:32 +0000

    Set HTML 4.01 Strict as default doctype

--- www/blank-master.tcl
+++ www/blank-master.tcl
@@ -60,7 +60,7 @@ ad_page_contract {
 }
 
 if {[template::util::is_nil doc(type)]} { 
-    set doc(type) {<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">}
+    set doc(type) {<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">}
 }
 
 if {![info exists doc(title)]} {
