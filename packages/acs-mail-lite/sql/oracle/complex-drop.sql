93076b1f65fd3e8fde94fc03a2008bd0b71cff70
Emmanuelle Raffenne <eraffenne@gmail.com>
2008-01-09 14:35:12 +0000

    Move complex stuff to separated scripts

---
+++ packages/acs-mail-lite/sql/oracle/complex-drop.sql
@@ -0,0 +1,9 @@
+-- 
+-- packages/acs-mail-lite/sql/oracle/complex-drop.sql
+-- 
+-- @creation-date 2008-01-09
+-- @arch-tag: d43373a6-963b-4096-9ed6-0370f64c71e7
+-- @cvs-id $Id$
+--
+
+drop TABLE acs_mail_lite_complex_queue;
--- packages/acs-mail-lite/sql/postgresql/acs-mail-lite-drop.sql
+++ packages/acs-mail-lite/sql/postgresql/acs-mail-lite-drop.sql
@@ -10,4 +10,5 @@ drop sequence acs_mail_lite_id_seq;
 drop table acs_mail_lite_mail_log; 
 drop table acs_mail_lite_bounce; 
 drop table acs_mail_lite_bounce_notif;
-drop TABLE acs_mail_lite_complex_queue;
+
+\i complex-drop.sql
