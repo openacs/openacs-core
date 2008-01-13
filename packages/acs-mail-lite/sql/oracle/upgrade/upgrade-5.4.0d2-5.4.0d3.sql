8062f24e07c796a017dd803e905fb1d508c93311
Don Baccus <dhogaza@pacifier.com>
2008-01-13 11:25:23 +0000

    Fixed oracle syntax - add doesn't allow "column", unlike PG

--- packages/acs-mail-lite/sql/oracle/upgrade/upgrade-5.4.0d2-5.4.0d3.sql
+++ packages/acs-mail-lite/sql/oracle/upgrade/upgrade-5.4.0d2-5.4.0d3.sql
@@ -5,17 +5,17 @@
 
 -- new columns
 
-alter table acs_mail_lite_queue add column  creation_date       varchar(4000);
-alter table acs_mail_lite_queue add column  locking_server      varchar(4000);
-alter table acs_mail_lite_queue add column  cc_addr             clob;
-alter table acs_mail_lite_queue add column  reply_to            varchar(400);
-alter table acs_mail_lite_queue add column  file_ids            varchar(4000);
-alter table acs_mail_lite_queue add column  mime_type           varchar(200);
-alter table acs_mail_lite_queue add column  object_id           integer;
-alter table acs_mail_lite_queue add column  no_callback_p       char(1)
+alter table acs_mail_lite_queue add  creation_date       varchar(4000);
+alter table acs_mail_lite_queue add  locking_server      varchar(4000);
+alter table acs_mail_lite_queue add  cc_addr             clob;
+alter table acs_mail_lite_queue add  reply_to            varchar(400);
+alter table acs_mail_lite_queue add  file_ids            varchar(4000);
+alter table acs_mail_lite_queue add  mime_type           varchar(200);
+alter table acs_mail_lite_queue add  object_id           integer;
+alter table acs_mail_lite_queue add  no_callback_p       char(1)
                                             constraint amlq_no_callback_p_ck
                                             check (no_callback_p in ('t','f'));
-alter table acs_mail_lite_queue add column  use_sender_p        char(1)
+alter table acs_mail_lite_queue add  use_sender_p        char(1)
                                             constraint amlq_use_sender_p_ck
                                             check (use_sender_p in ('t','f'));
 
