976618eb4b44363cdf0b6bd99638e38ab5023dd9
Dave Bauer <dave@thedesignexperience.org>
2008-01-23 19:12:58 +0000

    Remove historical debugging log statement! (finally)

--- packages/acs-tcl/tcl/00-database-procs.tcl
+++ packages/acs-tcl/tcl/00-database-procs.tcl
@@ -2551,7 +2551,7 @@ ad_proc -public db_source_sql_file {{
             }
 
             cd [file dirname $file]
-            ns_log notice "\n DAVEB pghost = '${pghost}' pgport = '${pgport}' pguser = '${pguser}' \n"
+
             if { $tcl_platform(platform) eq "windows" } {
                 set fp [open "|[file join [db_get_pgbin] psql] $pghost $pgport $pguser -f $file_name [db_get_database] $pgpass" "r"]
             } else {
