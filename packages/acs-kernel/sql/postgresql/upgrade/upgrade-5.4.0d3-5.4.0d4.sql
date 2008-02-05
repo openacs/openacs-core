8b7c6c04d34d67664f0563c9b799a550aa2e55e0
Nima Mazloumi <nima.mazloumi@gmx.de>
2008-02-05 14:26:52 +0000

    Added upgrade for missing cascade part for site_node_object_mappings

--- packages/acs-kernel/sql/postgresql/upgrade/upgrade-5.4.0d3-5.4.0d4.sql
+++ packages/acs-kernel/sql/postgresql/upgrade/upgrade-5.4.0d3-5.4.0d4.sql
@@ -1,1 +1,5 @@
 select acs_privilege__add_child('admin', 'annotate');
+
+-- adding missing cascade part
+ALTER TABLE site_node_object_mappings DROP CONSTRAINT snom_object_id_fk;
+ALTER TABLE site_node_object_mappings ADD CONSTRAINT snom_object_id_fk FOREIGN KEY (object_id) REFERENCES acs_objects (object_id) ON UPDATE CASCADE ON DELETE CASCADE;
