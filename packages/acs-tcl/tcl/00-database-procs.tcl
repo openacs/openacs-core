2ea62ab33153d79f97393905b6821da4b3221bc6
Don Baccus <dhogaza@pacifier.com>
2008-07-03 08:40:46 +0000

    Got rid of noisesome notice ...

--- packages/acs-tcl/tcl/00-database-procs.tcl
+++ packages/acs-tcl/tcl/00-database-procs.tcl
@@ -2576,7 +2576,6 @@ ad_proc -public db_source_sql_file {{
             }
 
             cd [file dirname $file]
-            ns_log notice "\n DAVEB pghost = '${pghost}' pgport = '${pgport}' pguser = '${pguser}' \n"
             if { $tcl_platform(platform) eq "windows" } {
                 set fp [open "|[file join [db_get_pgbin] psql] $pghost $pgport $pguser -f $file_name [db_get_database] $pgpass" "r"]
             } else {
