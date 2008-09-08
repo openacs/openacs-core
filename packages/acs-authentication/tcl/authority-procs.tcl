2255f694b487f3dcd0c74c426035eaf3eecb977d
Dave Bauer <dave@thedesignexperience.org>
2008-09-08 20:14:58 +0000

    Add search service contract for acs-authenticaton. TIP#125

--- packages/acs-authentication/tcl/authority-procs.tcl
+++ packages/acs-authentication/tcl/authority-procs.tcl
@@ -449,9 +449,11 @@ ad_proc -private auth::authority::get_column_defaults {} {
         register_impl_id ""
         register_url ""
         user_info_impl_id ""
+        search_impl_id ""
         get_doc_impl_id ""
         process_doc_impl_id ""
         batch_sync_enabled_p "f"
+        allow_user_entered_info_p "f"
     }
 }
 
@@ -473,7 +475,7 @@ ad_proc -private auth::authority::get_sc_impl_columns {} {
 
     @author Peter Marklund 
 } {
-    return {auth_impl_id pwd_impl_id register_impl_id user_info_impl_id get_doc_impl_id process_doc_impl_id}
+    return {auth_impl_id pwd_impl_id register_impl_id user_info_impl_id search_impl_id get_doc_impl_id process_doc_impl_id}
 }
 
 ad_proc -private auth::authority::get_select_columns {} {
@@ -481,7 +483,7 @@ ad_proc -private auth::authority::get_select_columns {} {
     
     @author Lars Pind (lars@collaboraid.biz)
 } {
-    return [concat [get_columns] auth_impl_name pwd_impl_name register_impl_name user_info_impl_name get_doc_impl_name process_doc_impl_name]
+    return [concat [get_columns] auth_impl_name pwd_impl_name register_impl_name user_info_impl_name get_search_impl_name get_doc_impl_name process_doc_impl_name]
 }
 
 
@@ -512,6 +514,7 @@ ad_proc -private auth::authority::get_not_cached {
     lappend columns "(select impl_pretty_name from acs_sc_impls where impl_id = pwd_impl_id) as pwd_impl_name"
     lappend columns "(select impl_pretty_name from acs_sc_impls where impl_id = register_impl_id) as register_impl_name"
     lappend columns "(select impl_pretty_name from acs_sc_impls where impl_id = user_info_impl_id) as user_info_impl_name"
+    lappend columns "(select impl_pretty_name from acs_sc_impls where impl_id = search_impl_id) as search_impl_name"
     lappend columns "(select impl_pretty_name from acs_sc_impls where impl_id = get_doc_impl_id) as get_doc_impl_name"
     lappend columns "(select impl_pretty_name from acs_sc_impls where impl_id = process_doc_impl_id) as process_doc_impl_name"
 
