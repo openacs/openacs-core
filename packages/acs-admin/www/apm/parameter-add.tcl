cab0b27d5c4bf64a007f1c8a1eb604d4a945761f
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2009-01-23 08:29:38 +0000

    fix for yet another occurance of: adding missing FROM-clause entry for table "acs_object_id_seq" ...

--- packages/acs-admin/www/apm/parameter-add.tcl
+++ packages/acs-admin/www/apm/parameter-add.tcl
@@ -10,10 +10,11 @@ ad_page_contract {
 
 set user_id [ad_get_user_id]
 
+set parameter_id [db_nextval acs_object_id_seq]
 db_1row apm_get_name { 
-    select package_key, pretty_name, version_name, acs_object_id_seq.nextval as parameter_id
-      from apm_package_version_info
-     where version_id = :version_id
+  select package_key, pretty_name, version_name
+  from apm_package_version_info
+  where version_id = :version_id
 }
 db_release_unused_handles
 
