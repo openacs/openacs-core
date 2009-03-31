c721cda467a1816d5734f885f0a13797edd2a01a
Emmanuelle Raffenne <eraffenne@gmail.com>
2009-03-31 14:50:08 +0000

    Added message to inform the user that her status has been changed

--- packages/acs-subsite/www/shared/make-invisible.tcl
+++ packages/acs-subsite/www/shared/make-invisible.tcl
@@ -1,10 +1,16 @@
 ad_page_contract {
     Make user invisible.
+} {
+    {return_url ""}
 }
 
 auth::require_login
 
 whos_online::set_invisible [ad_conn user_id]
 
-ad_returnredirect [ad_pvt_home]
+if { $return_url eq "" } {
+    set return_url [ad_pvt_home]
+}
+
+ad_returnredirect -message [_ acs-subsite.Online_status_set_invisible] -- $return_url
 
