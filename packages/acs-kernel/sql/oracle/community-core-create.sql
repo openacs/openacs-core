15cd493f5f846b6f1b0cb2c1bcca9a7a312747e9
Don Baccus <dhogaza@pacifier.com>
2009-05-31 00:58:33 +0000

    Added attributes for "user" columns "screen_name" and "username", as well as the new "bio" column which replaces the old generic storage attribute assigned to "person".

--- packages/acs-kernel/sql/oracle/community-core-create.sql
+++ packages/acs-kernel/sql/oracle/community-core-create.sql
@@ -198,6 +198,36 @@ begin
    package_name => 'acs_user'
  );
 
+ attr_id := acs_attribute.create_attribute (
+        object_type => 'user',
+        attribute_name => 'bio',
+        datatype => 'string',
+        pretty_name => '#acs-kernel.Bio#',
+        pretty_plural => '#acs-kernel.Bios#',
+	min_n_values => 0,
+	max_n_values => 1
+      );
+
+ attr_id := acs_attribute.create_attribute (
+        object_type => 'user',
+        attribute_name => 'username',
+        datatype => 'string',
+        pretty_name => '#acs-kernel.Username#',
+        pretty_plural => '#acs-kernel.Usernames#',
+	min_n_values => 0,
+	max_n_values => 1
+      );
+
+ attr_id := acs_attribute.create_attribute (
+        object_type => 'user',
+        attribute_name => 'screen_name',
+        datatype => 'string',
+        pretty_name => '#acs-kernel.Screen_Name#',
+        pretty_plural => '#acs-kernel.Screen_Names#',
+	min_n_values => 0,
+	max_n_values => 1
+      );
+
  commit;
 end;
 /
