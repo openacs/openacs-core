735bda4b7471826d51b7a03b70d5dfeeb7e0f123
Don Baccus <dhogaza@pacifier.com>
2009-05-31 01:00:02 +0000

    One more upgrade ...

---
+++ packages/acs-kernel/sql/postgresql/upgrade/upgrade-5.6.0d2-5.6.0d3.sql
@@ -0,0 +1,59 @@
+create function inline_0 ()
+returns integer as '
+begin
+
+  perform acs_attribute__create_attribute (
+        ''user'',
+        ''username'',
+        ''string'',
+        ''#acs-kernel.Username#'',
+        ''#acs-kernel.Usernames#'',
+        null,
+        null,
+        null,
+	0,
+	1,
+        null,
+        ''type_specific'',
+        ''f''
+      );
+
+  perform acs_attribute__create_attribute (
+        ''user'',
+        ''screen_name'',
+        ''string'',
+        ''#acs-kernel.Screen_Name#'',
+        ''#acs-kernel.Screen_Names#'',
+        null,
+        null,
+        null,
+	0,
+	1,
+        null,
+        ''type_specific'',
+        ''f''
+      );
+
+  perform acs_attribute__create_attribute (
+          ''user'',
+          ''bio'',
+          ''string'',
+          ''#acs-kernel.Bio#'',
+          ''#acs-kernel.Bios#'',
+          null,
+          null,
+          null,
+  	0,
+  	1,
+          null,
+          ''type_specific'',
+          ''f''
+        );
+
+  return 0;
+
+end;' language 'plpgsql';
+
+select inline_0 ();
+
+drop function inline_0 ();
