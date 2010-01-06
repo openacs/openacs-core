ddadb279bf676871a0f748faa0e310492933b414
Ryan Gallimore <ryan.gallimore@quest.ie>
2010-01-06 20:07:24 +0000

    add three column constraint to acs_data_links

--- packages/acs-kernel/sql/oracle/upgrade/upgrade-5.6.0d3-5.6.0d4.sql
+++ packages/acs-kernel/sql/oracle/upgrade/upgrade-5.6.0d3-5.6.0d4.sql
@@ -4,6 +4,5 @@ create index acs_data_links_rel_tag_idx on acs_data_links (relation_tag);
 
 alter table acs_data_links drop constraint acs_data_links_un;
 
-create unique index acs_data_links_un on acs_data_links (
-  object_id_one, object_id_two, relation_tag
-);
+alter table acs_data_links add constraint acs_data_links_un unique 
+(object_id_one, object_id_two, relation_tag);
