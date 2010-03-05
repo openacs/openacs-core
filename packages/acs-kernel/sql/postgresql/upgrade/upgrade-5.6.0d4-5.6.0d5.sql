8f98420c81720a77f280103b10d9cb5beb8b1043
Victor Guerra <vguerra@gmail.com>
2010-03-05 12:22:07 +0000

    Fixing constraint snom_node_id_fk, was connecting wrong field on table. Creation scripts are ok, just needed to correct upgrade files.

---
+++ packages/acs-kernel/sql/postgresql/upgrade/upgrade-5.6.0d4-5.6.0d5.sql
@@ -0,0 +1,3 @@
+-- fixing snom_node_id_fk
+ALTER TABLE site_node_object_mappings DROP CONSTRAINT snom_node_id_fk;
+ALTER TABLE site_node_object_mappings ADD CONSTRAINT snom_node_id_fk FOREIGN KEY (node_id) REFERENCES site_nodes (node_id) ON UPDATE CASCADE ON DELETE CASCADE;
