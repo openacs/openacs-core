2f8a86473be7402f01144826863299dfd35099ca
Don Baccus <dhogaza@pacifier.com>
2010-03-22 01:24:07 +0000

    Same change to pk constraint names as for initial install.

--- packages/acs-lang/sql/oracle/upgrade/upgrade-5.6.0d2-5.6.0d3.sql
+++ packages/acs-lang/sql/oracle/upgrade/upgrade-5.6.0d2-5.6.0d3.sql
@@ -5,7 +5,7 @@
 
 
 create table language_codes (
-       iso_639_2            char(3) constraint language_codes_pk primary key,
+       iso_639_2            char(3) constraint language_codes_iso_639_2_pk primary key,
        iso_639_1            char(2),
        label                varchar(200)
 );
@@ -14,7 +14,7 @@ comment on table language_codes is 'Contains ISO-639-2 language codes and their 
 
 create table country_codes (
        label               varchar(200),
-       country             char(2) constraint country_codes_pk primary key
+       country             char(2) constraint country_codes_country_pk primary key
 );
 
 comment on table country_codes is 'Contains ISO-3166 country codes';
