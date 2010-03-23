85f390617c72c0b3e6ccb986da1627f35e75b012
Don Baccus <dhogaza@pacifier.com>
2010-03-23 19:33:56 +0000

    Added 'embeds' to allowable dependencies.

--- packages/acs-kernel/sql/postgresql/upgrade/upgrade-5.6.0d3-5.6.0d4.sql
+++ packages/acs-kernel/sql/postgresql/upgrade/upgrade-5.6.0d3-5.6.0d4.sql
@@ -6,3 +6,8 @@ alter table acs_data_links drop constraint acs_data_links_un;
 
 alter table acs_data_links add constraint acs_data_links_un unique 
 (object_id_one, object_id_two, relation_tag);
+
+alter table apm_package_dependencies drop constraint apm_package_deps_type_ck;
+alter table apm_package_dependencies add
+  constraint apm_package_deps_type_ck
+  check (dependency_type in ('provides', 'requires', 'extends', 'embeds'));
