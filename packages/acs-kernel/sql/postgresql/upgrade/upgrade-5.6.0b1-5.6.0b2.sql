698afce6f2bf3c6ee5f0a4f923b4d16396a2f836
Don Baccus <dhogaza@pacifier.com>
2010-05-18 18:04:50 +0000

    Had to remove the drop/create of cc_users because some obscure views make use of it.  No existing code expects cc_users to contain "bio" anyway, so recreating the view was a matter of tidiness, not need.  Drop ... cascade would work but would break ecommerce and dotlrn-ecommerce, and likely some other packages, so this seems the lesser of two evils.

--- packages/acs-kernel/sql/postgresql/upgrade/upgrade-5.6.0b1-5.6.0b2.sql
+++ packages/acs-kernel/sql/postgresql/upgrade/upgrade-5.6.0b1-5.6.0b2.sql
@@ -2,8 +2,12 @@ create index acs_objects_package_idx on acs_objects (package_id);
 drop index acs_objects_package_object_idx;
 
 -- This is necessary because a previous upgrade script didn't recreate these views.
--- The bio code didn't use these views, but they should be created for future use
+-- The bio code didn't use this views, but they should be created for future use
 -- and consistency.
+
+-- Unfortunately, there are some obscure views build on cc_users, so it can not
+-- be dropped.  PG replace view does not allow for changing the view columns so
+-- we can not replace it, either.
 
 drop view acs_users_all;
 create view acs_users_all
@@ -13,16 +17,3 @@ from  parties pa, persons pe, users u
 where  pa.party_id = pe.person_id
 and pe.person_id = u.user_id;
 
-drop view cc_users;
-create view cc_users
-as
-select o.*, pa.*, pe.*, u.*, mr.member_state, mr.rel_id
-from acs_objects o, parties pa, persons pe, users u, group_member_map m, membership_rels mr
-where o.object_id = pa.party_id
-  and pa.party_id = pe.person_id
-  and pe.person_id = u.user_id
-  and u.user_id = m.member_id
-  and m.group_id = acs__magic_object_id('registered_users')
-  and m.rel_id = mr.rel_id
-  and m.container_id = m.group_id
-  and m.rel_type = 'membership_rel';
--- packages/acs-kernel/sql/postgresql/upgrade/upgrade-5.6.0d1-5.6.0d2.sql
+++ packages/acs-kernel/sql/postgresql/upgrade/upgrade-5.6.0d1-5.6.0d2.sql
@@ -26,20 +26,6 @@ begin
   where  pa.party_id = pe.person_id
   and pe.person_id = u.user_id;
 
-  drop view cc_users;
-  create view cc_users
-  as
-  select o.*, pa.*, pe.*, u.*, mr.member_state, mr.rel_id
-  from acs_objects o, parties pa, persons pe, users u, group_member_map m, membership_rels mr
-  where o.object_id = pa.party_id
-    and pa.party_id = pe.person_id
-    and pe.person_id = u.user_id
-    and u.user_id = m.member_id
-    and m.group_id = acs__magic_object_id(''registered_users'')
-    and m.rel_id = mr.rel_id
-    and m.container_id = m.group_id
-    and m.rel_type = ''membership_rel'';
-
   bio_id := attribute_id
             from acs_attributes
             where object_type = ''person''
