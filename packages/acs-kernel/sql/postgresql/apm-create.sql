666eb3b317c8234d4764bbdc1b1dcce5fad2abf1
Don Baccus <dhogaza@pacifier.com>
2010-10-31 00:15:01 +0000

    apm_parameter__get_value bug missed by merge apparently.

--- packages/acs-kernel/sql/postgresql/apm-create.sql
+++ packages/acs-kernel/sql/postgresql/apm-create.sql
@@ -1546,7 +1546,7 @@ begin
 
     select attr_value into value from apm_parameter_values v
     where v.package_id = get_value__package_id
-    and parameter_id = get_value__parameter_id;
+    and parameter_id = v_parameter_id;
 
     return value;
    
@@ -1564,7 +1564,7 @@ begin
 
     select attr_value into value from apm_parameter_values v
     where v.package_id is null
-    and parameter_id = get_value__parameter_id;
+    and parameter_id = v_parameter_id;
 
     return value;
    
