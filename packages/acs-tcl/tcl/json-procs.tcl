2bcc9db7a6fef860902a799a65d4375ea97b78e4
Don Baccus <dhogaza@pacifier.com>
2011-03-19 17:24:24 +0000

    Applied patch from malte which regularizes and escapes newlines in generated JSON strings.

--- packages/acs-tcl/tcl/json-procs.tcl
+++ packages/acs-tcl/tcl/json-procs.tcl
@@ -370,6 +370,11 @@ ad_proc -private util::json::gen_inner {value} {
                     && ![regexp {^(?:true|false|null)$} $value]} {
                     set value "\"$value\""
                 }
+                # Cleanup linebreaks
+                regsub -all {\r\n} $value "\n" value
+                regsub -all {\r} $value "\n" value
+                # JSON requires new line characters be escaped
+                regsub -all {\n} $value "\\n" value
                 return $value
             }
          }
