53def9005f77df739ca2a969c8b17e529639d502
Emmanuelle Raffenne <eraffenne@gmail.com>
2011-04-08 11:57:15 +0000

    i18n of the user interface

--- packages/acs-lang/www/admin/lookup.adp
+++ packages/acs-lang/www/admin/lookup.adp
@@ -3,17 +3,18 @@
   <property name="context">@context;noquote@</property>
   <property name="focus">lookup.key</property>
 
+<h1>#acs-lang.Look_up_message#</h1>
+
 <formtemplate id="lookup"></formtemplate>
 
 <if @message_p@ true>
-  <h2>Translated Message</h2>
-  <if @message@ nil><p><i>blank</i></if>
-  <else><p>@message@</p></else>
+  <h2>#acs-lang.Translated_Message#</h2>
+  <p>@message@</p>
 </if>
 
 <if @edit_url@ not nil>
   <ul class="action-links">
-    <li><a href="@edit_url@">Edit this message</a></li>
+    <li><a href="@edit_url@">#acs-lang.Edit_this_message#</a></li>
   </ul>
 </if>
 
--- packages/acs-lang/www/admin/lookup.tcl
+++ packages/acs-lang/www/admin/lookup.tcl
@@ -9,17 +9,17 @@ set message_p 0
 
 ad_form -name lookup -form {
     {key:text
-        {label "Message key"}
-        {help_text "Include package key, as in package-key.message-key"}
+        {label "[_ acs-lang.Message_key]"}
+        {help_text "[_ acs-lang.Include_package_key_as]"}
         {html {size 50}}
     }
     {locale:text 
-        {label "Locale"}
-        {help_text "Can be two-character for language only or five-character full locale"}
+        {label "[_ acs-lang.Locale]"}
+        {help_text "[_ acs-lang.Can_be_two_character]"}
     }
 } -on_submit {
     # No substitution
-    set message [lang::message::lookup $locale $key {} {} 0]
+    set message [lang::message::lookup $locale $key]
     
     set keyv [split $key "."]
     set package_key [lindex $keyv 0]
