26eee2f95514e7b1335377e8135a9063bf95917a
Don Baccus <dhogaza@pacifier.com>
2011-04-11 01:08:27 +0000

    Added a couple of CR revision attributes - the oracle equivalent of the previous PG commit.

--- packages/acs-content-repository/sql/oracle/types-create.sql
+++ packages/acs-content-repository/sql/oracle/types-create.sql
@@ -283,6 +283,22 @@ begin
    pretty_plural => 'Language'
  );
 
+ attr_id := content_type.create_attribute (
+   content_type   => 'content_revision',
+   attribute_name => 'item_id',
+   datatype => 'integer',
+   pretty_name => 'Item id',
+   pretty_plural => 'Item ids'
+ );
+
+ attr_id := content_type.create_attribute (
+   content_type   => 'content_revision',
+   attribute_name => 'content',
+   datatype => 'text',
+   pretty_name => 'content',
+   pretty_plural => 'content'
+ );
+
 end;
 /
 show errors
