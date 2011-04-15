2b349cc9fe9986ba2591facefe7149b1655f21e5
Emmanuelle Raffenne <eraffenne@gmail.com>
2011-04-15 10:29:16 +0000

    Localization and translation into spanish

--- packages/acs-subsite/www/admin/applications/index.tcl
+++ packages/acs-subsite/www/admin/applications/index.tcl
@@ -6,7 +6,7 @@ ad_page_contract {
     @cvs-id $Id$
 }
 
-set page_title "Applications"
+set page_title "[_ acs-subsite.Applications]"
 set context [list $page_title]
 
 # Get the subsite node ID
@@ -26,49 +26,49 @@ list::create \
     -multirow applications \
     -key node_id \
     -actions { 
-        "Add application" application-add "Add new application"
+        "#acs-subsite.Add_application#" application-add "#acs-subsite.Add_new_app#"
     } \
     -bulk_actions {
-        "Delete" application-delete "Delete selected applications"
+        "#acs-subsite.Delete#" application-delete "#acs-subsite.Delete_selected_app#"
     } \
     -elements {
         edit {
             sub_class narrow
             display_template {
-                <img src="/shared/images/Edit16.gif" height="16" width="16" alt="Edit application name and path" style="border:0">
+                <img src="/shared/images/Edit16.gif" height="16" width="16" alt="#acs-subsite.Edit_application_name_and_path#" style="border:0">
             }
             link_url_eval {[export_vars -base application-add { node_id }]}
-            link_html { title "Edit application name and path" }
+            link_html { title "#acs-subsite.Edit_application_name_and_path#" }
         }
         instance_name {
-            label "Name"
+            label "[_ acs-subsite.Name]"
             link_url_eval {../../$name/}
         }
         name {
-            label "URL"
+            label "[_ acs-subsite.URL]"
         }
         package_pretty_name {
-            label "Application"
+            label "[_ acs-subsite.Application]"
         }
         permissions {
-            label "Permissions"
+            label "[_ acs-subsite.Permissions]"
             link_url_eval {[export_vars -base permissions { package_id }]}
-            display_template {Permissions}
+            display_template { #acs-subsite.Permissions# }
             sub_class narrow
         }
         parameters {
-            label "Parameters"
+            label "[_ acs-subsite.Parameters]"
             link_url_col parameter_url
-            display_template {<if @applications.parameter_url@ not nil>Parameters</if>}
+            display_template {<if @applications.parameter_url@ not nil>[_ acs-subsite.Parameters]</if>}
             sub_class narrow
         }
         delete {
             sub_class narrow
             display_template {
-                <img src="/shared/images/Delete16.gif" height="16" width="16" alt="Delete this application" style="border:0">
+                <img src="/shared/images/Delete16.gif" height="16" width="16" alt="#acs-subsite.Delete_this_application#" style="border:0">
             }
             link_url_eval {[export_vars -base application-delete { node_id }]}
-            link_html { title "Delete this application" }
+            link_html { title "#acs-subsite.Delete_this_application#" }
         }
     }
 
