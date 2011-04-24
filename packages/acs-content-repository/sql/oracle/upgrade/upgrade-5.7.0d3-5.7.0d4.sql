9546db45185ec1cf640f537670d03619272d4d06
Don Baccus <dhogaza@pacifier.com>
2011-04-24 21:41:28 +0000

    Kludge around xotcl-core for oracle, as was done earlier for PG.

--- packages/acs-content-repository/sql/oracle/upgrade/upgrade-5.7.0d3-5.7.0d4.sql
+++ packages/acs-content-repository/sql/oracle/upgrade/upgrade-5.7.0d3-5.7.0d4.sql
@@ -1,22 +1,37 @@
 declare
  attr_id	acs_attributes.attribute_id%TYPE;
+ exists_p integer;
 begin
 
- attr_id := content_type.create_attribute (
-   content_type   => 'content_revision',
-   attribute_name => 'item_id',
-   datatype => 'integer',
-   pretty_name => 'Item id',
-   pretty_plural => 'Item ids'
- );
+  select count(*) into exists_p
+  from acs_attributes
+  where object_type = 'content_revision'
+    and attribute_name = 'item_id';
 
- attr_id := content_type.create_attribute (
-   content_type   => 'content_revision',
-   attribute_name => 'content',
-   datatype => 'text',
-   pretty_name => 'content',
-   pretty_plural => 'content'
- );
+  if exists_p = 0 then
+    attr_id := content_type.create_attribute (
+      content_type   => 'content_revision',
+      attribute_name => 'item_id',
+      datatype => 'integer',
+      pretty_name => 'Item id',
+      pretty_plural => 'Item ids'
+    );
+  end if;
+
+  select count(*) into exists_p
+  from acs_attributes
+  where object_type = 'content_revision'
+    and attribute_name = 'content';
+
+  if exists_p = 0 then
+    attr_id := content_type.create_attribute (
+      content_type   => 'content_revision',
+      attribute_name => 'content',
+      datatype => 'text',
+      pretty_name => 'content',
+      pretty_plural => 'content'
+    );
+  end if;
 
 end;
 /
