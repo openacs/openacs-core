4906fcb16fce72a1905f99c0dddeaa21538f3cd5
Ryan Gallimore <ryan.gallimore@quest.ie>
2011-05-15 16:43:24 +0000

    Fix RestrictErrorsToAdminsP parameter behaviour, which previously did nothing.

--- packages/acs-tcl/tcl/request-processor-procs.tcl
+++ packages/acs-tcl/tcl/request-processor-procs.tcl
@@ -778,8 +778,12 @@ ad_proc rp_report_error {
     #Serve the stacktrace
     set params [list [list stacktrace $message] [list user_id $user_id] [list error_file $error_file] [list prev_url $prev_url] [list feedback_id $feedback_id] [list error_url $error_url] [list bug_package_id $bug_package_id] [list vars_to_export $vars_to_export]]
     
-    if {![parameter::get -package_id [ad_acs_kernel_id] -parameter RestrictErrorsToAdminsP -default 0] || \
-            [permission::permission_p -object_id [ad_conn package_id] -privilege admin] } {
+    set error_message $message
+
+    if {[parameter::get -package_id [ad_acs_kernel_id] -parameter RestrictErrorsToAdminsP -default 0] && \
+            ![permission::permission_p -object_id [ad_conn package_id] -privilege admin] } {
+        set message {}
+        set params [lreplace $params 0 0 [list stacktrace $message]]    
     }
     
     with_catch errmsg {
@@ -788,7 +792,7 @@ ad_proc rp_report_error {
         # An error occurred during rendering of the error page
         global errorInfo
         ns_log Error "rp_report_error: Error rendering error page (!)\n$errorInfo"
-        set rendered_page "</table></table></table></h1></b></i><blockquote><pre>[ns_quotehtml $message]</pre></blockquote>[ad_footer]"
+        set rendered_page "</table></table></table></h1></b></i><blockquote><pre>[ns_quotehtml $error_message]</pre></blockquote>[ad_footer]"
     }
 
     ns_return 500 text/html $rendered_page
@@ -796,7 +800,7 @@ ad_proc rp_report_error {
     set headers [ns_conn headers]
     ns_log Error "[ns_conn method] http://[ns_set iget $headers host][ns_conn url]?[ns_conn query]
 referred by \"[ns_set iget $headers referer]\"
-$message"
+$error_message"
 
 }
 
