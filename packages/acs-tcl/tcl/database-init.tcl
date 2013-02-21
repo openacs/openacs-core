d677f6cc42278cfa1f1d108c592d27a32e5c9f3c
Victor Guerra <vguerra@gmail.com>
2013-02-21 14:17:52 +0000

    During installation process this script is loaded before installing the kernel, therefore one has to avoid using parameter::get_from_package_key which would cause to cache a acs_kernel package_id of 0, which has negative impacts when trying to install .lrn for example ( when executing install.xml statements that change kernel package's parameter values ).

--- packages/acs-tcl/tcl/database-init.tcl
+++ packages/acs-tcl/tcl/database-init.tcl
@@ -10,6 +10,4 @@ ad_library {
 
 #DRB: the default value is needed during the initial install of OpenACS
 ns_cache create db_cache_pool -size \
-    [parameter::get_from_package_key  \
-        -package_key acs-kernel \
-        -parameter DBCacheSize -default 50000]
+    [parameter::get -package_id [ad_acs_kernel_id] -parameter DBCacheSize -default 50000]
