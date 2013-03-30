5fd3d1ba7b18d1a7a5bc417ecdfd3e40f83c0a4d
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2013-03-30 13:22:22 +0000

    - update postgres function to modern style plpgsql definitions, avoid backslash escaping - add missing function_args

--- packages/acs-messaging/sql/postgresql/upgrade/upgrade-5.8.0d1-5.8.0d2.sql
+++ packages/acs-messaging/sql/postgresql/upgrade/upgrade-5.8.0d1-5.8.0d2.sql
@@ -1,55 +1,62 @@
 -- providing upgrade script for function 
 
-create or replace function acs_message__new (integer,integer,timestamptz,integer,
-varchar,varchar,varchar,varchar,text,integer,integer,integer,integer,
-varchar,varchar,boolean,integer)
-returns integer as '
-declare
-        p_message_id    alias for $1;  --default null,
-        p_reply_to      alias for $2;  --default null,
-        p_sent_date     alias for $3;  --default sysdate,
-        p_sender        alias for $4;  --default null,
-        p_rfc822_id     alias for $5;  --default null,
-        p_title         alias for $6;  --default null,
-        p_description   alias for $7;  --default null,
-        p_mime_type     alias for $8;  --default ''text/plain'',
-        p_text          alias for $9;  --default null,
-        p_data          alias for $10; --default null,
-        p_parent_id     alias for $11; --default 0,
-        p_context_id    alias for $12;
+
+
+-- added
+select define_function_args('acs_message__new','message_id,reply_to,sent_date,sender,rfc822_id,title,description,mime_type,text,data,parent_id,context_id,creation_user,creation_ip,object_type,is_live,package_id');
+
+--
+-- procedure acs_message__new/17
+--
+CREATE OR REPLACE FUNCTION acs_message__new(
+   p_message_id integer,    --default null,
+   p_reply_to integer,      --default null,
+   p_sent_date timestamptz, --default sysdate,
+   p_sender integer,        --default null,
+   p_rfc822_id varchar,     --default null,
+   p_title varchar,         --default null,
+   p_description varchar,   --default null,
+   p_mime_type varchar,     --default 'text/plain',
+   p_text text,             --default null,
+   p_data integer,          --default null,
+   p_parent_id integer,     --default 0,
+   p_context_id integer,
+   p_creation_user integer, --default null,
+   p_creation_ip varchar,   --default null,
+   p_object_type varchar,   --default 'acs_message',
+   p_is_live boolean,       --default 't'
+   p_package_id integer
+
+) RETURNS integer AS $$
+DECLARE
         p_creation_date timestamptz := current_timestamp;  -- alias for $13 --default sysdate,
-        p_creation_user alias for $13; --default null,
-        p_creation_ip   alias for $14; --default null,
-        p_object_type   alias for $15; --default ''acs_message'',
-        p_is_live       alias for $16; --default ''t''
-        p_package_id    alias for $17;
         v_message_id   acs_messages.message_id%TYPE;
         v_rfc822_id    acs_messages.rfc822_id%TYPE;
         v_revision_id  cr_revisions.revision_id%TYPE;
 		v_system_url   varchar;
 		v_domain_name  varchar;
 		v_idx		   integer;
-    begin
+    BEGIN
         -- generate a message id now so we can get an rfc822 message-id
         if p_message_id is null then
-            select nextval(''t_acs_object_id_seq'') into v_message_id;
+            select nextval('t_acs_object_id_seq') into v_message_id;
         else
             v_message_id := p_message_id;
         end if;
 
         -- need to make this mandatory also - jg
-        -- this needs to be fixed up, but Oracle doesn''t give us a way
+        -- this needs to be fixed up, but Oracle doesn't give us a way
         -- to get the FQDN
 
 	-- vk: get SystemURL parameter and use it to extract domain name
-         select apm__get_value(package_id, ''SystemURL'') into v_system_url
-          from apm_packages where package_key=''acs-kernel'';
-		v_idx := position(''http://'' in v_system_url);
+         select apm__get_value(package_id, 'SystemURL') into v_system_url
+          from apm_packages where package_key='acs-kernel';
+		v_idx := position('http://' in v_system_url);
 		v_domain_name := trim (substr(v_system_url, v_idx + 7));
 
         if p_rfc822_id is null then
-           v_rfc822_id := current_date || ''.'' || v_message_id || ''@'' ||
-               v_domain_name || ''.hate'';
+           v_rfc822_id := current_date || '.' || v_message_id || '@' ||
+               v_domain_name || '.hate';
         else
             v_rfc822_id := p_rfc822_id;
         end if;
@@ -64,13 +71,13 @@ declare
             p_context_id,			  -- context_id     
             p_creation_ip,			  -- creation_ip    
             p_object_type,			  -- item_subtype   
-            ''acs_message_revision'',		  -- content_type   
+            'acs_message_revision',		  -- content_type   
             null,				  -- title
             null,				  -- description
-            ''text/plain'',			  -- mime_type
+            'text/plain',			  -- mime_type
             null,				  -- nls_language
             null,				  -- text
-            ''text'',				  -- storage_type
+            'text',				  -- storage_type
             p_package_id
         );
 
@@ -94,5 +101,6 @@ declare
         );
 
         return v_message_id;
-end;' language 'plpgsql';
+END;
+$$ LANGUAGE plpgsql;
 
