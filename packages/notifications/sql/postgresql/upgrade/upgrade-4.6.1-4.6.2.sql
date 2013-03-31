f17f9fc5582ba556a22532780731ff275af6ee80
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2013-03-31 16:31:28 +0000

    - fix $$ quoting in upgrade scripts

--- packages/notifications/sql/postgresql/upgrade/upgrade-4.6.1-4.6.2.sql
+++ packages/notifications/sql/postgresql/upgrade/upgrade-4.6.1-4.6.2.sql
@@ -38,35 +38,22 @@ drop function notification_request__new (
 -- Then define the new one
 select define_function_args ('notification_request__new','request_id,object_type;notification_request,type_id,user_id,object_id,interval_id,delivery_method_id,format,dynamic_p;f,creation_date,creation_user,creation_ip,context_id');
 
-create function notification_request__new (
-       integer,                       -- request_id
-       varchar,                       -- object_type
-       integer,                       -- type_id
-       integer,                       -- user_id
-       integer,                       -- object_id
-       integer,                       -- interval_id
-       integer,                       -- delivery_method_id
-       varchar,                       -- format
-       bool,                          -- dynamic_p
-       timestamp with time zone,      -- creation_date
-       integer,                       -- creation_user
-       varchar,                       -- creation_ip
-       integer                        -- context_id
-) returns integer as '
+create or replace function notification_request__new (
+       p_request_id integer,
+       p_object_type varchar,
+       p_type_id integer,
+       p_user_id integer,
+       p_object_id integer,
+       p_interval_id integer,
+       p_delivery_method_id integer,
+       p_format varchar,
+       p_dynamic_p bool,
+       p_creation_date timestamp with time zone,
+       p_creation_user integer,
+       p_creation_ip varchar,
+       p_context_id integer
+) returns integer as $$
 DECLARE
-        p_request_id                            alias for $1;
-        p_object_type                           alias for $2;
-        p_type_id                               alias for $3;
-        p_user_id                               alias for $4;
-        p_object_id                             alias for $5;
-        p_interval_id                           alias for $6;
-        p_delivery_method_id                    alias for $7;
-        p_format                                alias for $8;
-        p_dynamic_p                             alias for $9;
-        p_creation_date                         alias for $10;
-        p_creation_user                         alias for $11;
-        p_creation_ip                           alias for $12;
-        p_context_id                            alias for $13;
         v_request_id                            integer;
 BEGIN
         v_request_id:= acs_object__new (
@@ -82,9 +69,7 @@ BEGIN
       (v_request_id, p_type_id, p_user_id, p_object_id, p_interval_id, p_delivery_method_id, p_format, p_dynamic_p);
 
       return v_request_id;                          
-
 END;
-
 $$ LANGUAGE plpgsql;
 
 
