d91b83129a825e0cef8405820f7942d435ac1cc7
Michael Steigman <michael@steigman.net>
2013-08-29 22:21:34 +0000

    update acs-admin cache page to work with both naviserver and aolserver

--- packages/acs-admin/www/cache/index.tcl
+++ packages/acs-admin/www/cache/index.tcl
@@ -8,25 +8,18 @@ set context [list [list "../developer" "Developer's Administration"] $page_title
 template::multirow create caches name entries size max flushed hit_rate
 
 foreach cache [lsort -dictionary [ns_cache_names]] {
-    if {[regexp {util_memoize_(.*)} $cache match suffix] \
-	    || $cache eq "util_memoize"} {
-	if {![info exists suffix] || "" eq $suffix} {
-	    set name "util_memoize"
-	    set match "util_memoize"
-	} else {
-	    set name $suffix
-	}
-	set pair [ns_cache_size $match]
-	set size [format "%.2f MB" [expr {[lindex $pair 1] / 1048576.0}]]
-	set max [format "%.2f MB" [expr {[lindex $pair 0] / 1048576.0}]]
-	ns_cache_stats $match stats_array
-	set entries $stats_array(entries)
-	set flushed $stats_array(flushed)
-	set hit_rate $stats_array(hitrate)
-	template::multirow append caches $name $entries $size $max \
-		$flushed $hit_rate
+    if { [ns_info name] eq "AOLserver" } {
+        ns_cache_stats $cache stats_array    
+    } else {
+        array set stats_array [ns_cache_stats $cache]
     }
-    set match ""
-    set suffix ""
-}
 
+    set pair [ns_cache_size $cache]
+    set size [format "%.2f MB" [expr {[lindex $pair 1] / 1048576.0}]]
+    set max [format "%.2f MB" [expr {[lindex $pair 0] / 1048576.0}]]
+    set entries $stats_array(entries)
+    set flushed $stats_array(flushed)
+    set hit_rate $stats_array(hitrate)
+    template::multirow append caches $cache $entries $size $max \
+        $flushed $hit_rate
+}
