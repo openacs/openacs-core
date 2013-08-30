7945e66a2719d63cb906e7d1908baf044b1b64c9
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2013-08-30 07:49:24 +0000

    - no need for separate code for AOLServer/NaviServer

--- packages/acs-admin/www/cache/index.tcl
+++ packages/acs-admin/www/cache/index.tcl
@@ -8,12 +8,7 @@ set context [list [list "../developer" "Developer's Administration"] $page_title
 template::multirow create caches name entries size max flushed hit_rate
 
 foreach cache [lsort -dictionary [ns_cache_names]] {
-    if { [ns_info name] eq "AOLserver" } {
-        ns_cache_stats $cache stats_array    
-    } else {
-        array set stats_array [ns_cache_stats $cache]
-    }
-
+    array set stats_array [ns_cache_stats $cache]
     set pair [ns_cache_size $cache]
     set size [format "%.2f MB" [expr {[lindex $pair 1] / 1048576.0}]]
     set max [format "%.2f MB" [expr {[lindex $pair 0] / 1048576.0}]]
