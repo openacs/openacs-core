b48d8734171109f3c3f3f2558109860bcdf37db4
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2013-09-24 19:39:53 +0000

    - remove currency_widget, which is inherently broken, since it depends on a non-existent table (not consistent with ref-currency)

--- packages/acs-tcl/tcl/widgets-procs.tcl
+++ packages/acs-tcl/tcl/widgets-procs.tcl
@@ -264,34 +264,6 @@ ad_proc ad_db_select_widget {
     }
 }
 
-ad_proc currency_widget {{default ""} {select_name "currency_code"} {size_subtag "size=\"4\""}} "Returns a currency selection box" {
-
-    set widget_value "<select name=\"$select_name\" $size_subtag>\n"
-    if { $default eq "" } {
-        if { [parameter::get -parameter SomeAmericanReadersP] } {
-	    append widget_value "<option value=\"\">Currency</option>
-<option value=\"USD\" selected=\"selected\">United States Dollar</option>\n"
-	} else {
-	    append widget_value "<option value=\"\" selected=\"selected\">Currency</option>\n"
-	}
-    }
-    db_foreach currency_info {
-	select currency_name, iso 
-	from currency_codes 
-	where supported_p='t'
-	order by currency_name 
-    } {
-	if { $default == $iso } {
-	    append widget_value "<option value=\"$iso\" selected=\"selected\">$currency_name</option>\n" 
-        } else {            
-            append widget_value "<option value=\"$iso\">$currency_name</option>\n"
-        }
-    }
-    
-    append widget_value "</select>\n"
-    return $widget_value
-}
-
 ad_proc ad_html_colors {} "Returns an array of HTML colors and names." {
     return {
 	{ Black 0 0 0 }
