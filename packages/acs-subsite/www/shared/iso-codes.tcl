62ae6b1d9a14923a610194515e89e7d0a8363899
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2013-09-28 15:27:59 +0000

    - remove deprecated call to ad_header

--- packages/acs-subsite/www/shared/iso-codes.tcl
+++ packages/acs-subsite/www/shared/iso-codes.tcl
@@ -8,14 +8,11 @@ ad_page_contract {
 
 if {![db_table_exists countries] } {
     # acs-reference countries not loaded
-    set header [ad_header "ISO Codes"]
 
     ad_return_template iso-codes-no-exist
-
     return
 }
 
-db_multirow ccodes country_codes "select iso, default_name from countries
-order by default_name" 
+db_multirow ccodes country_codes "select iso, default_name from countries order by default_name" 
 
 ad_return_template
