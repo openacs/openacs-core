7606649c865c3226aee061f92297b09d3447c957
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2013-09-28 15:14:52 +0000

    - use standard templating - fix HTML - remove call to deprecated ad_header

--- packages/acs-subsite/www/permissions/revoke.tcl
+++ packages/acs-subsite/www/permissions/revoke.tcl
@@ -18,40 +18,40 @@ if {[llength $revoke_list] == 0} {
   ad_script_abort
 }
 
-doc_body_append "[ad_header "Revoke Confirm"]
+set title "Revoke Confirm"
+set context [list $title]
 
-<h2>Revoke Confirm</h2>
+set body [subst {
+    <h2>Revoke Confirm</h2>
 
-<hr>
-
-Are you sure you want to remove the following entries from the access
-control list of [db_string name {select acs_object.name(:object_id)
-from dual}]?
-
-<ul>
-"
-foreach item $revoke_list {
-  set party_id [lindex $item 0]
-  set privilege [lindex $item 1]
-  doc_body_append "  <li>[db_string party_name {select acs_object.name(:party_id) from dual}]</li>\n"
-}
-
-doc_body_append "</ul>
-
-<form method=get action=revoke-2>
-[export_vars -form {object_id application_url}]
-
-"
+    <hr>
+    Are you sure you want to remove the following entries from the access
+    control list of [db_string name {select acs_object.name(:object_id) from dual}]?
+    <ul>
+}]
 
 foreach item $revoke_list {
-  doc_body_append "<input type=hidden name=revoke_list value=\"$item\">\n"
+    set party_id [lindex $item 0]
+    set privilege [lindex $item 1]
+    append body [subst {
+	<li>[db_string party_name {select acs_object.name(:party_id) from dual}]</li>
+    }]
 }
 
-doc_body_append "
+append body [subst {
+    </ul>
+    <form method="get" action="revoke-2">
+    [export_vars -form {object_id application_url}]
+}]
 
-<input name=operation type=submit value=Yes> <input name=operation type=submit value=No>
+foreach item $revoke_list {
+    append body [subst {
+	<input type="hidden" name="revoke_list" value="$item">
+    }]
+}
 
-</form>
+append body {
+    <input name="operation" type="submit" value="Yes"> <input name="operation" type="submit" value="No">
+    </form>
+}
 
-[ad_footer]
-"
