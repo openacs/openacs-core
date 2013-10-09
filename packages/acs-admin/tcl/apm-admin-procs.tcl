adcf026103bc66ca1e8741db9ed996759b1ccf08
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2013-10-09 06:47:30 +0000

    - no need to write ad_quoted to error.log

--- packages/acs-admin/tcl/apm-admin-procs.tcl
+++ packages/acs-admin/tcl/apm-admin-procs.tcl
@@ -502,7 +502,7 @@ ad_proc -private apm_build_repository {
 			    close $fp
 			    
 			    lappend cmd "|" [apm_gzip_cmd] -c ">" $apm_file
-			    ns_log Notice "Executing: [ad_quotehtml $cmd]"
+			    ns_log Notice "Executing: $cmd"
 			    if {[catch "exec $cd_helper $packages_root_path $cmd" errmsg]} {
 				ns_log notice "Error during tar in repository creation for\
 					file ${channel_dir}$pkg_info(package.key)-$pkg_info(name).apm:\
