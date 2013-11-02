4aca0de6b300dd3a5aec650c3e8bd807c2ff0d88
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2013-11-02 18:26:13 +0000

    - move initialization of nsv_arrays for apm    from -proc file of bootstapper to -init file in acs-tcl    (as suggested by Peter Marklund, but never implemented) - protect rename of "exec" against multiple renamings - bump version number to 5.8.1d3

---
+++ packages/acs-tcl/tcl/apm-init.tcl
@@ -0,0 +1,13 @@
+
+# FIXME: Peter M - This file cannot be watched with the APM as it
+# re-initializes the reload level to 0 everytime it is sourced. Could
+# we move these initialization to an -init.tcl file instead?
+
+# Initialize loader NSV arrays. See apm-procs.tcl for a description of
+# these arrays.
+
+nsv_array set apm_library_mtime [list]
+nsv_array set apm_version_procs_loaded_p [list]
+nsv_array set apm_reload_watch [list]
+nsv_array set apm_package_info [list]
+nsv_set apm_properties reload_level 0
