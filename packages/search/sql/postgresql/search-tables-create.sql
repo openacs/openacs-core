56bb18814ebc5eaff39f60a852aee7f7b3c8f8d5
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2015-02-24 08:12:40 +0000

    - fix for bug #3238

--- packages/search/sql/postgresql/search-tables-create.sql
+++ packages/search/sql/postgresql/search-tables-create.sql
@@ -1,7 +1,10 @@
 create table search_observer_queue (
-    object_id		   integer,
+    object_id		   integer not null
+                           references acs_objects(object_id) on delete cascade,
     event_date		   timestamptz default current_timestamp,
     event		   varchar(6)
 			   constraint search_observer_queue_event_ck
 			   check (event in ('INSERT','DELETE','UPDATE'))  
 );
+
+
