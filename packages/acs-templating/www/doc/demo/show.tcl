b856db02038d12fe6d285be2c02c2117410f2688
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2015-08-09 09:08:12 +0000

    - address issue in #3284

--- packages/acs-templating/www/doc/demo/show.tcl
+++ packages/acs-templating/www/doc/demo/show.tcl
@@ -10,19 +10,31 @@ ad_page_contract {
 
 if { $file eq "" } {
 
-  set output "no file specified"
+    set output "no file specified"
 
 } elseif { [regexp {\.\.|^/} $file] } {
 
-  set output "Only files within this directory may be shown."
+    set output "Only files within this directory may be shown."
 
 } else {
-
-  # [ns_url2file [ns_conn url]]  fails under request processor !
-  # the file for URL pkg/page may be in packages/pkg/www/page, not www/pkg/page
-  set dir [file dirname [ad_conn file]]
-  set text [ns_quotehtml [template::util::read_file $dir/$file]]
-  set output "<pre>$text</pre>"
+    #
+    # [ns_url2file [ns_conn url]] fails under request processor, since
+    # the request processor manges the provided url path.
+    #
+    set dir [file dirname [ad_conn file]]
+    if {[file readable $dir/$file]} {
+	#
+	# Probably, one should in real life the file with
+	# ns_returnfile, since the file might contain binary
+	# characters... but for this sample script, we return
+	# everything as html.
+	#
+	set text [ns_quotehtml [template::util::read_file $dir/$file]]
+	set output "<pre>$text</pre>"
+    } else {
+	set output "The specified file not readable"
+	# probably, we should return a 404 error, not a success status code
+    }
 }
 
 ns_return 200 text/html $output
