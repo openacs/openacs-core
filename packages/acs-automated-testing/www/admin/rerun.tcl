181262928eb7f0481863342b46741a890e83ef67
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2015-08-17 15:29:29 +0000

    - fix page contracts, improve saftey of argument passing via URLs

--- packages/acs-automated-testing/www/admin/rerun.tcl
+++ packages/acs-automated-testing/www/admin/rerun.tcl
@@ -1,10 +1,10 @@
 ad_page_contract {
     @cvs-id $Id$
 } {
-    {package_key ""}
+    {package_key:token ""}
     {category:aa_test_category ""}
     {view_by:aa_test_view_by "package"}
-    {testcase_id:naturalnum,notnull ""}
+    {testcase_id:word,notnull ""}
     {quiet:boolean "0"}
     {stress:boolean "0"}
     {security_risk:boolean "0"}
@@ -12,18 +12,22 @@ ad_page_contract {
 }
 
 if {$testcase_id eq ""} {
-  if {$quiet} {
-    aa_runseries -stress $stress -security_risk $security_risk -quiet $package_key $category
-  } else {
-    aa_runseries -stress $stress -security_risk $security_risk $package_key $category
-  }
-  ad_returnredirect "index?by_package_key=$package_key&by_category=$category&view_by=$view_by&quiet=$quiet&stress=$stress&security_risk=$security_risk"
+    if {$quiet} {
+	aa_runseries -stress $stress -security_risk $security_risk -quiet $package_key $category
+    } else {
+	aa_runseries -stress $stress -security_risk $security_risk $package_key $category
+    }
+    
+    ad_returnredirect [export_vars -base index {
+	{by_package_key $package_key}
+	{by_category $category}
+	view_by quiet stress security_risk}]
 } else {
-  if {$quiet} {
-    aa_runseries -quiet -testcase_id $testcase_id "" ""
-  } else {
-    aa_runseries -testcase_id $testcase_id "" ""
-  }
-  ad_returnredirect "testcase?testcase_id=$testcase_id&package_key=$package_key&quiet=$quiet"
+    if {$quiet} {
+	aa_runseries -quiet -testcase_id $testcase_id "" ""
+    } else {
+	aa_runseries -testcase_id $testcase_id "" ""
+    }
+    ad_returnredirect "testcase?testcase_id=$testcase_id&package_key=$package_key&quiet=$quiet"
 }
 
