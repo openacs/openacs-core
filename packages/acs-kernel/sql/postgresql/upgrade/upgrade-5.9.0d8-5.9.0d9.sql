f0dd305b02d19039d9ddb26cec894195957cc791
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2015-09-10 18:12:33 +0000

    - delete parameters from database, that were deleted from .info files ages ago

---
+++ packages/acs-kernel/sql/postgresql/upgrade/upgrade-5.9.0d8-5.9.0d9.sql
@@ -0,0 +1,9 @@
+-- cleanup of parameter deleted from .info files ages ago
+
+select apm__unregister_parameter(parameter_id) from apm_parameters
+  where package_key = 'acs-subsite'
+  and parameter_name in (
+      'background', 'bgcolor', 'textcolor',
+      'EmailRandomPasswordWhenForgottenP', 'UseCustomQuestionForPasswordReset',
+      'RequireQuestionForPasswordResetP'
+  );
