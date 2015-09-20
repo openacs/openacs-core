f3fd446e57648284c1214de1818dde1fe152574b
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2015-09-20 15:35:39 +0000

    - use include_contract

--- packages/acs-admin/lib/site-wide-services.tcl
+++ packages/acs-admin/lib/site-wide-services.tcl
@@ -1,3 +1,9 @@
+ad_include_contract {
+    Include for listing site wide services
+} {
+    {nr_subsites:integer 0}
+}
+
 set acs_admin_url      [apm_package_url_from_key "acs-admin"]
 set acs_lang_admin_url [apm_package_url_from_key "acs-lang"]admin/
 set acs_core_docs_url  [apm_package_url_from_key "acs-core-docs"]
