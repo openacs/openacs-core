7c6f69453829295e56b3c0b60376a58e0780e6f9
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2015-09-22 12:30:44 +0000

    - whitespace changes

--- packages/acs-templating/www/doc/demo/reference.tcl
+++ packages/acs-templating/www/doc/demo/reference.tcl
@@ -4,14 +4,8 @@ ad_page_contract {
   users:multirow
 }
 
-
-set query "select 
-             first_name, last_name, state
-           from
-             ad_template_sample_users"
-
+set query "select first_name, last_name, state from ad_template_sample_users"
 set e_query "$query where first_name like '%e%'"
-
 
 db_multirow users    users_query $query
 db_multirow e_people e_people_q  $e_query
