f5aa22e73ef5a227f0fb185f9b18c8a1d659b1cf
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2015-10-08 19:28:03 +0000

    - provide more security for granting/revoking swa permissions (use timeouts and configurable secrets)

--- packages/acs-admin/www/users/modify-admin-privileges.tcl
+++ packages/acs-admin/www/users/modify-admin-privileges.tcl
@@ -5,20 +5,16 @@ ad_page_contract {
     @cvs-id $Id$
 
 } {
-    user_id:naturalnum,notnull
-    action:notnull
-    confirmed_p:boolean,optional
+    user_id:naturalnum,notnull,verify
+    action:notnull,verify
+    {confirmed_p:boolean 0}
 }
 
-set confirmed_url "/acs-admin/users/modify-admin-privileges?user_id=$user_id&action=$action&confirmed_p=1"
-
-set return_url "/acs-admin/users/one?user_id=$user_id"
+set confirmed_url [export_vars -base /acs-admin/users/modify-admin-privileges {
+    user_id:sign(max_age=60) action:sign {confirmed_p 1}}]
+set return_url [export_vars -base /acs-admin/users/one {user_id}]
 
 set context [list [list "./" "Users"] "Modify privileges"]
-
-if {![info exists confirmed_p]} {
-    set confirmed_p 0
-}
 
 if {$confirmed_p} {
     if {"grant" eq $action} {
