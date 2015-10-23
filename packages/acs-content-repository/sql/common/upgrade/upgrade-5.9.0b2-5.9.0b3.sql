19f4737c0368f671ce9755b4a3c04653c5d7467f
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2015-10-23 11:44:00 +0000

    - fix incorrect entries in cr_revisions having the inccorect mime_type

--- packages/acs-content-repository/sql/common/upgrade/upgrade-5.9.0b2-5.9.0b3.sql
+++ packages/acs-content-repository/sql/common/upgrade/upgrade-5.9.0b2-5.9.0b3.sql
@@ -1,6 +1,9 @@
-
-delete from cr_mime_types where file_extension = 'svg';
-insert into cr_mime_types (label,mime_type,file_extension) values ('Image SVG', 'image/svg+xml', 'svg'); 
+insert into cr_mime_types (label,mime_type,file_extension)
+       select 'Image SVG', 'image/svg+xml', 'svg' from dual
+       where not exists (select 1 from cr_mime_types where mime_type = 'image/svg+xml');
+       
+update cr_revisions set mime_type = 'image/svg+xml' where mime_type = 'imate/xml+svg';
+delete from cr_mime_types where mime_type = 'image/xml+svg';
 
 delete from cr_extension_mime_type_map where extension = 'svg';
 insert into cr_extension_mime_type_map (extension, mime_type) 
