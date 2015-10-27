a469884ee9c7a7eb0e0e9224e2d532fb215f0660
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2015-10-27 10:42:32 +0000

    - update cr_content_mime_type_map as well (thanks to felix moedritscher)

--- packages/acs-content-repository/sql/common/upgrade/upgrade-5.9.0b2-5.9.0b3.sql
+++ packages/acs-content-repository/sql/common/upgrade/upgrade-5.9.0b2-5.9.0b3.sql
@@ -3,6 +3,8 @@ insert into cr_mime_types (label,mime_type,file_extension)
        where not exists (select 1 from cr_mime_types where mime_type = 'image/svg+xml');
        
 update cr_revisions set mime_type = 'image/svg+xml' where mime_type = 'imate/xml+svg';
+update cr_content_mime_type_map set mime_type = 'image/svg+xml' where mime_type = 'image/xml+svg';
+
 delete from cr_mime_types where mime_type = 'image/xml+svg';
 
 delete from cr_extension_mime_type_map where extension = 'svg';
