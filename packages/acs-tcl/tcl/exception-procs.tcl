3ce18c664273011bfe41c55d3df54ae2c683ff21
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2015-12-31 17:02:42 +0000

    - new function "ad_exception" to check, if an exception was raised be OpenACS

--- packages/acs-tcl/tcl/exception-procs.tcl
+++ packages/acs-tcl/tcl/exception-procs.tcl
@@ -1,59 +1,75 @@
 ad_library {
 
-  @author rhs@mit.edu
-  @creation-date 2000-09-09
-  @cvs-id $Id$
+    @author rhs@mit.edu
+    @creation-date 2000-09-09
+    @cvs-id $Id$
 }
 
 ad_proc -private ad_raise {exception {value ""}} {
-  @author rhs@mit.edu
-  @creation-date 2000-09-09
+    @author rhs@mit.edu
+    @creation-date 2000-09-09
 
-  Raise an exception.
+    Raise an exception.
 
-  If you use this I will kill you.
+    If you use this I will kill you.
 } {
-  return -code error -errorcode [list "AD" "EXCEPTION" $exception] $value
+    return -code error -errorcode [list "AD" "EXCEPTION" $exception] $value
+}
+
+ad_proc ad_exception {errorCode} {
+    @author gustaf.neumann@wu-wien.ac.at
+    @creation-date 2015-12-31
+
+    Check if the exception was caused by ad_raise (i.e. was an OpenACS
+    exception)
+    
+    @return ad_exception value or empty, in case the exception had other causes
+} {
+    lassign $errorCode flag type value
+    if {$flag eq "AD" && $type eq "EXCEPTION"} {
+        return $value
+    }
+    return ""
 }
 
 ad_proc -private ad_try {code args} {
 
-  @author rhs@mit.edu
-  @creation-date 2000-09-09
+    @author rhs@mit.edu
+    @creation-date 2000-09-09
 
-  Executes $code, catches any exceptions thrown by ad_raise and runs
-  any matching exception handlers.
+    Executes $code, catches any exceptions thrown by ad_raise and runs
+    any matching exception handlers.
 
-  If you use this I will kill you.
+    If you use this I will kill you.
 
-  @see with_finally 
-  @see with_catch
+    @see with_finally 
+    @see with_catch
 } {
 
-  if {[set errno [catch {uplevel $code} result]]} {
-    if {$errno == 1 
-	&& [lindex $::errorCode 0] eq "AD"
-	&& [lindex $::errorCode 1] eq "EXCEPTION"
-    } {
-      set exception [lindex $::errorCode 2]
+    if {[set errno [catch {uplevel $code} result]]} {
+        if {$errno == 1 
+            && [lindex $::errorCode 0] eq "AD"
+            && [lindex $::errorCode 1] eq "EXCEPTION"
+        } {
+            set exception [lindex $::errorCode 2]
 
-      set matched 0
-      for {set i 0} {$i < [llength $args]} {incr i 3} {
-	if {[string match [lindex $args $i] $exception]} {
-	  set matched 1
-	  break
-	}
-      }
+            set matched 0
+            for {set i 0} {$i < [llength $args]} {incr i 3} {
+                if {[string match [lindex $args $i] $exception]} {
+                    set matched 1
+                    break
+                }
+            }
 
-      if {$matched} {
-	upvar [lindex $args $i+1] var
-	set var $result
-	set errno [catch {uplevel [lindex $args $i+2]} result]
-      }
+            if {$matched} {
+                upvar [lindex $args $i+1] var
+                set var $result
+                set errno [catch {uplevel [lindex $args $i+2]} result]
+            }
+        }
+
+        return -code $errno -errorcode $::errorCode -errorinfo $::errorInfo $result
     }
-
-    return -code $errno -errorcode $::errorCode -errorinfo $::errorInfo $result
-  }
 }
 
 # Local variables:
