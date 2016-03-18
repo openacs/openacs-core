050c9a7e0e681138c191fac50c6668e97381053e
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2016-03-18 13:41:57 +0000

    - add more Open XML formats for MS-Office

--- packages/acs-content-repository/sql/common/mime-type-data.sql
+++ packages/acs-content-repository/sql/common/mime-type-data.sql
@@ -503,6 +503,19 @@ insert into cr_extension_mime_type_map (mime_type, extension) values ('applicati
 insert into cr_extension_mime_type_map (mime_type, extension) values ('application/vnd.openxmlformats-officedocument.wordprocessingml.template', 'dotx');
 
 
+insert into cr_mime_types(label, mime_type, file_extension) select 'Microsoft Office Word macro enabled', 'application/vnd.ms-word.document.macroenabled.12', 'docm' from dual;
+insert into cr_mime_types(label, mime_type, file_extension) select 'Microsoft Office Word Template macro enabled', 'application/vnd.ms-word.template.macroenabled.12', 'dotm' from dual;
+insert into cr_mime_types(label, mime_type, file_extension) select 'Microsoft Office Excel macro enabled', 'application/vnd.ms-excel.sheet.macroenabled.12', 'xlsm' from dual;
+insert into cr_mime_types(label, mime_type, file_extension) select 'Microsoft Office Excel Template macro enabled', 'application/vnd.ms-excel.template.macroenabled.12', 'xltm' from dual;
+insert into cr_mime_types(label, mime_type, file_extension) select 'Microsoft Office Excel Addin macro enabled', 'application/vnd.ms-excel.addin.macroenabled.12', 'xlam' from dual;
+insert into cr_mime_types(label, mime_type, file_extension) select 'Microsoft Office Excel Sheet binary macro enabled', 'application/vnd.ms-excel.sheet.binary.macroenabled.12', 'xlsb' from dual;
+insert into cr_mime_types(label, mime_type, file_extension) select 'Microsoft Office PowerPoint Addin macro enabled', 'application/vnd.ms-powerpoint.addin.macroenabled.12', 'ppam' from dual;
+insert into cr_mime_types(label, mime_type, file_extension) select 'Microsoft Office PowerPoint Presentation macro enabled', 'application/vnd.ms-powerpoint.presentation.macroenabled.12', 'pptm' from dual;
+insert into cr_mime_types(label, mime_type, file_extension) select 'Microsoft Office PowerPoint Template macro enabled', 'application/vnd.ms-powerpoint.template.macroenabled.12', 'potm' from dual;
+insert into cr_mime_types(label, mime_type, file_extension) select 'Microsoft Office PowerPoint Slideshow macro enabled', 'application/vnd.ms-powerpoint.slideshow.macroenabled.12', 'ppsm' from dual;
+
+
+
 --  Here are some less common mime types and extensions not defined here.
 --
 --  tsp       | application/dsptype
