15205cca851364c2aa496fdc0938adabae046217
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2016-05-13 18:00:05 +0000

    - fix potential traversal attack

--- packages/acs-api-browser/tcl/acs-api-documentation-procs.tcl
+++ packages/acs-api-browser/tcl/acs-api-documentation-procs.tcl
@@ -1472,15 +1472,7 @@ namespace eval ::apidoc {
         
         @return sanitized path
     } {
-
-        if {[regsub -all {[.][.]/} $path "" shortened_path]} {
-            set filename "$::acs::rootdir/$path"
-            ns_log notice [subst {INTRUDER ALERT:\n\nsomesone tried to snarf '$filename'!
-                file exists: [file exists $filename] user_id: [ad_conn user_id] peer: [ad_conn peeraddr]
-            }]
-            set path $shortened_path
-        }
-
+        set path [ns_normalizepath $path]
         if {![string match "$prefix/*" $path]} {
             set filename "$::acs::rootdir/$path"
             ns_log notice [subst {INTRUDER ALERT:\n\nsomesone tried to snarf '$filename'!
