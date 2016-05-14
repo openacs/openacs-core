c17f403db64c87417b8da67f6461689b13670e16
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2016-05-14 10:11:54 +0000

    - stenghten page contract to avoid attack vactors

--- packages/acs-templating/www/doc/demo/list9/index.tcl
+++ packages/acs-templating/www/doc/demo/list9/index.tcl
@@ -6,7 +6,7 @@ ad_page_contract {
   @creation-date 2000-10-23
   @cvs-id $Id$
 } -query {
-  orderby:optional
+  orderby:token,optional
   color_filter_value:optional
   page:naturalnum,optional
 } -properties {
