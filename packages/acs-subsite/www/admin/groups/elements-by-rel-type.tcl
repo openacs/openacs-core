3d8a199a93825aacbd1e2fada9948bc65a4481c6
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2016-06-06 18:29:31 +0000

    - remove misleading sql statement

--- packages/acs-subsite/www/admin/groups/elements-by-rel-type.tcl
+++ packages/acs-subsite/www/admin/groups/elements-by-rel-type.tcl
@@ -30,38 +30,7 @@ set create_p [permission::permission_p -object_id $group_id -privilege "create"]
 set return_url "[ad_conn url]?[ad_conn query]"
 set return_url_enc [ad_urlencode $return_url]
 
-db_multirow -extend {elements_display_url relations_add_url} rels relations_query { 
-    select g.rel_type, g.group_rel_id,
-           acs_object_type.pretty_name(g.rel_type) as rel_type_pretty_name,
-           s.segment_id, s.segment_name, 
-           acs_rel_type.role_pretty_plural(rel_types.role_two) as role_pretty_plural,
-           acs_rel_type.role_pretty_name(rel_types.role_two) as role_pretty_name,
-           rels.num_rels,
-           decode(valid_types.group_id, null, 0, 1) as rel_type_valid_p
-      from group_rels g, 
-           rel_segments s, 
-           acs_rel_types rel_types,
-           (select rel_type, count(*) as num_rels
-              from group_component_map
-             where group_id = :group_id
-               and group_id = container_id
-           group by rel_type
-           UNION ALL
-           select rel_type, count(*) as num_rels
-             from group_approved_member_map
-             where group_id = :group_id
-               and group_id = container_id
-           group by rel_type) rels,
-           rc_valid_rel_types valid_types
-     where g.group_id = s.group_id(+)
-       and g.rel_type = s.rel_type(+)
-       and g.rel_type = rels.rel_type(+)
-       and g.rel_type = rel_types.rel_type
-       and g.group_id = :group_id
-       and g.group_id = valid_types.group_id(+)
-       and g.rel_type = valid_types.rel_type(+)
-     order by lower(g.rel_type)
-} {
+db_multirow -extend {elements_display_url relations_add_url} rels relations_query {} {
     # The role pretty names can be message catalog keys that need
     # to be localized before they are displayed
     set role_pretty_name [lang::util::localize $role_pretty_name]
