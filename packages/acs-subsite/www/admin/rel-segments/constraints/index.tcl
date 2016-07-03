0668e80dba15dcacbd7fdc7036736fdb62fc1f40
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2016-07-03 14:19:14 +0000

    - reduce dependencies on acs_object_party_privilege_map (for PostgreSQL)

--- packages/acs-subsite/www/admin/rel-segments/constraints/index.tcl
+++ packages/acs-subsite/www/admin/rel-segments/constraints/index.tcl
@@ -1,5 +1,3 @@
-# /packages/mbryzek-subsite/www/admin/rel-segments/constraints/index.tcl
-
 ad_page_contract {
 
     Shows all constraints on which the user has read permission
@@ -10,28 +8,14 @@ ad_page_contract {
 
 }
 
-set context [list [list ../ "Relational segments"] "Constraints"]
-
-set user_id [ad_conn user_id]
-
+set context    [list [list ../ "Relational segments"] "Constraints"]
+set user_id    [ad_conn user_id]
 set package_id [ad_conn package_id]
 
 # Select out basic information about all the constraints on which the
 # user has read permission
 
-db_multirow constraints select_rel_constraints {
-    select c.constraint_id, c.constraint_name
-      from rel_constraints c, acs_object_party_privilege_map perm,
-           application_group_segments s1, application_group_segments s2
-     where perm.object_id = c.constraint_id
-       and perm.party_id = :user_id
-       and perm.privilege = 'read'
-       and s1.segment_id = c.rel_segment
-       and s1.package_id = :package_id
-       and s2.segment_id = c.required_rel_segment
-       and s2.package_id = :package_id
-     order by lower(c.constraint_name)
-}
+db_multirow constraints select_rel_constraints {}
 
 ad_return_template
 
