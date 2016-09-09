dee94a826bbe46adba0c36244a78b07fae0c63f3
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2016-09-09 07:32:25 +0000

    - fix upgrade script for PostgreSQL before 9.2: the old version checked   already the version number, but actually the SQL compilation failed   due to the unknown "IF EXISTS" for sequences.

--- packages/acs-kernel/sql/postgresql/upgrade/upgrade-5.9.1d3-5.9.1d4.sql
+++ packages/acs-kernel/sql/postgresql/upgrade/upgrade-5.9.1d3-5.9.1d4.sql
@@ -1,30 +1,20 @@
-create or replace function inline_0 ()
+CREATE or REPLACE FUNCTION inline_0 ()
 returns integer as $$
 DECLARE
    v_dummy integer;
 BEGIN
-   select setting from pg_settings where name='server_version_num' and setting::int >= 90200 into v_dummy;
-   IF found THEN
+    SELECT 1 FROM pg_views WHERE viewname = 'anon_func_seq' INTO v_dummy;
+    IF found THEN
 
-        select 1 from pg_views where viewname = 'anon_func_seq' into v_dummy;
-	IF found THEN
-      	   drop view IF EXISTS anon_func_seq;
-   	   ALTER SEQUENCE IF EXISTS t_anon_func_seq RENAME TO anon_func_seq;
-	END IF;
-
-   ELSE
-	-- verison earlier than 9.2, no "IF EXISTS" for ALTER SEQUENCE
-        select 1 from pg_views where viewname = 'anon_func_seq' into v_dummy;
-	IF found THEN
-	   drop view anon_func_seq;
-      	   ALTER SEQUENCE t_anon_func_seq RENAME TO anon_func_seq;
-	END IF;
+        DROP VIEW IF EXISTS anon_func_seq;
+        IF EXISTS (SELECT 0 FROM pg_class WHERE relname = 't_anon_func_seq' ) THEN
+            ALTER SEQUENCE t_anon_func_seq RENAME TO anon_func_seq;
+        END IF;
 
    END IF;
    return 1;
 END;
-$$ language 'plpgsql';
+$$ language plpgsql;
 
 select inline_0();
 drop function inline_0();
-
