4265811d4cb7e934b2b8cb95e5a3121510fb3276
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2016-11-10 14:45:09 +0000

    - offer "Pages" only, when there is an index page

--- packages/acs-admin/lib/service-parameters.tcl
+++ packages/acs-admin/lib/service-parameters.tcl
@@ -12,16 +12,18 @@ if { ![acs_user::site_wide_admin_p] } {
 set user_id [ad_conn user_id]
 set swadmin_p 0
 db_multirow -extend { url admin_url param_url } packages services_select {} {
-    if { [file exists "[acs_package_root_dir $package_key]/www/"] } {
-        catch {
-            set url [apm_package_url_from_key $package_key]
-            if { $url ne "" && [file exists "[acs_package_root_dir $package_key]/www/admin/"] } {
-                set admin_url "${url}admin/"
-            }
-            if { [file exists "[acs_package_root_dir $package_key]/www/sitewide-admin/"] } {
-                set sitewide_admin_url "/acs-admin/package/$package_key/"
-                set swadmin_p 1
-            }
+    set root_dir [acs_package_root_dir $package_key]
+    if { [file exists $root_dir/www/] } {
+        set url [apm_package_url_from_key $package_key]
+        if { $url ne "" && [file exists $root_dir/www/admin/] } {
+            set admin_url "${url}admin/"
+        }
+        if { [file exists $root_dir/www/sitewide-admin/] } {
+            set sitewide_admin_url "/acs-admin/package/$package_key/"
+            set swadmin_p 1
+        }
+        if {[glob -nocomplain $root_dir/www/index.*] eq ""} {
+            set url ""
         }
     }
     if { $parameter_count > 0 } {
