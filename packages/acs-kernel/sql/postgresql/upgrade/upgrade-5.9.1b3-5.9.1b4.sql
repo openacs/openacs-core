ced7fb3dc1e5979c4b3e7a015214161e2fde3341
Antonio Pisano <antonio@elettrotecnica.it>
2017-03-21 16:21:20 +0000

    Fix upgrade logics, because old instances named constraint differently. Also, skip execution when not needed, as can be demanding (acts on acs_objects).

--- packages/acs-kernel/sql/postgresql/upgrade/upgrade-5.9.1b3-5.9.1b4.sql
+++ packages/acs-kernel/sql/postgresql/upgrade/upgrade-5.9.1b3-5.9.1b4.sql
@@ -5,10 +5,23 @@
 
 begin;
 
-alter table acs_rels drop constraint acs_rels_object_id_one_fk;
-alter table acs_rels drop constraint acs_rels_object_id_two_fk;
+-- make sure the old constraint names don't exist
+alter table acs_rels drop constraint if exists acs_object_rels_one_fk;
+alter table acs_rels drop constraint if exists acs_object_rels_two_fk;
 
-alter table acs_rels add constraint acs_rels_object_id_one_fk foreign key (object_id_one) references acs_objects(object_id) on delete cascade;
-alter table acs_rels add constraint acs_rels_object_id_two_fk foreign key (object_id_two) references acs_objects(object_id) on delete cascade;
+-- create the new constraints if they don't exist already
+DO $$
+BEGIN
+   BEGIN
+      alter table acs_rels add constraint acs_rels_object_id_one_fk foreign key (object_id_one) references acs_objects(object_id) on delete cascade;
+   EXCEPTION
+      WHEN duplicate_object THEN RAISE NOTICE 'Table constraint acs_rels_object_id_one_fk already exists, skipping';
+   END;
+   BEGIN
+      alter table acs_rels add constraint acs_rels_object_id_two_fk foreign key (object_id_two) references acs_objects(object_id) on delete cascade;
+   EXCEPTION
+      WHEN duplicate_object THEN RAISE NOTICE 'Table constraint acs_rels_object_id_two_fk already exists, skipping';
+   END;
+END $$;
 
 end;
