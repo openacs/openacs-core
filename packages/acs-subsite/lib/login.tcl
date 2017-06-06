3904fb0500abe2eb35b071c25bc4397b7946b44a
Antonio Pisano <antonio@elettrotecnica.it>
2017-06-06 15:01:46 +0000

    Make sure message exists when dealing with a banned user

--- packages/acs-subsite/lib/login.tcl
+++ packages/acs-subsite/lib/login.tcl
@@ -294,9 +294,10 @@ ad_form -extend -name login -on_request {
 		ad_return_template
 		
 	    } else {
+                set message [expr { [info exists auth_info(account_message)] ? $auth_info(account_message) : "" }]
 		# Display the message on a separate page
-            ad_returnredirect \
-                -message $auth_info(account_message) \
+                ad_returnredirect \
+                -message $message \
                 -html \
                 [export_vars \
                      -base "[subsite::get_element -element url]register/account-closed"]
