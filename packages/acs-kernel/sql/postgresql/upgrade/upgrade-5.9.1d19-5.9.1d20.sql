05f90597cf0f8a5074e3f9d866738de8aa40049e
Antonio Pisano <antonio@elettrotecnica.it>
2017-10-05 11:18:24 +0000

    Backfix upgrade to be PostgreSQL 9.0 compliant

--- packages/acs-kernel/sql/postgresql/upgrade/upgrade-5.9.1d19-5.9.1d20.sql
+++ packages/acs-kernel/sql/postgresql/upgrade/upgrade-5.9.1d19-5.9.1d20.sql
@@ -7,14 +7,12 @@ select define_function_args('util__get_primary_keys','table');
 --
 -- procedure util__get_primary_keys/1
 --
-CREATE OR REPLACE FUNCTION util__get_primary_keys(
-   p_table text
-) RETURNS SETOF pg_attribute.attname%TYPE AS $$
+CREATE OR REPLACE FUNCTION util__get_primary_keys(text) RETURNS SETOF pg_attribute.attname%TYPE AS $$
   SELECT a.attname
     FROM pg_index i
     JOIN pg_attribute a ON a.attrelid = i.indrelid
                        AND a.attnum = ANY(i.indkey)
-  WHERE i.indrelid = p_table::regclass
+  WHERE i.indrelid = $1::regclass
     AND i.indisprimary;
 $$ LANGUAGE sql;
 
