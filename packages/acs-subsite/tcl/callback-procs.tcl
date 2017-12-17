282dc5fb2e597dd46b9e969e1505d1b288560c2d
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2017-12-17 18:02:43 +0000

    white space changes

--- packages/acs-subsite/tcl/callback-procs.tcl
+++ packages/acs-subsite/tcl/callback-procs.tcl
@@ -1,5 +1,5 @@
 ad_library {
-    Callback definitions 
+    Callback definitions
 
     @author Jeff Davis <davis@xarg.net>
     @creation-date 2005-03-11
@@ -10,28 +10,28 @@ ad_proc -public -callback user::workspace {
     -user_id
 } {
     used to generate html fragments for display on the /pvt/home page.
-    
+
     The HTML fragment should have an h2 header for sectioning.
-    
+
     @param user_id - the user to display
     @see callback::user::workspace::impl::acs-subsite
 } -
 
 ad_proc -public -callback user::workspace -impl acs-subsite {
-	-user_id
+        -user_id
 } {
-    Generate a table showing the application group membership 
+    Generate a table showing the application group membership
 } {
     set themed_template [template::themed_template /packages/acs-subsite/lib/user-subsites]
     return [template::adp_include $themed_template [list user_id $user_id]]
 }
 
-ad_proc -public -callback user::registration { 
-    -package_id 
+ad_proc -public -callback user::registration {
+    -package_id
 } {
     used to verify if there is another registration process.
     The implementation must return the url of the registration page.
-} - 
+} -
 
 ad_proc -callback subsite::get_extra_headers {
 } {
