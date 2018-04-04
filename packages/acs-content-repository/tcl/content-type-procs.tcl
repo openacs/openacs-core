ca84d5cfee04e8bea16c4685633a2a39b72b89df
Hector Romojaro <hector.romojaro@gmail.com>
2018-04-04 19:17:41 +0000

    Fix typos

--- packages/acs-content-repository/tcl/content-type-procs.tcl
+++ packages/acs-content-repository/tcl/content-type-procs.tcl
@@ -52,7 +52,7 @@ ad_proc -public content::type::delete {
     @param content_type
     @param drop_children_p
     @param drop_table_p
-    @param drop_objets_p Drop the objects of this content type along with all entries in cr_items and cr_revisions. Will not be done by default.
+    @param drop_objects_p Drop the objects of this content type along with all entries in cr_items and cr_revisions. Will not be done by default.
 } {
     if {$drop_objects_p == "f"} {
 	return [package_exec_plsql -var_list [list \
