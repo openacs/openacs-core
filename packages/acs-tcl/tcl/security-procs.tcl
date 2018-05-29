6fc103d08d03382544e8aab2d3d905e627918e15
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2018-05-29 15:14:44 +0000

    flag current request as being performed via aa_testing

--- packages/acs-tcl/tcl/security-procs.tcl
+++ packages/acs-tcl/tcl/security-procs.tcl
@@ -126,6 +126,7 @@ ad_proc -private sec_handler {} {
                     #ad_conn -set session_id [sec_allocate_session]
                     set auth_level ok
                     set untrusted_user_id $user_id
+                    set ::__aa_testing_mode 1
                 }
             }
         } else {
