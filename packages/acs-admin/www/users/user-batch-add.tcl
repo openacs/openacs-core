06e40a23e2859c9de2a4f57b50da5303b9415d01
Antonio Pisano <antonio@elettrotecnica.it>
2018-05-29 08:34:41 +0000

    Use more API when retrieving user info

--- packages/acs-admin/www/users/user-batch-add.tcl
+++ packages/acs-admin/www/users/user-batch-add.tcl
@@ -10,12 +10,10 @@ ad_page_contract {
 }
 
 set admin_user_id [ad_conn user_id]
-set admin_email [db_string unused {
-    select email from parties where party_id = :admin_user_id
-}]
-set administration_name [db_string admin_name {
-    select first_names || ' ' || last_name from persons where person_id = :admin_user_id
-}]
+
+set admin [acs_user::get -user_id $admin_user_id]
+set email               [dict get $admin email]
+set administration_name [dict get $admin name]
 
 set context [list [list "./" "Users"] "Notify added user"]
 set system_name [ad_system_name]
