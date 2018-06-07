a58d054b0655ca9fad320ffc452063afb702b4fc
Hector Romojaro <hector.romojaro@gmail.com>
2018-06-07 16:35:39 +0000

    Deprecate wrappers for 'permission::permission_p' and permission::require_permission.

--- packages/notifications/tcl/notifications-security-procs.tcl
+++ packages/notifications/tcl/notifications-security-procs.tcl
@@ -29,50 +29,66 @@ namespace eval notification::security {
         return [db_string user_approved_p {} -default 0]
     }
 
-    ad_proc -public can_notify_object_p {
+    ad_proc -deprecated -public can_notify_object_p {
         {-user_id ""}
         {-object_id:required}
     } {
         This checks if a user can request notification on a given object.
 
+        Deprecated: reduce the amount of wrappers to permission::permission_p
+
         @param user_id
         @param object_id
+
+        @see permission::permission_p
     } {
         return [permission::permission_p -party_id $user_id -object_id $object_id -privilege "read"]
     }
 
-    ad_proc -public require_notify_object {
+    ad_proc -deprecated -public require_notify_object {
         {-user_id ""}
         {-object_id:required}
     } {
         Require the ability to notify on an object.
 
+        Deprecated: just a wrapper to permission::require_permission
+
         @param user_id
         @param object_id
+
+        @see permission::require_permission
     } {
         permission::require_permission -party_id $user_id -object_id $object_id -privilege "read"
     }
 
-    ad_proc -public can_admin_request_p {
+    ad_proc -deprecated -public can_admin_request_p {
         {-user_id ""}
         {-request_id:required}
     } {
         Checks if a user can manage a given notification request.
 
+        Deprecated: reduce the amount of wrappers to permission::permission_p
+
         @param user_id
         @param request_id
+
+        @see permission::permission_p
     } {
         return [permission::permission_p -party_id $user_id -object_id $request_id -privilege "admin"]
     }
 
-    ad_proc -public require_admin_request {
+    ad_proc -deprecated -public require_admin_request {
         {-user_id ""}
         {-request_id:required}
     } {
         Require the ability to admin a request.
 
+        Deprecated: just a wrapper to permission::require_permission
+
         @param user_id
         @param request_id
+
+        @see permission::require_permission
     } {
         permission::require_permission -party_id $user_id -object_id $object_id -privilege "admin"
     }
