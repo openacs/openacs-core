29459c9c497fd20d020cbdf99027288b31912fb3
gernst <>
2018-06-25 14:59:07 +0000

    Removed redundant cache creation

--- packages/acs-tcl/tcl/acs-permissions-procs.tcl
+++ packages/acs-tcl/tcl/acs-permissions-procs.tcl
@@ -313,17 +313,6 @@ if {[info commands ns_cache_eval] ne ""} {
     # Some of this code will go away, when abstract cache management
     # will be introduced.
     #
-    try {
-        ns_cache_flush permission_cache NOTHING
-    } on error {errorMsg} {
-        ns_log notice "acs-tcl: creating permission cache"
-        ns_cache_create \
-            -expires [parameter::get -package_id [ad_acs_kernel_id] \
-                          -parameter PermissionCacheTimeout \
-                          -default 300] \
-            permission_cache 100000
-    }
-
     #
     # run permission call or get permission from cache
     #
