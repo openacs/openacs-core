ac70d9ecf53499ebe22f0e4b0ca2307e75c456e9
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2018-06-26 09:28:01 +0000

    revert to previous version, since "Administration" links were plain wrong (not pointing to SUBSITE/admin but to a constant url)

--- packages/acs-admin/www/subsites.tcl
+++ packages/acs-admin/www/subsites.tcl
@@ -18,33 +18,20 @@ if {$subsite_number > 500} {
 } else {
     set too_many_subsites_p 0
 
-    db_multirow -extend {
-        theme
-        theme_url
-        admin_url
-        path_pretty
-        parameter_url
-        node_url
-    } subsites subsite_admin_urls [subst {
-        select s.node_id,
-               p.package_id
-        from   site_nodes s, apm_packages p
-        where  s.object_id = p.package_id
-        and    p.package_key in ($package_keys)        
-    }] {
+    db_multirow -extend { theme theme_url admin_url path_pretty parameter_url} subsites subsite_admin_urls {} {
         set admin_url "${node_url}admin/"
         set parameter_url [export_vars -base /shared/parameters {package_id {return_url "[ad_conn url]"}}]
-        set node [site_node::get -node_id $node_id]
-        set path_pretty [dict get $node instance_name]
-        set parent_id   [dict get $node parent_id]
-        set node_url    [dict get $node url]
+        set path_pretty $instance_name
+        array set node [site_node::get -node_id $node_id]
+        set parent_id $node(parent_id)
         set theme [parameter::get -parameter ThemeKey -package_id $package_id]
         set theme_url ${admin_url}themes/
         
         while { $parent_id ne "" } {
-            set node [site_node::get -node_id $parent_id]
-            set path_pretty "[dict get $node instance_name] > $path_pretty"
-            set parent_id [dict get $node parent_id]
+            array unset node
+            array set node [site_node::get -node_id $parent_id]
+            set path_pretty "$node(instance_name) > $path_pretty"
+            set parent_id $node(parent_id)
         }
     }
     multirow sort subsites path_pretty
@@ -64,6 +51,7 @@ if {$subsite_number > 500} {
                 display_template {\#acs-admin.Pages#}
                 html {align left}
             }
+
             theme {
                 label "Theme"
                 link_url_col theme_url
