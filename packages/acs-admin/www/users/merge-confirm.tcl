28ace7d750499a8da470986b968eb5dbfa329fc9
Antonio Pisano <antonio@elettrotecnica.it>
2018-06-27 16:56:41 +0000

    Prefer on-spot api as it is quicker

--- packages/acs-admin/www/users/merge-confirm.tcl
+++ packages/acs-admin/www/users/merge-confirm.tcl
@@ -12,8 +12,8 @@ ad_page_contract {
     context:onevalue
 } -validate {
     if_diff_authority {        
-        set from_authority_id [acs_user::get_element -user_id $from_user_id -element authority_id]
-        set to_authority_id [acs_user::get_element -user_id $to_user_id -element authority_id]
+        set from_authority_id [acs_user::get_user_info -user_id $from_user_id -element authority_id]
+        set to_authority_id [acs_user::get_user_info -user_id $to_user_id -element authority_id]
 	if { $from_authority_id ne $to_authority_id } {
 	    ad_complain "Merge only works for users from the same authority"
 	}
