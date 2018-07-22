c1ba5846f8e460755407bb1c59c7b6e0826fdc6a
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2018-07-22 09:46:48 +0000

    rework test cases, make tested procs explicit

--- packages/acs-lang/tcl/test/acs-lang-localization-procs.tcl
+++ packages/acs-lang/tcl/test/acs-lang-localization-procs.tcl
@@ -17,6 +17,10 @@ aa_register_case \
       lc_time_utc_to_local
       lc_time_local_to_utc
 
+      lang::catalog::import
+      lang::catalog::import
+      lang::system::locale_set_enabled
+
     } lang_test__lc_numeric {
 
     Minimal testset for localization functions. To be extended.
@@ -32,7 +36,7 @@ aa_register_case \
 
     lang::catalog::import -locales it_IT -package_key acs-lang
     lang::catalog::import -locales de_DE -package_key acs-lang
-    
+
     aa_equals "format us number" [lc_numeric 123456.789 {} en_US] 123,456.789
     aa_equals "format de number" [lc_numeric 123456.789 {} de_DE] 123.456,789
     aa_equals "format it number" [lc_numeric 123456.789 {} it_IT] 123.456,789
@@ -40,14 +44,14 @@ aa_register_case \
     aa_equals "parse us number" [lc_parse_number 123,456.789 en_US] 123456.789
     aa_equals "parse de number" [lc_parse_number 123.456,789 de_DE] 123456.789
     aa_equals "parse it number" [lc_parse_number 123.456,789 it_IT] 123456.789
-    
-    set time [lc_time_fmt "2013-09-21 23:11:22" "%c" en_US] 
+
+    set time [lc_time_fmt "2013-09-21 23:11:22" "%c" en_US]
     aa_equals "format us time" [lrange $time 0 end-1] "Sat September 21, 2013 11:11 PM"
 
-    set time [lc_time_fmt "2013-09-21 23:11:22" "%c" de_DE] 
+    set time [lc_time_fmt "2013-09-21 23:11:22" "%c" de_DE]
     aa_equals "format us time" [lrange $time 0 end-1] "Sa, 21. September 2013 23:11"
 
-    set time [lc_time_fmt "2013-09-21 23:11:22" "%c" it_IT] 
+    set time [lc_time_fmt "2013-09-21 23:11:22" "%c" it_IT]
     aa_equals "format us time" [lrange $time 0 end-1] "Sab 21 Settembre 2013 23:11"
 
     aa_equals "asian time " [lc_time_utc_to_local "2013-09-21 23:11:22" "Asia/Tokyo"] "2013-09-22 08:11:22"
--- packages/acs-lang/tcl/test/acs-lang-message-procs.tcl
+++ packages/acs-lang/tcl/test/acs-lang-message-procs.tcl
@@ -4,31 +4,37 @@ ad_library {
    @creation-date 11 Aug 2006
 }
 
-aa_register_case -cats {smoke api} \
-                 -procs {lang::message::register lang::message::get} \
-                  test_message_register {
+aa_register_case \
+    -cats {smoke api} \
+    -procs {
+        lang::message::register
+        lang::message::get
+    } \
+    test_message_register {
     Simple test that registrates a new message to the BD.
-   
+
 } {
     aa_run_with_teardown -rollback -test_code {
 
-	set message_key [ad_generate_random_string]
+        set message_key [ad_generate_random_string]
         set message [ad_generate_random_string]
         set package_key "acs-translations"
         set locale "en_US"
         aa_log "Creating message : $message || message key: $message_key"
-        
+
         # Creates the new message
         lang::message::register $locale $package_key $message_key $message
 
         # Try to retrieve the new message created.
 
-        lang::message::get -package_key $package_key -message_key $message_key -locale $locale -array message_new
+        lang::message::get \
+            -package_key $package_key \
+            -message_key $message_key \
+            -locale $locale \
+            -array message_new
 
-        aa_true "Message add succeeded" [string equal $message_new(message) $message] 
-
+        aa_equals "Message add succeeded" $message_new(message) $message
     }
-
 }
 
 
