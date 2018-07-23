9407700eb0230beffcbb0d04003d7f7afb0c97cb
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2018-07-23 11:42:56 +0000

    fix too many colons

--- packages/acs-automated-testing/www/admin/record-test.tcl
+++ packages/acs-automated-testing/www/admin/record-test.tcl
@@ -102,13 +102,13 @@ ad_form -name new_test -method post -export {package_key return_url} \
 
     if {$login_id eq ""} {
         if {$login_type == -2} {
-#	    set login_code "twt::user:::create"
+#	    set login_code "twt::user::create"
 	    set login_code "
-     array set user_info \[twt::user:::create\]
+     array set user_info \[twt::user::create\]
      twt::user::login \$user_info(email) \$user_info(password)
  "
         } elseif {$login_type == -1} {
-#	    set login_code "twt::user:::create -admin"
+#	    set login_code "twt::user::create -admin"
 	    set login_code "
      array set user_info \[twt::user::create -admin\]
      twt::user::login \$user_info(email) \$user_info(password)
