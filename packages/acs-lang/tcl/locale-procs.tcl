a67c0e5a9c65569b583a4cdd5445b0310de09c8d
Hector Romojaro <hector.romojaro@gmail.com>
2018-07-25 19:31:31 +0000

    Harmonize parameter names in proc documentation removing unexistent ones

--- packages/acs-lang/tcl/locale-procs.tcl
+++ packages/acs-lang/tcl/locale-procs.tcl
@@ -697,7 +697,7 @@ ad_proc -public lang::conn::charset {
     Returns the MIME charset name corresponding to the current connection's locale.
 
     @author        Lars Pind (lars@pinds.com)
-    @param locale  Name of a locale, as language_COUNTRY using ISO 639 and ISO 3166
+
     @return        IANA MIME character set name
 } {
     return [lang::util::charset_for_locale [lang::conn::locale]]
