7559832518d47356dc2a5be8b755af65d2b1cd11
Hector Romojaro <hector.romojaro@gmail.com>
2018-07-25 12:55:01 +0000

    Fix misleading and uncoherent 'ad_dispatch' proc documentation, discovered using the 'documentation__check_parameters' test.

--- packages/acs-bootstrap-installer/tcl/00-proc-procs.tcl
+++ packages/acs-bootstrap-installer/tcl/00-proc-procs.tcl
@@ -1040,13 +1040,14 @@ ad_proc -public ad_dispatch {
     type
     args 
 } {
-    Calls method_name for the type of object_id with object_id as the
-    first arg, and the remaining args are the remainder of the args to
-    method_name. Example ad_call_method method1 foo bar baz calls the method1 associated with the type of foo, with foo bar and baz
-    as the 3 arguments.
+    Calls method_name with the type as the first arg, and the remaining args are
+    the remainder of the args to method_name.
+
+    Example: "ad_dispatch method1 foo bar baz" calls the method1 associated with
+             the type foo, with foo bar and baz as the 3 arguments.
 
     @param method_name method name
-    @param object_id the target, it is the first arg to the method
+    @param type associated type
     @param args the remaining arguments
 } {
     return [ad_apply ${method_name}__$type $args]
