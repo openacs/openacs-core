50da2e960ac83c5ddd280cd59313183acc886578
Antonio Pisano <antonio@elettrotecnica.it>
2018-09-14 10:24:08 +0000

    Move image::get_info together with its pals procs in the content repository... just to realize it should probably go away at some point

--- packages/acs-content-repository/tcl/image-procs.tcl
+++ packages/acs-content-repository/tcl/image-procs.tcl
@@ -12,7 +12,7 @@ ad_library {
     @cvs-id $Id$
 }
 
-namespace eval image:: {}
+namespace eval image {}
 
 ad_proc -public image::new {
     {-name ""}
@@ -156,6 +156,39 @@ ad_proc -public image::get_file_dimensions {
 	catch {set size [image::imagemagick_file_dimensions -filename $filename]}
     }
     return $size
+}
+
+ad_proc -public image::get_info {
+    {-filename:required}
+    {-array:required}
+} {
+    Get the width and height of an image file. 
+    The width and height are returned as 'height' and 'width' entries in the array named in the parameter.
+    Uses ImageMagick instead of AOLserver function because it can handle more than
+    just gifs and jpegs. The plan is to add the ability to get more details later.
+
+    This proc duplicates and seems somehow superseded by
+    image::imagemagick_identify, it might be a good idea to deprecate
+    it in the future. One important difference is this proc won't fail
+    in case of error.
+
+    @param filename Name of the image file in the file system.
+    @param array   Name of an array where you want the information returned.
+
+    @see image::imagemagick_identify
+} {
+    upvar 1 $array row
+    array set row {
+        height {}
+        width {}
+    }
+
+    catch {
+        set identify_string [exec identify $filename]
+        regexp {[ ]+([0-9]+)[x]([0-9]+)[\+]*} $identify_string x width height
+        set row(width) $width
+        set row(height) $height
+    }
 }
 
 ad_proc -public image::imagemagick_identify {
