382d12437c8cc49ba579143a1b6b2fab8bfee901
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2018-09-27 09:01:24 +0000

    whitespace cleanup

--- packages/acs-admin/tcl/acs-admin-procs.tcl
+++ packages/acs-admin/tcl/acs-admin-procs.tcl
@@ -10,7 +10,7 @@ namespace eval acs_admin {
 
     ad_proc ::acs_admin::check_expired_certificates {} {
 
-        Check expire-dates of certificates and send warning 
+        Check expire-dates of certificates and send warning
         emails to the admin. In case HTTPS is not configured via the
         "nsssl" driver, or the command line tool "openssl" openssl is
         installed, the proc does nothing.  } {
