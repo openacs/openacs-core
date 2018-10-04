56b05e1d93d416a934bb44974e5f50869ead2773
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2018-10-04 09:53:51 +0000

    add new proc "acs_sc_update_alias_wrappers" to allow refetching of wrapper functions

--- packages/acs-service-contract/tcl/acs-service-contract-procs.tcl
+++ packages/acs-service-contract/tcl/acs-service-contract-procs.tcl
@@ -218,7 +218,41 @@ ad_proc -private acs_sc_proc {
     return 1
 }
 
+ad_proc acs_sc_update_alias_wrappers {} {
 
+    Loop over actual bindings, finding every impl alias for each contract operation
+    and adfine a "acs_sc_proc" for all of these.
+    
+} {
+    db_foreach impl_operation {
+        select ia.impl_contract_name, 
+               ia.impl_operation_name,
+               ia.impl_name,
+               ia.impl_alias,
+               ia.impl_pl
+        from   acs_sc_bindings b, acs_sc_impl_aliases ia
+        where  ia.impl_id = b.impl_id
+    } {
+        #
+        # Create the AcsSc.Contract.Operation.Impl wrapper proc for this implementation
+        #
+        if {[catch {
+            #
+            # Check, if the wrapper exists already
+            #
+            set proc_name [acs_sc_generate_name $impl_contract_name $impl_name $impl_operation_name]
+            if {[info commands ::$proc_name] eq ""} {
+                #
+                # Create it new.
+                #
+                acs_sc_proc $impl_contract_name $impl_operation_name $impl_name $impl_alias $impl_pl
+            }
+        } errorMsg]} {
+            ns_log error "Service contract initialization failed, call was:\n\
+	           acs_sc_proc $impl_contract_name $impl_operation_name $impl_name $impl_alias $impl_pl" 
+        }
+    }
+}
 
 ad_proc -private acs_sc_get_statement {
     impl_alias
