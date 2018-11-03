8da7469b87f0fdebe2d1542045537246e1443800
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2018-11-03 11:08:47 +0000

    whitespace cleanup

--- packages/acs-tcl/tcl/test/site-nodes-test-procs.tcl
+++ packages/acs-tcl/tcl/test/site-nodes-test-procs.tcl
@@ -22,70 +22,70 @@ aa_register_case \
 } {
     aa_run_with_teardown -rollback -test_code {
 
-	aa_log "# 1) mount /doc1 /doc2 /doc1/doc3"
-	set doc1_name [ad_generate_random_string]
-	set doc2_name [ad_generate_random_string]
-	set doc3_name [ad_generate_random_string]
-	set node1_pkg_id [site_node::instantiate_and_mount \
-			      -node_name $doc1_name \
-			      -package_key acs-core-docs]
-	set node1_node_id [site_node::get_node_id -url "/$doc1_name"]
-	set node2_pkg_id [site_node::instantiate_and_mount \
-			      -node_name $doc2_name \
-			      -package_key acs-core-docs]
-	set node2_node_id [site_node::get_node_id -url "/$doc2_name"]
-	set node3_pkg_id [site_node::instantiate_and_mount \
-			      -parent_node_id $node1_node_id \
-			      -node_name $doc3_name \
-			      -package_key acs-core-docs]
-	set node3_node_id [site_node::get_node_id -url "/$doc1_name/$doc3_name"]
+        aa_log "# 1) mount /doc1 /doc2 /doc1/doc3"
+        set doc1_name [ad_generate_random_string]
+        set doc2_name [ad_generate_random_string]
+        set doc3_name [ad_generate_random_string]
+        set node1_pkg_id [site_node::instantiate_and_mount \
+                              -node_name $doc1_name \
+                              -package_key acs-core-docs]
+        set node1_node_id [site_node::get_node_id -url "/$doc1_name"]
+        set node2_pkg_id [site_node::instantiate_and_mount \
+                              -node_name $doc2_name \
+                              -package_key acs-core-docs]
+        set node2_node_id [site_node::get_node_id -url "/$doc2_name"]
+        set node3_pkg_id [site_node::instantiate_and_mount \
+                              -parent_node_id $node1_node_id \
+                              -node_name $doc3_name \
+                              -package_key acs-core-docs]
+        set node3_node_id [site_node::get_node_id -url "/$doc1_name/$doc3_name"]
         set root_node_id [site_node::get_node_id -url /]
-	aa_equals "Verify url /doc1 for node1" [site_node::get_url -node_id $node1_node_id] "/$doc1_name/"
-	aa_equals "Verify url /doc1/doc3 for node3" [site_node::get_url -node_id $node3_node_id] "/$doc1_name/$doc3_name/"
-	aa_equals "Verify url /doc2 for node2" [site_node::get_url -node_id $node2_node_id] "/$doc2_name/"
+        aa_equals "Verify url /doc1 for node1" [site_node::get_url -node_id $node1_node_id] "/$doc1_name/"
+        aa_equals "Verify url /doc1/doc3 for node3" [site_node::get_url -node_id $node3_node_id] "/$doc1_name/$doc3_name/"
+        aa_equals "Verify url /doc2 for node2" [site_node::get_url -node_id $node2_node_id] "/$doc2_name/"
 
-	aa_log "# 2) rename /doc1 => doc4: Test /doc4 /doc4/doc3 /doc2"
-	set doc4_name [ad_generate_random_string]
-	site_node::rename -node_id $node1_node_id -name $doc4_name
-	aa_equals "Check new url /doc4" [site_node::get_node_id -url "/$doc4_name"] $node1_node_id
-	aa_equals "Check new url /doc4/doc3" [site_node::get_node_id -url "/$doc4_name/$doc3_name"] $node3_node_id
-	aa_equals "Check old url /doc2" [site_node::get_node_id -url "/$doc2_name"] $node2_node_id
-	aa_equals "Make sure old url /doc1 now matches /" [site_node::get_node_id -url "/$doc1_name/"] $root_node_id
-	aa_equals "Make sure old url /doc1/doc3 now matches /" [site_node::get_node_id -url "/$doc1_name/$doc3_name/"] $root_node_id
-	aa_equals "Verify url /doc4 for node1" [site_node::get_url -node_id $node1_node_id] "/$doc4_name/"
-	aa_equals "Verify url /doc4/doc3 for node3" [site_node::get_url -node_id $node3_node_id] "/$doc4_name/$doc3_name/"
-	aa_equals "Verify url /doc2 for node2" [site_node::get_url -node_id $node2_node_id] "/$doc2_name/"
+        aa_log "# 2) rename /doc1 => doc4: Test /doc4 /doc4/doc3 /doc2"
+        set doc4_name [ad_generate_random_string]
+        site_node::rename -node_id $node1_node_id -name $doc4_name
+        aa_equals "Check new url /doc4" [site_node::get_node_id -url "/$doc4_name"] $node1_node_id
+        aa_equals "Check new url /doc4/doc3" [site_node::get_node_id -url "/$doc4_name/$doc3_name"] $node3_node_id
+        aa_equals "Check old url /doc2" [site_node::get_node_id -url "/$doc2_name"] $node2_node_id
+        aa_equals "Make sure old url /doc1 now matches /" [site_node::get_node_id -url "/$doc1_name/"] $root_node_id
+        aa_equals "Make sure old url /doc1/doc3 now matches /" [site_node::get_node_id -url "/$doc1_name/$doc3_name/"] $root_node_id
+        aa_equals "Verify url /doc4 for node1" [site_node::get_url -node_id $node1_node_id] "/$doc4_name/"
+        aa_equals "Verify url /doc4/doc3 for node3" [site_node::get_url -node_id $node3_node_id] "/$doc4_name/$doc3_name/"
+        aa_equals "Verify url /doc2 for node2" [site_node::get_url -node_id $node2_node_id] "/$doc2_name/"
 
-	aa_log "# 3) init_cache: Test /doc5 /doc5/doc3 /doc2"
-	set doc5_name [ad_generate_random_string]
-	db_dml rename_node1 {
-	    update site_nodes
-	    set name = :doc5_name
-	    where node_id = :node1_node_id
-	}
+        aa_log "# 3) init_cache: Test /doc5 /doc5/doc3 /doc2"
+        set doc5_name [ad_generate_random_string]
+        db_dml rename_node1 {
+            update site_nodes
+            set name = :doc5_name
+            where node_id = :node1_node_id
+        }
         ns_cache_transaction_rollback
-	site_node::init_cache
+        site_node::init_cache
         ns_cache_transaction_begin
 
-	aa_equals "Check url /doc5" [site_node::get_node_id -url "/$doc5_name"] $node1_node_id
-	aa_equals "Check url /doc5/doc3" [site_node::get_node_id -url "/$doc5_name/$doc3_name"] $node3_node_id
-	aa_equals "Check url /doc2" [site_node::get_node_id -url "/$doc2_name"] $node2_node_id
-	aa_equals "Make sure old url /doc1 now matches" [site_node::get_node_id -url "/$doc1_name/"] $root_node_id
-	aa_equals "Make sure old url /doc1/doc3 now matches" [site_node::get_node_id -url "/$doc1_name/$doc3_name/"] $root_node_id
-	aa_equals "Make sure old url /doc4 now matches" [site_node::get_node_id -url "/$doc4_name/"] $root_node_id
-	aa_equals "Make sure old url /doc4/doc3 now matches" [site_node::get_node_id -url "/$doc4_name/$doc3_name/"] $root_node_id
-	aa_equals "Verify url /doc5 for node1" [site_node::get_url -node_id $node1_node_id] "/$doc5_name/"
-	aa_equals "Verify url /doc5/doc3 for node3" [site_node::get_url -node_id $node3_node_id] "/$doc5_name/$doc3_name/"
-	aa_equals "Verify url /doc2 for node2" [site_node::get_url -node_id $node2_node_id] "/$doc2_name/"
+        aa_equals "Check url /doc5" [site_node::get_node_id -url "/$doc5_name"] $node1_node_id
+        aa_equals "Check url /doc5/doc3" [site_node::get_node_id -url "/$doc5_name/$doc3_name"] $node3_node_id
+        aa_equals "Check url /doc2" [site_node::get_node_id -url "/$doc2_name"] $node2_node_id
+        aa_equals "Make sure old url /doc1 now matches" [site_node::get_node_id -url "/$doc1_name/"] $root_node_id
+        aa_equals "Make sure old url /doc1/doc3 now matches" [site_node::get_node_id -url "/$doc1_name/$doc3_name/"] $root_node_id
+        aa_equals "Make sure old url /doc4 now matches" [site_node::get_node_id -url "/$doc4_name/"] $root_node_id
+        aa_equals "Make sure old url /doc4/doc3 now matches" [site_node::get_node_id -url "/$doc4_name/$doc3_name/"] $root_node_id
+        aa_equals "Verify url /doc5 for node1" [site_node::get_url -node_id $node1_node_id] "/$doc5_name/"
+        aa_equals "Verify url /doc5/doc3 for node3" [site_node::get_url -node_id $node3_node_id] "/$doc5_name/$doc3_name/"
+        aa_equals "Verify url /doc2 for node2" [site_node::get_url -node_id $node2_node_id] "/$doc2_name/"
 
-	aa_log "# 4) delete doc3: Test /doc5 /doc2, nonexisting /doc5/doc3"
-	site_node::unmount -node_id $node3_node_id
-	site_node::delete -node_id $node3_node_id
-	aa_equals "Check url /doc5" [site_node::get_node_id -url "/$doc5_name"] $node1_node_id
-	aa_equals "Check url /doc2" [site_node::get_node_id -url "/$doc2_name"] $node2_node_id
-	aa_equals "Make sure old url /doc5/doc3 now matches /doc5" [site_node::get_node_id -url "/$doc5_name/$doc3_name/"] $node1_node_id
-	aa_equals "Verify url /doc5 for node1" [site_node::get_url -node_id $node1_node_id] "/$doc5_name/"
-	aa_equals "Verify url /doc2 for node2" [site_node::get_url -node_id $node2_node_id] "/$doc2_name/"
+        aa_log "# 4) delete doc3: Test /doc5 /doc2, nonexisting /doc5/doc3"
+        site_node::unmount -node_id $node3_node_id
+        site_node::delete -node_id $node3_node_id
+        aa_equals "Check url /doc5" [site_node::get_node_id -url "/$doc5_name"] $node1_node_id
+        aa_equals "Check url /doc2" [site_node::get_node_id -url "/$doc2_name"] $node2_node_id
+        aa_equals "Make sure old url /doc5/doc3 now matches /doc5" [site_node::get_node_id -url "/$doc5_name/$doc3_name/"] $node1_node_id
+        aa_equals "Verify url /doc5 for node1" [site_node::get_url -node_id $node1_node_id] "/$doc5_name/"
+        aa_equals "Verify url /doc2 for node2" [site_node::get_url -node_id $node2_node_id] "/$doc2_name/"
     }
 }
 
