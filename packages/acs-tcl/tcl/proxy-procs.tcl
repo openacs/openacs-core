d9f1d94e799537830205166e2b462097d7885f7e
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2018-11-09 13:18:17 +0000

    add metering for "ns_proxy get" operation

--- packages/acs-tcl/tcl/proxy-procs.tcl
+++ packages/acs-tcl/tcl/proxy-procs.tcl
@@ -1,30 +1,34 @@
-# packages/acs-tcl/tcl/proxy-procs.tcl                                                                                             
 ad_library {
 
     Proxy procs
 
-    @author <yourname> (<your email>)
+    @author Malte Sussdorff, Gustaf Neumann
     @creation-date 2007-09-17
-    @cvs-id $Id$
 }
 
 #
 # First check if ns_proxy is available
 #
 if {![catch {ns_proxy configure ExecPool -maxruns 0}]} {
-    
+
     namespace eval proxy {}
-    
+
     ad_proc -public proxy::exec {
-        {-call}
+        {-call:required}
         {-cd}
     } {
         Execute the statement in a proxy instead of normal exec
 
-        @param call Call which is passed to the "exec" command (required)
-        @param cd  change to the given directory before executing the command
+        @param call Call which is passed to the "exec" command
+        @param cd   Change to the given directory before executing the command
     } {
+        set start_time [clock clicks -milliseconds]
         set handle [ns_proxy get ExecPool]
+        if {[clock clicks -milliseconds] - $start_time > 5} {
+            ns_log warning "ExecPool: getting handle took \
+                [expr {[clock clicks -milliseconds] - $start_time}]ms (potential configuration issue)"
+        }
+
         ad_try {
             if {[info exists cd]} {
                 #
