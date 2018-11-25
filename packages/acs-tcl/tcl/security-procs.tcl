8657bfc59a480cc2bc7c65106a51b9ae68759811
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2018-11-25 17:25:07 +0000

    call sec_login_handler instead of just sec_generate_session_id_cookie, since otherwise, cryptographically valid session cookie could be used without a ad_login_cookie

--- packages/acs-tcl/tcl/security-procs.tcl
+++ packages/acs-tcl/tcl/security-procs.tcl
@@ -222,7 +222,7 @@ ad_proc -private sec_handler {} {
             # # if only they keep requesting pages frequently enough, but the alternative was that
             # # the situation where LoginTimeout = 0 (infinite) and the user unchecks the "Remember me" checkbox
             # # would cause users' sessions to expire as soon as the session needed to be renewed
-            sec_generate_session_id_cookie
+            #sec_generate_session_id_cookie
 
             # apisano 2018-06-08: as discussed in
             # https://openacs.org/forums/message-view?message_id=1691183#msg_1691183,
@@ -232,10 +232,12 @@ ad_proc -private sec_handler {} {
             # GN: when we use "sec_login_handler" instead of the
             # previous code using "sec_generate_session_id_cookie",
             # persistent logins stop to work (people are logged out
-            # from time to time). So, i switched for the time being
-            # the to old code.
+            # from time to time). However, when just
+            # [sec_generate_session_id_cookie] is used then the login
+            # cookie is nevery checked, as long there is a
+            # cryptographically valid session cookie.
             #
-            #sec_login_handler
+            sec_login_handler
         }
 
         #
