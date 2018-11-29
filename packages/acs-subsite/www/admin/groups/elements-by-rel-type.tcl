f46642f516328be3762e547d6f199a171afd3005
Antonio Pisano <antonio@elettrotecnica.it>
2018-11-29 10:01:42 +0000

    Use ad_include_contract for this include page

--- packages/acs-subsite/www/admin/groups/elements-by-rel-type.tcl
+++ packages/acs-subsite/www/admin/groups/elements-by-rel-type.tcl
@@ -1,28 +1,32 @@
-# /packages/subsite/www/admin/groups/elements-by-rel-type.tcl
-#
-# Datasource for elements-by-rel-type.adp 
-# (meant to be included by other templates) 
-#
-# Shows the user a summary of elements (components or members) of the
-# given group, provided that the user has permission to see the
-# element.  The elements are summarized by their relationship to the
-# given group.
-#
-# NOTE:
-# There is no scope check done here to ensure that the element "belongs" to
-# the subsite.  The pages that use this template already check that the
-# given group_id is in scope; therefore, all of its elements must be in
-# scope.  And even if a developer screws up and uses this template without
-# checking that the give group_id belongs to the current subsite, the user
-# would only be able to see elements that they have permission to see.
-# Thus we take the lazy (and efficient) approach of not checking the
-# scope of the elements returned by this datasource.
-#
-# Params: group_id
-#
-# @author oumi@arsdigita.com
-# @creation-date 2001-2-6
-# @cvs-id $Id$
+ad_include_contract {
+
+    /packages/subsite/www/admin/groups/elements-by-rel-type.tcl
+
+    Datasource for elements-by-rel-type.adp
+    (meant to be included by other templates)
+
+    Shows the user a summary of elements (components or members) of the
+    given group, provided that the user has permission to see the
+    element.  The elements are summarized by their relationship to the
+    given group.
+
+    NOTE:
+    There is no scope check done here to ensure that the element "belongs" to
+    the subsite.  The pages that use this template already check that the
+    given group_id is in scope; therefore, all of its elements must be in
+    scope.  And even if a developer screws up and uses this template without
+    checking that the give group_id belongs to the current subsite, the user
+    would only be able to see elements that they have permission to see.
+    Thus we take the lazy (and efficient) approach of not checking the
+    scope of the elements returned by this datasource.
+
+    @author oumi@arsdigita.com
+    @creation-date 2001-2-6
+    @cvs-id $Id$
+
+} {
+    group_id:integer
+}
 
 set user_id [ad_conn user_id]
 set admin_p [permission::permission_p -object_id $group_id -privilege "admin"]
