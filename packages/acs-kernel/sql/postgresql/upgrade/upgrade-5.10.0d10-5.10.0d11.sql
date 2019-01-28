3a9520b7ebcc0a7c03398148f37e861cdf6a611e
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2019-01-28 21:44:12 +0000

    schema queries: allow mixed case for column and table names (similar to function_args
    bump versiom number to 5.10.0d11

--- packages/acs-kernel/sql/postgresql/upgrade/upgrade-5.10.0d10-5.10.0d11.sql
+++ packages/acs-kernel/sql/postgresql/upgrade/upgrade-5.10.0d10-5.10.0d11.sql
@@ -1,94 +1,14 @@
 --
--- /packages/acs-kernel/sql/utilities-create.sql
+-- Make sure, that table_name and column_name work
+-- case-insensitive (similar to function_args)
 --
--- Useful PL/SQL utility routines.
---
--- @author Jon Salz (jsalz@mit.edu)
--- @creation-date 12 Aug 2000
--- @cvs-id $Id$
---
-
-
-
--- added
-select define_function_args('util__multiple_nextval','v_sequence_name,v_count');
-
---
--- procedure util__multiple_nextval/2
---
-CREATE OR REPLACE FUNCTION util__multiple_nextval(
-   v_sequence_name varchar,
-   v_count integer
-) RETURNS varchar AS $$
-DECLARE
-  a_sequence_values      text default '';
-  v_rec                  record;
-BEGIN
-    for counter in 1..v_count loop
-        for v_rec in EXECUTE 'select ' || quote_ident(v_sequence_name) || '.nextval as a_seq_val'
-        LOOP
-           a_sequence_values := a_sequence_values || '','' || v_rec.a_seq_val;
-          exit;
-        end loop;
-    end loop;
-
-    return substr(a_sequence_values, 2);
-
-END;
-$$ LANGUAGE plpgsql;
-
-
-
--- added
-select define_function_args('util__logical_negation','true_or_false');
-
---
--- procedure util__logical_negation/1
---
-CREATE OR REPLACE FUNCTION util__logical_negation(
-   true_or_false boolean
-) RETURNS boolean AS $$
-DECLARE
-BEGIN
-      IF true_or_false is null THEN
-        return null;
-      ELSE IF true_or_false = 'f' THEN
-        return 't';
-      ELSE
-        return 'f';
-      END IF; END IF;
-END;
-$$ LANGUAGE plpgsql immutable strict;
-
-
--- added
-select define_function_args('util__table_exists','name');
-
---
--- procedure util__table_exists/1
---
-CREATE OR REPLACE FUNCTION util__table_exists(
-   name text
-) RETURNS boolean AS $$
-DECLARE
-BEGIN
-      return exists (
-       select 1 from pg_class
-          where relname = name
-            and pg_table_is_visible(oid));
-END;
-$$ LANGUAGE plpgsql;
-
-
--- added
-select define_function_args('util__table_column_exists','t_name,c_name');
 
 --
 -- procedure util__table_column_exists/1
 --
 CREATE OR REPLACE FUNCTION util__table_column_exists(
-   t_name  text,
    c_name text
+   t_name  text,
 ) RETURNS boolean AS $$
 DECLARE
 BEGIN
@@ -99,43 +19,6 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-
--- added
-select define_function_args('util__view_exists','name');
-
---
--- procedure util__view_exists/1
---
-CREATE OR REPLACE FUNCTION util__view_exists(
-   name text
-) RETURNS boolean AS $$
-DECLARE
-BEGIN
-      return exists (
-       select 1 from pg_views where viewname = name);
-END;
-$$ LANGUAGE plpgsql;
-
-
--- added
-select define_function_args('util__index_exists','name');
-
---
--- procedure util__index_exists/1
---
-CREATE OR REPLACE FUNCTION util__index_exists(
-   name text
-) RETURNS boolean AS $$
-DECLARE
-BEGIN
-      return exists (
-       select 1 from pg_indexes where indexname = name);
-END;
-$$ LANGUAGE plpgsql;
-
-
--- added
-select define_function_args('util__foreign_key_exists','table,column,reftable,refcolumn');
 
 --
 -- procedure util__foreign_key_exists/4
@@ -171,9 +54,6 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
--- added
-select define_function_args('util__unique_exists','table,column,single_p;true');
-
 --
 -- procedure util__unique_exists/3
 --
@@ -205,9 +85,6 @@ BEGIN
 	      and constraint_schema  = kcu.constraint_schema) = 1));
 END;
 $$ LANGUAGE plpgsql;
-
--- added
-select define_function_args('util__primary_key_exists','table,column,single_p;true');
 
 --
 -- procedure util__unique_exists/3
@@ -242,9 +119,6 @@ END;
 $$ LANGUAGE plpgsql;
 
 
--- added
-select define_function_args('util__not_null_exists','table,column');
-
 --
 -- procedure util__not_null_exists/2
 --
@@ -263,8 +137,6 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
--- added
-select define_function_args('util__get_default','table,column');
 
 --
 -- procedure util__get_default/2
@@ -283,17 +155,3 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
--- added
-select define_function_args('util__get_primary_keys','table');
-
---
--- procedure util__get_primary_keys/1
---
-CREATE OR REPLACE FUNCTION util__get_primary_keys(text) RETURNS SETOF pg_attribute.attname%TYPE AS $$
-  SELECT a.attname
-    FROM pg_index i
-    JOIN pg_attribute a ON a.attrelid = i.indrelid
-                       AND a.attnum = ANY(i.indkey)
-  WHERE i.indrelid = $1::regclass
-    AND i.indisprimary;
-$$ LANGUAGE sql;
