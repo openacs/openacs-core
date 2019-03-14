ea6f95098adc142d1958645ed69de9583df531d5
Antonio Pisano <antonio@elettrotecnica.it>
2019-03-14 16:25:56 +0000

    Add ad_page_contract (fixes automated tests)

--- packages/acs-subsite/www/shared/index.tcl
+++ packages/acs-subsite/www/shared/index.tcl
@@ -1,5 +1,7 @@
-# $Id$
-# user try to play with the URL and get the directory structure instead of a file
+ad_page_contract {
+    # $Id$
+    # user try to play with the URL and get the directory structure instead of a file
+}
 
 ad_returnredirect [subsite::get_element -element url]
 ad_script_abort
