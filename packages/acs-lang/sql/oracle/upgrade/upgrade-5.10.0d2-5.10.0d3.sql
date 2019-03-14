4440e5b3324ed2c5e7fb539e34360b5a1b36ef26
Antonio Pisano <antonio@elettrotecnica.it>
2019-03-14 10:41:42 +0000

    Trim padding from column to be sure

--- packages/acs-lang/sql/oracle/upgrade/upgrade-5.10.0d2-5.10.0d3.sql
+++ packages/acs-lang/sql/oracle/upgrade/upgrade-5.10.0d2-5.10.0d3.sql
@@ -4,6 +4,9 @@ drop view enabled_locales;
 
 alter table ad_locales modify column language varchar2(3);
 
+-- normalize eventual padding from previous life as a char(n)
+update ad_locales set language = trim(language);
+
 create or replace view enabled_locales as
 select * from ad_locales
 where enabled_p = 't';
