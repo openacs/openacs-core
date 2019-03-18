f0d9fd90a887635a0ed223d797acdb3d156392dd
Antonio Pisano <antonio@elettrotecnica.it>
2019-03-18 15:52:16 +0000

    Reduce divergency between oracle and postgres codebase

--- packages/acs-subsite/lib/applications.tcl
+++ packages/acs-subsite/lib/applications.tcl
@@ -29,7 +29,21 @@ set subsite_node_id [subsite::get_element -element node_id]
 
 set user_id [ad_conn user_id]
 
-db_multirow applications select_applications {}
+db_multirow applications select_applications {
+    select p.package_id,
+           p.instance_name,
+           n.node_id,
+           n.name
+    from   site_nodes n,
+           apm_packages p,
+           apm_package_types t
+    where  n.parent_id = :subsite_node_id
+    and    p.package_id = n.object_id
+    and    t.package_key = p.package_key
+    and    t.package_type = 'apm_application'
+    and    acs_permission.permission_p(p.package_id, :user_id, 'read')
+    order  by upper(instance_name)
+}
 
 
 # Local variables:
