a3882e7fefad9bcb78e3bb412ec0c088b99e85b3
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2019-04-07 15:49:42 +0000

    automatically re-source the testfile when re-running a single test

--- packages/acs-automated-testing/www/admin/rerun.tcl
+++ packages/acs-automated-testing/www/admin/rerun.tcl
@@ -13,27 +13,39 @@ ad_page_contract {
 
 if {$testcase_id eq ""} {
     if {$quiet} {
-	aa_runseries -stress $stress -security_risk $security_risk -quiet $package_key $category
+        aa_runseries -stress $stress -security_risk $security_risk -quiet $package_key $category
     } else {
-	aa_runseries -stress $stress -security_risk $security_risk $package_key $category
+        aa_runseries -stress $stress -security_risk $security_risk $package_key $category
     }
-    
-    ad_returnredirect [export_vars -base index {
-	{by_package_key $package_key}
-	{by_category $category}
-	view_by quiet stress security_risk}]
-    ad_script_abort
+
+    set return_url [export_vars -base index {
+        {by_package_key $package_key}
+        {by_category $category}
+        view_by quiet stress security_risk}]
 } else {
-    if {$quiet} {
-	aa_runseries -quiet -testcase_id $testcase_id "" ""
-    } else {
-	aa_runseries -testcase_id $testcase_id "" ""
+    #
+    # Rerun of a single test case always resources the definition.
+    #
+    foreach c [nsv_get aa_test cases] {
+        if {[lindex $c 0] eq $testcase_id} {
+            set absolute_file_path [lindex $c 2]
+            ns_log notice "Sourcing test definition file $absolute_file_path"
+            apm_source $absolute_file_path
+            break
+        }
     }
-    ad_returnredirect "testcase?testcase_id=$testcase_id&package_key=$package_key&quiet=$quiet"
-    ad_script_abort
+
+    if {$quiet} {
+        aa_runseries -quiet -testcase_id $testcase_id "" ""
+    } else {
+        aa_runseries -testcase_id $testcase_id "" ""
+    }
+    set return_url [export_vars -base testcase {
+        testcase_id package_key quiet
+    }]
 }
 
-
+ad_returnredirect $return_url
 
 # Local variables:
 #    mode: tcl
