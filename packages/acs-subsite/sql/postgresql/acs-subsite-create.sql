be565eff116b926c08654284b737c9a50f8a1af5
Antonio Pisano <antonio@elettrotecnica.it>
2019-07-26 11:12:11 +0000

    Cleanup commented code

--- packages/acs-subsite/sql/postgresql/acs-subsite-create.sql
+++ packages/acs-subsite/sql/postgresql/acs-subsite-create.sql
@@ -20,22 +20,6 @@
 -- weren't created in the original ACS 4.2 Oracle sources.
 -- \i user-profiles-create.sql
 
--- This view lets us avoid using acs_object.name to get party_names.
---
--- create or replace view party_names
--- as
--- select p.party_id,
---        decode(groups.group_id,
---               null, decode(persons.person_id,
---                            null, p.email,
---                            persons.first_names || ' ' || persons.last_name),
---               groups.group_name) as party_name
--- from parties p,
---      groups,
---      persons
--- where p.party_id = groups.group_id(+)
---   and p.party_id = persons.person_id(+);
-
 create view party_names
 as
 select p.party_id,
