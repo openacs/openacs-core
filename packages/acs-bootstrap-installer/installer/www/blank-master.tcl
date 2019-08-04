312ee0010691640235712bdaad3f90a528663f4d
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2019-08-04 17:57:54 +0000

    reduce warnings from Chrome audits

--- packages/acs-bootstrap-installer/installer/www/blank-master.tcl
+++ packages/acs-bootstrap-installer/installer/www/blank-master.tcl
@@ -62,7 +62,7 @@ ad_page_contract {
 }
 
 if {![info exists doc(type)]} { 
-    set doc(type) {<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">}
+    set doc(type) {<!DOCTYPE HTML>}
 }
 
 #
