e5d4ff8a6aad19e9445972cbe0c0a37647a4add2
Antonio Pisano <antonio@elettrotecnica.it>
2019-09-03 11:45:15 +0000

    Reduce divergency between oracle and postgres

--- packages/acs-tcl/tcl/object-procs.tcl
+++ packages/acs-tcl/tcl/object-procs.tcl
@@ -52,7 +52,9 @@ ad_proc -public acs_object_name { object_id } {
     Returns the name of an object.
 
 } {
-    return [db_string object_name_get {}]
+    return [db_string object_name_get {
+        select acs_object.name(:object_id) from dual
+    }]
 }
 
 ad_proc -public acs_object_type { object_id } {
@@ -80,7 +82,23 @@ ad_proc -public acs_object::get {
     @param array An array in the caller's namespace into which the info should be delivered (upvared)
 } {
     upvar 1 $array row
-    db_1row select_object {} -column_array row
+    db_1row select_object {
+        select o.object_id,
+               o.title,
+               o.package_id,
+               o.object_type,
+               o.context_id,
+               o.security_inherit_p,
+               o.creation_user,
+               to_char(o.creation_date, 'YYYY-MM-DD HH24:MI:SS') as creation_date_ansi,
+               o.creation_ip,
+               to_char(o.last_modified, 'YYYY-MM-DD HH24:MI:SS') as last_modified_ansi,
+               o.modifying_user,
+               o.modifying_ip,
+               acs_object.name(o.object_id) as object_name
+        from   acs_objects o
+        where  o.object_id = :object_id
+    } -column_array row
 }
 
 ad_proc -public acs_object::package_id {
