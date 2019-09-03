ce3cbd33d69eee075fd217eff090b830c1c15707
Antonio Pisano <antonio@elettrotecnica.it>
2019-09-03 11:15:36 +0000

    Reduce divergency between oracle and postgres

--- packages/acs-subsite/www/admin/rel-segments/elements-display.tcl
+++ packages/acs-subsite/www/admin/rel-segments/elements-display.tcl
@@ -6,7 +6,15 @@ set write_p [permission::permission_p -object_id $segment_id -privilege "write"]
 set package_url [ad_conn package_url]
 set user_id [ad_conn user_id]
 
-db_multirow elements elements_select {}
+db_multirow elements elements_select {
+    select acs_object.name(map.party_id) as name, map.rel_id,
+           case when map.container_id = :group_id then 1 else 0 end as direct_p,
+           acs_object.name(map.container_id) as container_name
+      from rel_segment_party_map map
+     where acs_permission.permission_p(map.party_id, :user_id, 'read')
+       and map.segment_id = :segment_id
+     order by name
+}
 
 
 
