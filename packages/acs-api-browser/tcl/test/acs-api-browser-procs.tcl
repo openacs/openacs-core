8871e7be5a77a158362020a5be5b0f4aaf9dce4e
Antonio Pisano <antonio@elettrotecnica.it>
2019-09-05 15:08:19 +0000

    New test for acs_api_browser_api_add_to_proc_doc

--- packages/acs-api-browser/tcl/test/acs-api-browser-procs.tcl
+++ packages/acs-api-browser/tcl/test/acs-api-browser-procs.tcl
@@ -22,7 +22,50 @@ aa_register_case \
             aa_true "api documentation proc can document itself" \
                 [string match "*packages/acs-api-browser/tcl/acs-api-documentation-procs.tcl*" $result]
         }
-}
+    }
+
+aa_register_case \
+    -cats { api smoke } \
+    -procs {
+        api_add_to_proc_doc
+    } \
+    acs_api_browser_api_add_to_proc_doc {
+        Check api_add_to_proc_doc
+    } {
+        set proc_name [ad_generate_random_string]
+        set property [ad_generate_random_string]
+        set value [ad_generate_random_string]
+        set value2 ${value}2
+        api_add_to_proc_doc \
+            -proc_name $proc_name \
+            -property $property \
+            -value $value
+
+        aa_true "nsv was created" [nsv_exists api_proc_doc $proc_name]
+
+        aa_true "nsv contains the property" [dict exists [nsv_get api_proc_doc $proc_name] $property]
+
+        aa_true "Property has 1 value" \
+            {[llength [dict get [nsv_get api_proc_doc $proc_name] $property]] == 1}
+        aa_log "Adding the same value again"
+        api_add_to_proc_doc \
+            -proc_name $proc_name \
+            -property $property \
+            -value $value
+        aa_true "Property still has 1 value" \
+            {[llength [dict get [nsv_get api_proc_doc $proc_name] $property]] == 1}
+
+        aa_log "Adding a different value"
+        api_add_to_proc_doc \
+            -proc_name $proc_name \
+            -property $property \
+            -value $value2
+        aa_true "Property now has 2 values" \
+            {[llength [dict get [nsv_get api_proc_doc $proc_name] $property]] == 2}
+
+        nsv_unset -nocomplain -- $proc_name
+    }
+
 # Local variables:
 #    mode: tcl
 #    tcl-indent-level: 4
