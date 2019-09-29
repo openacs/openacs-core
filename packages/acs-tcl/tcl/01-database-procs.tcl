3b3482a24d4964bbea430957f950787ecb228502
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2019-09-29 14:55:43 +0000

    prefer curly braces for list of options

--- packages/acs-tcl/tcl/01-database-procs.tcl
+++ packages/acs-tcl/tcl/01-database-procs.tcl
@@ -2951,7 +2951,7 @@ ad_proc -public db_load_sql_data {
             set copy_command [subst -nobackslashes [read $fd]]
             close $fd
             set copy_file [ns_mktemp [ad_tmpdir]/psql-copyfile-XXXXXX]
-            set fd [open $copy_file "CREAT EXCL WRONLY" 0600]
+            set fd [open $copy_file {CREAT EXCL WRONLY} 0600]
             puts $fd $copy_command
             close $fd
 
