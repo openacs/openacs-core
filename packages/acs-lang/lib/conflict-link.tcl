0eac15111098bc93ac7ddfb5424a3c8013781764
Antonio Pisano <antonio@elettrotecnica.it>
2019-12-19 15:19:58 +0000

    Use ad_include_contract

--- packages/acs-lang/lib/conflict-link.tcl
+++ packages/acs-lang/lib/conflict-link.tcl
@@ -1,25 +1,15 @@
-# Optional parameters:
-#
-# package_key
-# locale
-
-# Default params to empty string
-# and build the link export list
-set link_export_list [list]
-foreach param {package_key locale} {
-    if { ![info exists $param] } {
-        set $param ""
-    } else {
-        # param provided
-        lappend link_export_list $param
-    }
+ad_include_contract {
+    Display the link to conflicting message keys
+} {
+    {package_key:token ""}
+    {locale:token ""}
 }
 
 set conflict_count [lang::message::conflict_count \
                         -package_key $package_key \
                         -locale $locale]
 
-set message_conflicts_url [export_vars -base message-conflicts $link_export_list]
+set message_conflicts_url [export_vars -base message-conflicts -no_empty {package_key locale}]
 
 # Local variables:
 #    mode: tcl
