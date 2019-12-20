fc7ae550fc64669ac2570b4a4e32c1e7189c801f
Antonio Pisano <antonio@elettrotecnica.it>
2019-12-20 16:52:04 +0000

    Add ad_page_contract

--- packages/acs-bootstrap-installer/installer/www/SYSTEM/csp-collector.tcl
+++ packages/acs-bootstrap-installer/installer/www/SYSTEM/csp-collector.tcl
@@ -1,2 +1,7 @@
+ad_page_contract {
+    Page responsible for collecting CSP complaints for the browser and
+    putting them in the log file
+}
+
 ns_log notice "CSP violation: [ns_conn content] user-agent: [ns_set iget [ns_conn headers] user-agent] user_id [ad_conn user_id] peer [ad_conn peeraddr]"
 ns_return 200 text/plain ok
