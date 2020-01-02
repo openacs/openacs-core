5eba80b685d44bd12a288a681c2b22a0c31a0866
Antonio Pisano <antonio@elettrotecnica.it>
2020-01-02 18:13:02 +0000

    Reduce divergency between oracle and postgres codebase

--- packages/acs-service-contract/www/index.tcl
+++ packages/acs-service-contract/www/index.tcl
@@ -29,7 +29,23 @@ template::list::create \
 
 
 
-db_multirow valid_installed_bindings valid_installed_binding {}
+db_multirow valid_installed_bindings valid_installed_binding {
+        select q.*
+        from (select
+                  b.contract_id,
+                  b.impl_id,
+                  (select contract_name from acs_sc_contracts
+                    where contract_id = b.contract_id) as contract_name,
+                  impl.impl_name,
+                  impl.impl_owner_name,
+                  impl.impl_pretty_name
+              from
+                  acs_sc_bindings b,
+                  acs_sc_impls impl
+              where
+                  impl.impl_id = b.impl_id) q
+        order  by upper(contract_name), contract_name, upper(impl_name), impl_name
+}
 
 template::list::create \
     -name valid_installed_bindings \
