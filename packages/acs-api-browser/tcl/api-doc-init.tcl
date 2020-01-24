4b1c880ecece84222e2d41eef9586a597c4419fb
Hector Romojaro <hector.romojaro@gmail.com>
2020-01-24 16:01:07 +0000

    Whitespace changes + editor hints

--- packages/acs-api-browser/tcl/api-doc-init.tcl
+++ packages/acs-api-browser/tcl/api-doc-init.tcl
@@ -15,10 +15,14 @@
 #         ns_param IncludeCallingInfo true
 #
 if {[parameter::get \
-	 -package_id [apm_package_id_from_key acs-api-browser] \
-	 -parameter IncludeCallingInfo \
-	 -default false]} {
+        -package_id [apm_package_id_from_key acs-api-browser] \
+        -parameter IncludeCallingInfo \
+        -default false]} {
     ad_schedule_proc -thread t -once t 1 ::api_add_calling_info_to_procdoc
 }
 
-
+# Local variables:
+#    mode: tcl
+#    tcl-indent-level: 4
+#    indent-tabs-mode: nil
+# End:
