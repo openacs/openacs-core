36f6f6d4b48b062ad695f202a6263181b2292605
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2020-03-31 19:27:24 +0000

    template::hide: new API function to hide certain CSS classes via javascript

--- packages/acs-templating/tcl/head-procs.tcl
+++ packages/acs-templating/tcl/head-procs.tcl
@@ -1083,6 +1083,22 @@ ad_proc template::add_refresh_on_history_handler {} {
     }
 }
 
+ad_proc template::hide {
+    -class:required
+} {
+    Hide certain CSS classes in the dom-tree by setting for elements
+    of the specified class the property "display" to "none".
+
+    @param class CSS class to be hidden
+} {
+    template::add_script -section body -script [subst -nocommands {
+        window.addEventListener('DOMContentLoaded', (event) => {
+            var els = document.getElementsByClassName("$class");
+            for(var i = 0; i < els.length; i++) { els[i].style.display = "none"; }
+        });
+    }]
+}
+
 ad_proc template::add_event_listener {
     {-event click}
     {-CSSclass "acs-listen"}
