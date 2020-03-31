2f6f71e87bcbfaedfc42dbe496ed8bff67cde670
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2020-03-31 19:26:06 +0000

    improve readability

--- packages/acs-templating/tcl/test/head-test-procs.tcl
+++ packages/acs-templating/tcl/test/head-test-procs.tcl
@@ -1,7 +1,7 @@
 ad_library {
-    
+
     Tests for adp parsing
-    
+
     @author Gustaf Neumann
     @creation-date 2018-03-09
 }
@@ -24,8 +24,8 @@ aa_register_case \
     template::head::includes -container a -parts {b c}
     template::head::includes -container e -parts {f g}
 
-    aa_equals "is a already included" [template::head::included_p a] 0
-    aa_equals "is b already included" [template::head::included_p b] 1
+    aa_equals "is 'a' already included" [template::head::included_p a] 0
+    aa_equals "is 'b' already included" [template::head::included_p b] 1
 
     #
     # Flush one container via flush_link; the contained elements
@@ -33,11 +33,11 @@ aa_register_case \
     #
     template::head::flush_link -href a -rel stylesheet
 
-    aa_equals "is a already included" [template::head::included_p a] 0
-    aa_equals "is b already included" [template::head::included_p b] 0
+    aa_equals "is 'a' already included" [template::head::included_p a] 0
+    aa_equals "is 'b' already included" [template::head::included_p b] 0
 
-    aa_equals "is e already included" [template::head::included_p e] 0
-    aa_equals "is f already included" [template::head::included_p f] 1
+    aa_equals "is 'e' already included" [template::head::included_p e] 0
+    aa_equals "is 'f' already included" [template::head::included_p f] 1
 
     #
     # Flush the second container via flush_script; the contained
@@ -45,8 +45,8 @@ aa_register_case \
     #
     template::head::flush_script -src e
 
-    aa_equals "is e already included" [template::head::included_p e] 0
-    aa_equals "is f already included" [template::head::included_p f] 0
+    aa_equals "is 'e' already included" [template::head::included_p e] 0
+    aa_equals "is 'f' already included" [template::head::included_p f] 0
 }
 
 
