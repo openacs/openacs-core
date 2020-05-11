dccadcf71f95ba526701a020a63fcbfeb35504bc
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2020-05-11 19:09:53 +0000

    add as well support for schema name to util__table_column_exists and util__view_exists

--- packages/acs-kernel/sql/postgresql/utilities-create.sql
+++ packages/acs-kernel/sql/postgresql/utilities-create.sql
@@ -109,11 +109,29 @@ CREATE OR REPLACE FUNCTION util__table_column_exists(
    p_column text
 ) RETURNS boolean AS $$
 DECLARE
+ v_schema    varchar;
+ v_tablename varchar;
 BEGIN
-      return exists (
-       select 1 from information_schema.columns c
-         where c.table_name  = lower(p_table)
-           and c.column_name = lower(p_column));
+    IF (position('.' in p_table) = 0) THEN
+        --
+	-- table without a schema name
+	--
+      	return exists (
+            select 1 from information_schema.columns c
+            where table_name  = lower(p_table)
+            and column_name = lower(p_column));
+    ELSE
+        --
+	-- table with schema name
+	--    
+        SELECT split_part(p_table, '.', 1) into v_schema;
+        SELECT split_part(p_table, '.', 2) into v_tablename;
+        return exists (
+            select 1 from information_schema.columns
+            where table_name  = lower(v_tablename)
+            and column_name = lower(p_column)
+            and table_schema = v_schema);
+    END IF;
 END;
 $$ LANGUAGE plpgsql;
 
@@ -128,9 +146,26 @@ CREATE OR REPLACE FUNCTION util__view_exists(
    name text
 ) RETURNS boolean AS $$
 DECLARE
+ v_schema    varchar;
+ v_tablename varchar;
 BEGIN
-      return exists (
-       select 1 from pg_views where viewname = name);
+    IF (position('.' in name) = 0) THEN
+        --
+	-- view without a schema name
+	--
+        return exists (
+           select 1 from pg_views where viewname = name);
+    ELSE
+        --
+	-- table with schema name
+	--    
+        SELECT split_part(name, '.', 1) into v_schema;
+        SELECT split_part(name, '.', 2) into v_tablename;
+        return exists (
+            select 1 from information_schema.views
+            where table_name  = lower(v_tablename)
+            and table_schema = v_schema);
+    END IF;
 END;
 $$ LANGUAGE plpgsql;
 
