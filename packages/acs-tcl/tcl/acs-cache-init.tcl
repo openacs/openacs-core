f126966871e5d9504034a052f681469afca14e19
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2020-06-06 09:43:19 +0000

    fix typo

--- packages/acs-tcl/tcl/acs-cache-init.tcl
+++ packages/acs-tcl/tcl/acs-cache-init.tcl
@@ -1,6 +1,6 @@
 #
 # The acs::misc_cache is a potential successor of the util_memoize_cache, 
-# but in a partioned fashion to make it scalable. It should only be
+# but in a partitioned fashion to make it scalable. It should only be
 # used for situation, where not wild-card flushes are required.
 #
 set cacheType [expr {[::acs::icanuse "ns_hash"] ? "HashKeyPartitionedCache" : "Cache"}]
