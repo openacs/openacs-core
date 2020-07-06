9d9c50d0f86c3e31dbe02574485ac197fd800364
Antonio Pisano <antonio@elettrotecnica.it>
2020-07-06 13:55:54 +0000

    Document current preferred idiom to quote database lists, without official deprecation, for the time being

--- packages/acs-templating/tcl/util-procs.tcl
+++ packages/acs-templating/tcl/util-procs.tcl
@@ -608,6 +608,12 @@ ad_proc -public template::util::number_list { last_number {start_at 0} } {
 ad_proc -public template::util::tcl_to_sql_list { lst } {
     Convert a Tcl list to a SQL list, for use with the "in" statement.
     Uses double single quotes (similar to ns_dbquotevalue) to escape single quotes
+
+    Note: this proc could be deprecated in the not so far future, as
+    Naviserver now provides a native api for this and a
+    tcl-implemented fallback exists in current OpenACS code.
+
+    @see ns_dbquotelist
 } {
 
     if { [llength $lst] > 0 } {
