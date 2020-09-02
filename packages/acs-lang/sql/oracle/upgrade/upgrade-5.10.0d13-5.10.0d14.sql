a404610f5fb56e1c7c764d690dbf3e4b0c188c50
Antonio Pisano <antonio@elettrotecnica.it>
2020-09-02 11:25:49 +0000

    Do not insert object_ids that do not exist in acs_object (leftover group message keys)

--- packages/acs-lang/sql/oracle/upgrade/upgrade-5.10.0d13-5.10.0d14.sql
+++ packages/acs-lang/sql/oracle/upgrade/upgrade-5.10.0d13-5.10.0d14.sql
@@ -5,4 +5,5 @@
 update lang_message_keys set
   object_id = cast(split_part(message_key, '_', 3) as integer)
  where package_key = 'acs-translations'
-   and message_key like 'group_title_%';
+   and message_key like 'group_title_%'
+   and exists (select 1 from acs_objects where object_id = cast(split_part(message_key, '_', 3) as integer));
