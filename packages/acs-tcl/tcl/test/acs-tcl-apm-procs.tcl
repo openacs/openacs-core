d084c0281b45048456791ef849dcd57d59ad4327
Hector Romojaro <hector.romojaro@gmail.com>
2020-10-20 12:29:22 +0000

    Rework of the 'test_apm_parameter__register' and 'test_apm_package_instance__new' test cases. Ignored disabled packages in both tests. Check all enabled packages in 'test_apm_package_instance__new', instead of picking just one randomly, so all issues instantiating packages are found at once. Indentation changes.

--- packages/acs-tcl/tcl/test/acs-tcl-apm-procs.tcl
+++ packages/acs-tcl/tcl/test/acs-tcl-apm-procs.tcl
@@ -20,13 +20,15 @@ aa_register_case \
 } {
     aa_run_with_teardown -rollback -test_code {
 
-        set package_list [db_list get_packages "select package_key from apm_package_types"]
-        aa_log "List of packages: \{$package_list\}"
+        set package_list [db_list get_packages {
+            select package_key
+              from apm_enabled_package_versions
+        }]
+        aa_log "List of packages: [list $package_list]"
         set list_index [util::random_range [expr {[llength $package_list] - 1}]]
         set package_key [lrange $package_list $list_index $list_index]
-
-        set parameter_name [ad_generate_random_string]
-        set description [ad_generate_random_string]
+        set parameter_name  [ad_generate_random_string]
+        set description     [ad_generate_random_string]
 
         set values { {number} {string} }
         set index [util::random_range 1]
@@ -42,11 +44,21 @@ aa_register_case \
             set default_value [ad_generate_random_string]
         }
 
-        aa_log "Parameter to be added: name $parameter_name\n descr $description\n datatype $datatype\n default_value $default_value"
+        aa_log "$package_key parameter to be added: name $parameter_name
+        descr $description
+        datatype $datatype
+        default_value $default_value"
 
-        set parameter_id [apm_parameter_register $parameter_name $description $package_key $default_value $datatype]
+        set parameter_id [apm_parameter_register \
+            $parameter_name \
+            $description \
+            $package_key \
+            $default_value \
+            $datatype]
 
-        aa_true "Parameter register succeeded" {$parameter_id ne ""}
+        aa_true "$package_key parameter register succeeded" \
+            {$parameter_id ne ""}
+
     }
 }
 
@@ -61,20 +73,26 @@ aa_register_case \
 
     aa_run_with_teardown -rollback -test_code {
 
-        set package_list [db_list get_packages "select package_key from apm_package_types"]
-        aa_log "List of packages: \{$package_list\}"
+        set package_list [db_list get_packages {
+            select package_key
+              from apm_enabled_package_versions
+        }]
+        aa_log "List of packages: [list $package_list]"
 
-        set list_index [util::random_range [expr {[llength $package_list] - 1}]]
-        set package_key [lrange $package_list $list_index $list_index]
-        set instance_name "$package_key-[ad_generate_random_string]"
+        foreach package_key $package_list {
+            set package_id ""
+            set instance_name "$package_key-[ad_generate_random_string]"
 
-        aa_log "Package to be instantiated: $package_key"
-        aa_log "Instance name to be added: $instance_name"
-        set error_occurred [catch {
-            set package_id [apm_package_instance_new -package_key $package_key -instance_name $instance_name ]
-        } err_men]
-        aa_log "Error Message $error_occurred: $err_men "
-        aa_true "Setting the new instance succeeded" {[info exists package_id] && $package_id ne ""}
+            aa_log "Package to be instantiated: $package_key"
+            aa_log "Instance name to be added: $instance_name"
+            set error_occurred [catch {
+                set package_id [apm_package_instance_new \
+                    -package_key $package_key \
+                    -instance_name $instance_name ]
+            } err_men]
+            aa_log "Error Message $error_occurred: $err_men "
+            aa_true "Setting the new instance succeeded" {$package_id ne ""}
+        }
     }
 }
 
