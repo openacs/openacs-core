0a8c9080bf260388efa6c20239956b7b4a6b17ae
Antonio Pisano <antonio@elettrotecnica.it>
2020-11-06 15:52:50 +0000

    Add missing semicolon

--- packages/notifications/sql/postgresql/upgrade/upgrade-5.10.0d6-5.10.0d7.sql
+++ packages/notifications/sql/postgresql/upgrade/upgrade-5.10.0d6-5.10.0d7.sql
@@ -5,4 +5,4 @@ select acs_object__delete(o.object_id)
 from acs_objects o
      left join notification_requests r on r.request_id = o.object_id
 where object_type = 'notification_request'
-  and r.request_id is null
+  and r.request_id is null;
