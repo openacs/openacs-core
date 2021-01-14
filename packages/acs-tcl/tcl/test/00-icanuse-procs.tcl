a104aca71761fb766a2cb8e2ae8a82c4e5cb22d2
Antonio Pisano <antonio@elettrotecnica.it>
2021-01-14 14:30:13 +0000

    Extend test to expose that we need a better idiom to detect also NaviServer commands

--- packages/acs-tcl/tcl/test/00-icanuse-procs.tcl
+++ packages/acs-tcl/tcl/test/00-icanuse-procs.tcl
@@ -14,6 +14,7 @@ aa_register_case \
         Check whether we can detect if a command has a subcommand.
 
     } {
+        aa_section "Test a plain Tcl command"
         set flags {
             bytelength
             cat
@@ -46,4 +47,17 @@ aa_register_case \
 
         set flag [ad_generate_random_string]
         aa_false "string has no subcommand called '$flag'" [acs::cmd_has_subcommand string $flag]
+
+        aa_section "Test a NaviServer command"
+        set flags {
+            get
+            set
+        }
+        foreach flag $flags {
+            aa_true "nsv_array subcommand '$flag' is recognized" \
+                [acs::cmd_has_subcommand nsv_array $flag]
+        }
+        set flag [ad_generate_random_string]
+        aa_false "nsv_array has no subcommand called '$flag'" \
+            [acs::cmd_has_subcommand nsv_array $flag]
     }
