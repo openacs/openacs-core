26c468070b0af4a17b00223c69e1e30f3aac9111
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2021-02-28 16:40:27 +0000

    simplify and modernize Tcl code

--- packages/acs-automated-testing/tcl/filter-procs.tcl
+++ packages/acs-automated-testing/tcl/filter-procs.tcl
@@ -2,25 +2,21 @@ ad_page_contract_filter aa_test_view_by { name value } {
   Checks whether a view_by value has a value of "testcase", "package" or
   "category"
 } {
-  if {$value ne "testcase" &&
-      $value ne "package"} {
-    ad_complain "Invalid view_by name"
-    return 0
-  }
-  return 1
+    if {$value ni {testcase package}} {
+        ad_complain "Invalid view_by name"
+        return 0
+    }
+    return 1
 }
 
 ad_page_contract_filter aa_test_category { name value } {
   Checks whether a category value has is valid.
 } {
-  set found 0
-  foreach category [nsv_get aa_test categories] {
-    if {$value == $category} {
-      return 1
+    if {$value ni [nsv_get aa_test categories]} {
+        ad_complain "$value is not a valid acs-automated-testing testcase category"
+        return 0
     }
-  }
-  ad_complain "$value is not a valid acs-automated-testing testcase category"
-  return 0
+    return 1
 }
 
 # Local variables:
