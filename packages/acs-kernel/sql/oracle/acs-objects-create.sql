1bf772fdd6e2423f64c6fb974c944ae30ec8b36e
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2021-03-24 10:16:17 +0000

    added oracle upgrade scripts for new check constraints

--- packages/acs-kernel/sql/oracle/acs-objects-create.sql
+++ packages/acs-kernel/sql/oracle/acs-objects-create.sql
@@ -158,6 +158,14 @@ create table acs_objects (
 	unique (context_id, object_id) disable
 );
 
+--
+-- Avoid potential loops on context_ids. A context_id must be
+-- different from the object_id. If no context_id should be checked, its
+-- value must be NULL. Note that this constraint is not guaranteed to
+-- avoid all loops; it is still possible to create indirect recursive
+-- loops but excludes some real-world problems.
+ALTER TABLE acs_objects ADD CONSTRAINT acs_objects_context_id_ck CHECK (context_id != object_id);
+
 create index acs_objects_context_object_idx on acs_objects (context_id, object_id);
 
 alter table acs_objects modify constraint acs_objects_context_object_un enable;
