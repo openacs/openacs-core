d64889c1531586ca9c107f50800d3b8bf396a22c
Antonio Pisano <antonio@elettrotecnica.it>
2021-09-07 12:12:46 +0000

    Add include contract

--- packages/acs-templating/lib/body_scripts.tcl
+++ packages/acs-templating/lib/body_scripts.tcl
@@ -1,3 +1,7 @@
+ad_include_contract {
+    Renders the body scripts into the page
+}
+
 ns_log notice "BODY_SCRIPTS called"
 template::get_body_event_handlers
 template::prepare_body_script_multirow
