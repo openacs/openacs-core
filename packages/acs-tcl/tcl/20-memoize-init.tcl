e02fd165d3a49574d3305ab90f4d624593732a3c
Antonio Pisano <antonio@elettrotecnica.it>
2021-10-18 16:31:20 +0000

    Prefer api to retrieve the acs-kernel id, as this will work also in corercases such as bootstraping of a new OpenACS instance

--- packages/acs-tcl/tcl/20-memoize-init.tcl
+++ packages/acs-tcl/tcl/20-memoize-init.tcl
@@ -1,7 +1,7 @@
 # Create the cache used by util_memoize.
 
 ns_cache create util_memoize -size \
-    [parameter::get -package_id $::acs::kernel_id -parameter MaxSize -default 200000]
+    [parameter::get -package_id [ad_acs_kernel_id] -parameter MaxSize -default 200000]
 
 # Local variables:
 #    mode: tcl
