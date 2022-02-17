ec118c29172f7b1fb9ea6e23f360e8dcc8b88dfe
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2022-02-17 15:20:12 +0000

    Fixes for Oracle 19c: fix data types and interval calculation

--- packages/acs-templating/sql/oracle/captcha-create.sql
+++ packages/acs-templating/sql/oracle/captcha-create.sql
@@ -1,14 +1,12 @@
+--
 -- Captcha data-model
--- Note: in Postgres we can define the default for expiration as "one
--- hour from now". This is apparently trickier in Oracle, so the
--- default as defined by the datamodel is equal to automatic failure
--- for the captcha check. In practice, this is fine, because the
--- actual expiration we set in the widget code.
+--
 
 create table template_widget_captchas (
-   image_checksum text primary key,
-   text text not null,
-   expiration timestamp not null default current_timestamp
+   image_checksum  varchar2(4000) primary key,
+   text            varchar2(4000) not null,
+   expiration      timestamp default (sysdate + interval '1' hour )
+                   constraint template_widget_captchas_nn not null
 );
 
 create index template_widget_captchas_expiration_idx on template_widget_captchas(expiration);
