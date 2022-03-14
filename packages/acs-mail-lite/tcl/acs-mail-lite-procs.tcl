dff2760cc55dda2d197398c906c2fe9cb43cbebb
Antonio Pisano <antonio@elettrotecnica.it>
2022-03-14 13:39:30 +0000

    Fix typo in doc

--- packages/acs-mail-lite/tcl/acs-mail-lite-procs.tcl
+++ packages/acs-mail-lite/tcl/acs-mail-lite-procs.tcl
@@ -288,10 +288,11 @@ namespace eval acs_mail_lite {
         {-object_id ""}
     } {
 
-        Prepare an email to be send with the option to pass in a list
-        of file_ids as well as specify an html_body and a mime_type. It
-        also supports multiple "TO" recipients as well as CC
-        and BCC recipients. Runs entirely off MIME and SMTP to achieve this.
+        Prepare an email to be sent, with the option to pass in a list
+        of file_ids as well as specify an html_body and a
+        mime_type. It also supports multiple "TO" recipients as well
+        as CC and BCC recipients. Runs entirely off MIME and SMTP to
+        achieve this.
 
         @param send_immediately The email is send immediately and not
                stored in the acs_mail_lite_queue
@@ -454,10 +455,11 @@ namespace eval acs_mail_lite {
         {-force_delivery_mode ""}
     } {
 
-        Prepare an email to be send immediately with the option to pass in a list
-        of file_ids as well as specify an html_body and a mime_type. It also supports
-        multiple "TO" recipients as well as CC
-        and BCC recipients. Runs entirely off MIME and SMTP to achieve this.
+        Prepare an email to be sent immediately, with the option to
+        pass in a list of file_ids as well as specify an html_body and
+        a mime_type. It also supports multiple "TO" recipients as well
+        as CC and BCC recipients. Runs entirely off MIME and SMTP to
+        achieve this.
 
         @param to_addr List of e-mail addresses to send this mail to.
 
