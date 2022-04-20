81af3b1b8360d13554128e74ece879fb2ec70032
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2022-04-20 08:51:01 +0000

    revive delete regression test (was formerly in xowiki)

--- packages/acs-templating/tcl/test/parse-test-procs.tcl
+++ packages/acs-templating/tcl/test/parse-test-procs.tcl
@@ -87,6 +87,30 @@ aa_register_case \
 
 }
 
+aa_register_case \
+    -cats {api smoke production_safe} \
+    -procs {
+        ::template::adp_parse_tags
+    } \
+    adp_parse_tags {
+
+        Checks the helper template::adp_parse_tags, which performs a
+        subset of template::adp_compile.
+
+    } {
+        set HTML {<p>foo <adp:icon name="edit">bar}
+        set result [::template::adp_parse_tags $HTML]
+        aa_log [ns_quotehtml $result]
+        aa_true "test substitution of adp:icon contains either 'class' or 'src' attribute" \
+            [regexp {(class=|src=)} $result]
+        set HTML {<p>foo @a@ <adp:icon name="edit">bar @b@}
+        set result [::template::adp_parse_tags $HTML]
+        aa_log [ns_quotehtml $result]
+        aa_true "test substitution contains still template variables" \
+            [regexp {@} $result]
+    }
+
+
 # Local variables:
 #    mode: tcl
 #    tcl-indent-level: 4
