c5007e2ff4f1c5e2e9a61b6caee3a316902b2e09
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2022-08-11 11:00:48 +0000

    prefer parameter::get_from_package_key over fetching package_id first

--- packages/acs-templating/tcl/template-init.tcl
+++ packages/acs-templating/tcl/template-init.tcl
@@ -8,15 +8,17 @@
 # License.  Full text of the license is available from the GNU Project:
 # http://www.fsf.org/copyleft/gpl.html
 
-# XXX (bquinn): This file should not be here.
-
-set pkg_id [apm_package_id_from_key acs-templating]
-
-if { [parameter::get -package_id $pkg_id -parameter ShowCompiledTemplatesP -default 0] } {
+if { [parameter::get_from_package_key \
+          -package_key acs-templating \
+          -parameter ShowCompiledTemplatesP \
+          -default 0] } {
   ad_register_filter postauth GET *.cmp template::cmp_page_filter
 }
 
-if { [parameter::get -package_id $pkg_id -parameter ShowDataDictionariesP -default 0] } {
+if { [parameter::get_from_package_key \
+          -package_key acs-templating \
+          -parameter ShowDataDictionariesP \
+          -default 0] } {
   ad_register_filter postauth GET *.dat template::dat_page_filter
   ad_register_filter postauth GET *.frm template::frm_page_filter
 }
