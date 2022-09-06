a007a70a8381ad5623b13ed9cc58ace6533d7cc5
Antonio Pisano <antonio@elettrotecnica.it>
2022-09-06 05:21:58 +0000

    Deprecate util_AnsiDatetoPrettyDate

--- packages/acs-tcl/tcl/utilities-procs.tcl
+++ packages/acs-tcl/tcl/utilities-procs.tcl
@@ -253,10 +253,15 @@ ad_proc -public util::get_referrer {
 
 
 
-ad_proc -public util_AnsiDatetoPrettyDate {
+ad_proc -deprecated util_AnsiDatetoPrettyDate {
     sql_date
 } {
     Converts 1998-09-05 to September 5, 1998
+
+    DEPRECATED: this proc hardcodes the date format and the language
+    to American English. Better alternatives exist in acs-lang.
+
+    @see lc_time_fmt
 } {
     set sql_date [string range $sql_date 0 9]
     if { ![regexp {(.*)-(.*)-(.*)$} $sql_date match year month day] } {
