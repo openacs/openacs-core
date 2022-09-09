7c5d7336fa328a2b088aec3eaca421c004650ee2
Antonio Pisano <antonio@elettrotecnica.it>
2022-09-09 10:00:05 +0000

    Deprecate old api

--- packages/acs-automated-testing/tcl/example-procs.tcl
+++ packages/acs-automated-testing/tcl/example-procs.tcl
@@ -15,7 +15,7 @@ ad_library {
     @cvs-id $Id$
 }
 
-ad_proc -private aa_example_write_audit_entry {
+ad_proc -deprecated -private aa_example_write_audit_entry {
   name
   value
 } {
@@ -26,7 +26,7 @@ ad_proc -private aa_example_write_audit_entry {
   return 1
 }
 
-ad_proc -public aa_example_write_audit_entries {
+ad_proc -deprecated aa_example_write_audit_entries {
   entries
 } {
   @author Peter Harper
