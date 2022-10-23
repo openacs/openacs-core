c906fad4f0ac5ef83d060b3b485dd60698ac02a6
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2022-10-23 18:49:37 +0000

    added support for listing all registered URN of an OpenACS instance

---
+++ packages/acs-templating/www/sitewide-admin/index.adp
@@ -0,0 +1,6 @@
+<master>
+<property name="doc(title)">@title;literal@</property>
+<property name="context">@context;literal@</property>
+
+<h1>@title;noquote@</h1>
+<include src="/packages/acs-templating/lib/registered-urns" match="*">
---
+++ packages/acs-templating/www/sitewide-admin/index.tcl
@@ -0,0 +1,15 @@
+ad_page_contract {
+    @author Gustaf Neumann
+
+    @creation-date Aug 6, 2018
+} {
+}
+
+set title "ACS Templating Package - Sitewide Admin"
+set context [list $title]
+
+# Local variables:
+#    mode: tcl
+#    tcl-indent-level: 4
+#    indent-tabs-mode: nil
+# End:
