02144dc1bdc7418307ec519d571409f4bd9ae969
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2022-10-23 17:44:43 +0000

    registered-urns: new adp-include for listing registered URNs
    This function is useful e.g. for the packages using optionally
    CDN or local resources. It helps an admin for testing the
    registered resources (e.g. via sitewide-admin pages).
    
    Bump version number to 5.10.1d19.

---
+++ packages/acs-templating/lib/registered-urns.adp
@@ -0,0 +1,6 @@
+<h4>Registered URNs</h4>
+<ul>
+<multiple name="urns">
+<li><code>@urns.urn@</code>: <a href="@urns.url@">@urns.url@</a></li>
+</multiple>
+</ul>
---
+++ packages/acs-templating/lib/registered-urns.tcl
@@ -0,0 +1,20 @@
+ad_include_contract {
+    Renders the registered URNs, optionally filtered via match pattern.
+
+    @param match optional match pattern
+    @creation-date 2022-10-23
+    @author Gustaf Neumann
+} {
+    {match "*"}
+}
+
+template::multirow create urns urn url
+foreach urn [lsort [array names ::template::head::urn $match]] {
+    template::multirow append urns $urn [template::head::resolve_urn $urn]
+}
+
+# Local variables:
+#    mode: tcl
+#    tcl-indent-level: 4
+#    indent-tabs-mode: nil
+# End:
