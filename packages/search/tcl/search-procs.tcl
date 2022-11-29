4f1641ea42f44c67920bef2d83f5e37869605984
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2022-11-29 18:34:00 +0000

    Make use of new API "ad_mktmpdir" and "ad_opentmpfile" instead of "ad_tmpnam"

--- packages/search/tcl/search-procs.tcl
+++ packages/search/tcl/search-procs.tcl
@@ -320,8 +320,7 @@ ad_proc -private search::content_filter {
         # Write content to a file and let the filter below extract the
         # words for the index from the file.
         #
-        set tmp_filename [ad_tmpnam]
-        set f [open $tmp_filename w]
+        set f [ad_opentmpfile tmp_filename]
         puts $f $data
         close $f
         set data $tmp_filename
