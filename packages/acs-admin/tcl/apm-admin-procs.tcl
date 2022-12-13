7f34244e3ee3c8ef36a1c9dfcfe59d49f1036348
Antonio Pisano <antonio@elettrotecnica.it>
2022-12-13 14:58:25 +0000

    Replace api

--- packages/acs-admin/tcl/apm-admin-procs.tcl
+++ packages/acs-admin/tcl/apm-admin-procs.tcl
@@ -477,14 +477,13 @@ ad_proc -private apm_build_repository {
                             # The path to the 'packages' directory in the checkout
                             set packages_root_path [file join {*}[lrange [file split $spec_file] 0 end-2]]
 
-                            set tmp_filename [ad_tmpnam]
-                            lappend cmd -C $packages_root_path --files-from $tmp_filename
-
-                            set fp [open $tmp_filename w]
+                            set fp [ad_opentmpfile tmp_filename]
                             foreach file $files {
                                 puts $fp $package_key/$file
                             }
                             close $fp
+
+                            lappend cmd -C $packages_root_path --files-from $tmp_filename
 
                             lappend cmd "|" [apm_gzip_cmd] -c ">" $apm_file
                             ns_log Notice "Executing: exec $cd_helper $packages_root_path $cmd"
