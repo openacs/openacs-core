ec45c6b4325d2b6bade97314f88a62f6a32048c0
Antonio Pisano <antonio@elettrotecnica.it>
2023-02-07 11:47:40 +0000

    Deprecate acs_privacy:: api and inline the few usages in the dotlrn packages
    See https://openacs.org/forums/message-view?message_id=5672076

--- packages/acs-tcl/tcl/acs-private-data-procs.tcl
+++ packages/acs-tcl/tcl/acs-private-data-procs.tcl
@@ -9,27 +9,45 @@ ad_library {
 
 namespace eval acs_privacy {
 
-    ad_proc -public privacy_control_enabled_p {} {
+    ad_proc -deprecated privacy_control_enabled_p {} {
         Returns whether privacy control is turned on or not.
         This is provided in order to have complete backwards
         compatibility with past behaviors, where private information
         was in no way regulated.
+
+        DEPRECATED: this privacy feature was only used in the context
+        of dotlrn, which also has an own set of apis fot a similar
+        purpose.
+
+        @see dotlrn
     } {
         # If no parameter set, then we assume privacy control is DISABLED
         return [parameter::get -package_id [ad_acs_kernel_id] -parameter PrivacyControlEnabledP -default 0]
     }
 
-    ad_proc -public privacy_control_set {val} {
+    ad_proc -deprecated privacy_control_set {val} {
         set the privacy control
+
+        DEPRECATED: this privacy feature was only used in the context
+        of dotlrn, which also has an own set of apis fot a similar
+        purpose.
+
+        @see dotlrn
     } {
         return [parameter::set_value -value $val -package_id [ad_acs_kernel_id] -parameter PrivacyControlEnabledP]
     }
 
-    ad_proc -public user_can_read_private_data_p {
+    ad_proc -deprecated user_can_read_private_data_p {
         {-user_id ""}
         {-object_id:required}
     } {
         check if a user can access an object's private data
+
+        DEPRECATED: this privacy feature was only used in the context
+        of dotlrn, which also has an own set of apis fot a similar
+        purpose.
+
+        @see dotlrn
     } {
         if {[privacy_control_enabled_p]} {
             return [permission::permission_p -party_id $user_id -object_id $object_id -privilege read_private_data]
@@ -39,12 +57,18 @@ namespace eval acs_privacy {
         }
     }
 
-    ad_proc -public set_user_read_private_data {
+    ad_proc -deprecated set_user_read_private_data {
         {-user_id:required}
         {-object_id:required}
         {-value:required}
     } {
         grant permission to access private data
+
+        DEPRECATED: this privacy feature was only used in the context
+        of dotlrn, which also has an own set of apis fot a similar
+        purpose.
+
+        @see dotlrn
     } {
         if { [string is true -strict $value] } {
             permission::grant -party_id $user_id -object_id $object_id -privilege read_private_data
