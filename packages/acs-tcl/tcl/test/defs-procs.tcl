98bf9defddd878265271f993ccd433938d17f158
Antonio Pisano <antonio@elettrotecnica.it>
2023-02-28 12:39:53 +0000

    Increase proc coverage

--- packages/acs-tcl/tcl/test/defs-procs.tcl
+++ packages/acs-tcl/tcl/test/defs-procs.tcl
@@ -14,6 +14,12 @@ aa_register_case \
         acs_community_member_link
         subsite::get_element
         ad_admin_home
+        ad_pvt_home
+        ad_pvt_home_name
+        ad_pvt_home_link
+        ad_publisher_name
+        ad_site_home_link
+        ad_system_name
     } \
     user_links_api {
         Test the various procs that generate a community member URL.
@@ -22,7 +28,7 @@ aa_register_case \
             select max(user_id) from users
         }]
 
-        set admin_url [parameter::get -package_id $::acs::kernel_id -parameter CommunityMemberAdminURL]
+        set admin_url [parameter::get -package_id [ad_acs_kernel_id] -parameter CommunityMemberAdminURL]
 
         aa_section "Admin URL"
         set url [acs_community_member_admin_url -user_id $user_id]
@@ -45,7 +51,7 @@ aa_register_case \
         aa_true "Link '$link' contains the username" \
             {[string first [person::name -person_id $user_id] $link] >= 0}
 
-        set member_url [parameter::get -package_id $::acs::kernel_id -parameter CommunityMemberURL]
+        set member_url [parameter::get -package_id [ad_acs_kernel_id] -parameter CommunityMemberURL]
         set subsite_url [subsite::get_element -element url -notrailing]
 
         aa_section "Member URL"
@@ -74,6 +80,64 @@ aa_register_case \
         aa_section "Admin home URL"
         aa_equals "The admin home URL is as expected" \
             [ad_admin_home] [subsite::get_element -element url]admin
+
+        aa_section "Private workspace"
+        set pvt_home_url [ad_pvt_home]
+        aa_equals "The private workspace URL is as expected" \
+            $pvt_home_url \
+            [subsite::get_element -element url -notrailing][parameter::get -package_id [ad_acs_kernel_id] -parameter HomeURL]
+
+        set pvt_home_name [ad_pvt_home_name]
+        aa_equals "The private home name is expected" \
+            $pvt_home_name \
+            [lang::util::localize [parameter::get -package_id [ad_acs_kernel_id] -parameter HomeName]]
+
+        set pvt_home_link [ad_pvt_home_link]
+        aa_true "Private home link is expected" {
+            [string first $pvt_home_url $pvt_home_link] >= 0 &&
+            [string first $pvt_home_name $pvt_home_link] >= 0 &&
+            [ad_looks_like_html_p $pvt_home_link]
+        }
+
+        aa_section "Publisher name"
+        aa_equals "Publisher name is expected" \
+            [ad_publisher_name] \
+            [parameter::get -package_id [ad_acs_kernel_id] -parameter PublisherName]
+
+        aa_section "Site home link"
+        set old_user_id [ad_conn user_id]
+        try {
+            ad_conn -set user_id 0
+            set site_home_link [ad_site_home_link]
+
+            aa_true "Subsite name is in the link (user 0)" {
+                [subsite::get_element -element name] eq "" ||
+                [string first [subsite::get_element -element name] $site_home_link] >= 0
+            }
+            aa_true "Subsite URL is in the link (user 0)" {
+                [string first [subsite::get_element -element url] $site_home_link] >= 0
+            }
+            aa_true "Link is HTML (user 0)" [ad_looks_like_html_p $site_home_link]
+
+            ad_conn -set user_id 1
+            set site_home_link [ad_site_home_link]
+
+            aa_true "Subsite name is in the link (user 1)" {
+                [subsite::get_element -element name] eq "" ||
+                [string first [subsite::get_element -element name] $site_home_link] >= 0
+            }
+            aa_true "Home URL is in the link (user 1)" {
+                [string first $pvt_home_url $site_home_link] >= 0
+            }
+            aa_true "Link is HTML (user 1)" [ad_looks_like_html_p $site_home_link]
+        } finally {
+            ad_conn -set user_id $old_user_id
+        }
+
+        aa_section "System name"
+        aa_equals "ad_system_name returns expected" \
+            [ad_system_name] \
+            [parameter::get -package_id [ad_acs_kernel_id] -parameter SystemName]
     }
 
 aa_register_case \
