02242440137917110c2bc4d0659c298ef1c4e1fc
Antonio Pisano <antonio@elettrotecnica.it>
2023-08-09 11:50:07 +0000

    Whitespace changes

--- packages/notifications/lib/notification-widget.tcl
+++ packages/notifications/lib/notification-widget.tcl
@@ -17,14 +17,24 @@ if {$url eq ""} {
 }
 
 set type_id [notification::type::get_type_id -short_name $type]
-set request_id [notification::request::get_request_id -type_id $type_id -object_id $object_id -user_id $user_id]
-    
+set request_id [notification::request::get_request_id \
+                    -type_id $type_id \
+                    -object_id $object_id \
+                    -user_id $user_id]
+
 if {$request_id ne ""} {
-    set sub_url [notification::display::unsubscribe_url -request_id $request_id -url $url]
+    set sub_url [notification::display::unsubscribe_url \
+                     -request_id $request_id \
+                     -url $url]
     set title [_ notifications.lt_Ubsubscribe_Notification_]
     set sub_chunk [_ notifications.lt_You_have_requested_no]
 } else {
-    set sub_url [notification::display::subscribe_url -type $type -object_id $object_id -url $url -user_id $user_id -pretty_name $pretty_name]
+    set sub_url [notification::display::subscribe_url \
+                     -type $type \
+                     -object_id $object_id \
+                     -url $url \
+                     -user_id $user_id \
+                     -pretty_name $pretty_name]
     set title [_ notifications.lt_Request_Notification_]
     set sub_chunk [_ notifications.lt_You_may_a_hrefsub_url]
 }
@@ -38,4 +48,3 @@ if { $show_subscribers_p && [permission::permission_p -object_id $object_id -pri
 #    tcl-indent-level: 4
 #    indent-tabs-mode: nil
 # End:
-
