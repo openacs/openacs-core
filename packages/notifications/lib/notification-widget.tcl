50a1e9a6f26f3ce96b89581f6fe45b829ac011ba
Antonio Pisano <antonio@elettrotecnica.it>
2023-08-09 11:34:20 +0000

    Cleanup obsolete variable

--- packages/notifications/lib/notification-widget.tcl
+++ packages/notifications/lib/notification-widget.tcl
@@ -20,12 +20,10 @@ set type_id [notification::type::get_type_id -short_name $type]
 set request_id [notification::request::get_request_id -type_id $type_id -object_id $object_id -user_id $user_id]
     
 if {$request_id ne ""} {
-    set icon /resources/acs-subsite/email_delete.gif
     set sub_url [notification::display::unsubscribe_url -request_id $request_id -url $url]
     set title [_ notifications.lt_Ubsubscribe_Notification_]
     set sub_chunk [_ notifications.lt_You_have_requested_no]
 } else {
-    set icon /resources/acs-subsite/email_add.gif
     set sub_url [notification::display::subscribe_url -type $type -object_id $object_id -url $url -user_id $user_id -pretty_name $pretty_name]
     set title [_ notifications.lt_Request_Notification_]
     set sub_chunk [_ notifications.lt_You_may_a_hrefsub_url]
