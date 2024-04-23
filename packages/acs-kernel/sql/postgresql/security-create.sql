28d069f527ec88f0a81bce6d1e2f78c6e8908567
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2024-04-23 12:19:02 +0000

    Fixed issue introduced in OpenACS 5.9.0
    The old version did not insert a property value via the
    sec_session_property__upsert() in PostgreSQL on the initial setting
    (later updates were OK). The broken version was just adding a tuple
    and left the "property_value" empty.
    
    Many thanks to Jonathan Kelley for identifying the issue and reporting it.

--- packages/acs-kernel/sql/postgresql/security-create.sql
+++ packages/acs-kernel/sql/postgresql/security-create.sql
@@ -65,23 +65,23 @@ $$
 BEGIN
     LOOP
         -- first try to update the key
-    update sec_session_properties
+      update sec_session_properties
         set   property_value = p_value, secure_p = p_secure_p, last_hit = p_last_hit 
         where session_id = p_session_id and module = p_module and property_name = p_name;
-        IF found THEN
-            return;
-        END IF;
-        -- not there, so try to insert the key
-        -- if someone else inserts the same key concurrently,
-        -- we could get a unique-key failure
-        BEGIN
-            insert into sec_session_properties
-                   (session_id,   module,   property_name, secure_p,   last_hit)
-            values (p_session_id, p_module, p_name,        p_secure_p, p_last_hit);
-            RETURN;
-            EXCEPTION WHEN unique_violation THEN
-            -- Do nothing, and loop to try the UPDATE again.
-        END;
+      IF found THEN
+         RETURN;
+      END IF;
+      -- not there, so try to insert the key
+      -- if someone else inserts the same key concurrently,
+      -- we could get a unique-key failure
+      BEGIN
+         insert into sec_session_properties
+                   (session_id,   module,   property_name, property_value, secure_p,   last_hit)
+            values (p_session_id, p_module, p_name,        p_value,        p_secure_p, p_last_hit);
+         RETURN;
+         EXCEPTION WHEN unique_violation THEN
+         -- Do nothing, and loop to try the UPDATE again.
+      END;
     END LOOP;
 END;
 $$ LANGUAGE plpgsql;
