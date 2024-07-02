9d8b6d5b128f34e1dadc23d19dd01bd10b56f361
Thomas Renner <thomas.renner@wu.ac.at>
2024-07-02 15:00:18 +0000

    use original provided host-header-field in log statement

--- packages/acs-tcl/tcl/security-procs.tcl
+++ packages/acs-tcl/tcl/security-procs.tcl
@@ -3177,7 +3177,7 @@ ad_proc -public security::validated_host_header {} {
                     set value
                 }]
                 if {$normalizedHostHeaderValue in $names} {
-                    ns_log notice "security::validated_host_header: found $normalizedHostHeaderValue" \
+                    ns_log notice "security::validated_host_header: found $hostHeaderValue" \
                         "in global virtual server configuration for $driver"
                     return 1
                 }
