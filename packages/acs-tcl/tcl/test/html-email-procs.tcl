9db02ee10b1b3fd3af71f51dd542c3e105ec69ba
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2024-08-31 09:46:33 +0000

    improved test case
    - load first mime package before loading it
    - provide version of the mime package in the test log
    - don't provide ns_set id as "error"

--- packages/acs-tcl/tcl/test/html-email-procs.tcl
+++ packages/acs-tcl/tcl/test/html-email-procs.tcl
@@ -6,16 +6,25 @@ aa_register_case \
     -cats {api smoke} \
     -procs {ad_build_mime_message} \
     ad_build_mime_message {
-    Basic test of build mime message
-} {
-    aa_false "Build mime message, no error" \
-        [catch {ad_build_mime_message \
-                    "Test Message" \
-                    "<p>Test Message</p>"} errmsg]
-    aa_log err=$errmsg
-    aa_false "Package require mime package found" \
-        [catch {package require mime} errmsg]
+        Basic test of build mime message
+    } {
+        try {
+            package require mime
+        } on ok {versionNumber} {
+            aa_true "MIME package in version $versionNumber loaded" 1
+        } on error {errorMsg} {
+            aa_false "could not load MIME package from tcllib: $errorMsg" 1
+        }
 
+        try {
+            ad_build_mime_message \
+                "Test Message" \
+                "<p>Test Message</p>"
+        } on ok {ns_set} {
+            aa_true "built ns_set for containing an email message" {$ns_set ne ""}
+        } on error {errorMsg} {
+            aa_false "could not build build_mime_message: $errorMsg" 1
+        }
 
 }
 # Local variables:
