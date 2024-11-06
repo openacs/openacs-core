174c269df25be928ad61267ee596e223eb36442e
Antonio Pisano <antonio@elettrotecnica.it>
2024-11-06 15:51:58 +0000

    Bring existing doc into the object's documentation, so that tests would not fail...

--- packages/acs-subsite/tcl/group-init.tcl
+++ packages/acs-subsite/tcl/group-init.tcl
@@ -1,14 +1,15 @@
-#
-# Create group cache. The sizes can be tailored in the config
-# file like the following:
-#
-# ns_section ns/server/${server}/acs/acs-subsite
-#   ns_param GroupCache        2MB
-#
 ::acs::KeyPartitionedCache create ::acs::group_cache \
     -package_key acs-subsite \
     -parameter GroupCache \
-    -default_size 2MB
+    -default_size 2MB {
+        #
+        # Create group cache. The sizes can be tailored in the config
+        # file like the following:
+        #
+        # ns_section ns/server/${server}/acs/acs-subsite
+        #   ns_param GroupCache        2MB
+        #
+    }
 
 # Local variables:
 #    mode: tcl
