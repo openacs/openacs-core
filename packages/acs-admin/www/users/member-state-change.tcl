openacs/openacs-core

commit 3657d747e189ffc9b345b190e8969ff8db78c0c1

harden page contracts

packages/acs-admin/www/users/member-state-change.tcl
@@ -11,7 +11,7 @@ ad_page_contract {
     {member_state:trim}
     {email_verified_p:boolean ""}
     {return_url:localurl ""}
-    {pass_through:boolean false}
+    {pass_through:boolean,notnull false}
 } -validate {
     valid_member_state -requires member_state {
         if {$member_state ni {approved banned deleted merged "needs approval" rejected}} {
@@ -40,6 +40,9 @@ set name   [dict get $user_info name]
 set email  [dict get $user_info email]
 set rel_id [dict get $user_info rel_id]
 
+if {$email_verified ne ""} {
+    set email_verified [expr {[string is true -strict] ? "t" : "f"}]
+}
 #
 # This page is used for state changes in the member_state, and as well
 # on email confirm require and approve operations.

packages/acs-admin/www/apm/index.tcl
@@ -6,10 +6,10 @@ ad_page_contract {
     @author Jon Salz (jsalz@arsdigita.com)
     @cvs-id $Id$
 } {
-    { orderby:token "package_key" }
-    { owned_by:word "everyone" }
-    { supertype "all" }
-    { reload_links_p:boolean 0 }
+    { orderby:token,notnull "package_key" }
+    { owned_by:word,notnull "everyone" }
+    { supertype:notnull "all" }
+    { reload_links_p:boolean,notnull 0 }
 }
 
 set page_title "Package Manager"

packages/acs-admin/www/apm/package-add-2.tcl
@@ -8,11 +8,11 @@ ad_page_contract {
     package_key:token
     pretty_name
     pretty_plural
-    { package_type "apm_application"}
-    { initial_install_p:boolean "f" }
-    { singleton_p:boolean "f" }
-    { implements_subsite_p:boolean f }
-    { inherit_templates_p:boolean t }
+    { package_type:notnull "apm_application"}
+    { initial_install_p:boolean,notnull "f" }
+    { singleton_p:boolean,notnull "f" }
+    { implements_subsite_p:boolean,notnull f }
+    { inherit_templates_p:boolean,notnull t }
     { auto_mount "" }
     package_uri
     version_name
@@ -26,9 +26,9 @@ ad_page_contract {
     { owner_uri:multiple}
     { vendor ""}
     { vendor_uri ""}
-    { install_p:boolean 0 }
-    {implements_subsite_p:boolean "f"}
-    {inherit_templates_p:boolean "f"}
+    { install_p:boolean,notnull 0 }
+    {implements_subsite_p:boolean,notnull "f"}
+    {inherit_templates_p:boolean,notnull "f"}
 } -validate {
     package_key_format -requires {package_key} {
         if { [regexp {[^a-z0-9-]} $package_key] } {

packages/acs-admin/www/apm/package-delete-2.tcl
@@ -6,8 +6,8 @@ ad_page_contract {
     @creation-date Fri Oct 13 08:42:50 2000
     @cvs-id $Id$
 } {
-    version_id:naturalnum
-    {remove_files:boolean 0}
+    version_id:naturalnum,notnull
+    {remove_files:boolean,notnull 0}
     {sql_drop_scripts:multiple ""}
 }
 

packages/acs-admin/www/apm/package-load-2.tcl
@@ -9,7 +9,7 @@ ad_page_contract {
 } {
     {url ""}
     {file_path ""}
-    {delete:boolean 0}
+    {delete:boolean,notnull 0}
 } -validate {
     
     url_xor_file_path {

packages/acs-admin/www/apm/packages-install-2.tcl
@@ -7,7 +7,7 @@ ad_page_contract {
     @cvs-id $Id$
 } {
     {package_key:token,multiple ""}
-    {force_p:boolean "f"}
+    {force_p:boolean,notnull "f"}
 }
 
 set title "Package Installation"

packages/acs-admin/www/apm/packages-install.tcl
@@ -6,8 +6,8 @@ ad_page_contract {
     @cvs-id $Id$
 
 } {
-    {checked_by_default_p:boolean 0}
-    {operation:word all}
+    {checked_by_default_p:boolean,notnull 0}
+    {operation:word,notnull all}
 }
 
 set title "Package Installation"

packages/acs-admin/www/apm/version-edit-2.tcl
@@ -17,7 +17,7 @@ ad_page_contract {
     vendor_uri
     {auto_mount ""}
     {release_date ""}
-    { upgrade_p:boolean 0 }
+    { upgrade_p:boolean,notnull 0 }
 }
 
 # Validate dynamic package version attributes

commit 91bc59a427e2d2d5c45b243285ad4c4cf8b290c9

Allow to verify emails again (fix bug introduced in https://github.com/openacs/openacs-core/commit/3657d747e189ffc9b345b190e8969ff8db78c0c1 )

packages/acs-admin/www/users/member-state-change.tcl
@@ -41,7 +41,7 @@ set email  [dict get $user_info email]
 set rel_id [dict get $user_info rel_id]
 
 if {$email_verified_p ne ""} {
-    set email_verified_p [expr {[string is true -strict] ? "t" : "f"}]
+    set email_verified_p [expr {[string is true -strict $email_verified_p] ? "t" : "f"}]
 }
 #
 # This page is used for state changes in the member_state, and as well